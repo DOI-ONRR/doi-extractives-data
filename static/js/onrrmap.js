8c70992a0199c928533e4bbc5262252b4812f3e6
Michelle <michelle.hertzfeld@gsa.gov>
2014-10-21 21:54:36 -0700

    Layout overhaul. Also added ascii code ellipses instead of dot dot dot, which is great.

--- static/js/onrrmap.js
+++ static/js/onrrmap.js
@@ -13,7 +13,7 @@ var gomDrawOrder = ['Eastern Gulf of Mexico','Central Gulf of Mexico','Western G
 
 var mapdataviz = L.map('map', {
     scrollWheelZoom: false
-  }).setView([37.9282, -99.5795], 4);
+  }).setView([41.5, -99.5795], 4);
 L.tileLayer('http://a.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: 'Tiles Courtesy of <a href="http://www.mapquest.com/" target="_blank">MapQuest</a>',
     maxZoom: 18,

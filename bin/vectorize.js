f26490d8715f82d1c54bb342b9699c397a618fcf
Shawn Allen <shawn.allen@gsa.gov>
2015-04-13 15:26:12 -0700

    use rw so we can read from stdin

--- bin/vectorize.js
+++ bin/vectorize.js
@@ -22,7 +22,7 @@ if (options.help) {
   return yargs.showHelp();
 }
 
-var fs = require('fs');
+var rw = require('rw');
 var util = require('../lib/util');
 var async = require('async');
 var d3 = require('d3');
@@ -32,15 +32,13 @@ var albersCustom = require('../lib/albers-custom');
 
 var scripts = [];
 
-var out = fs.createWriteStream(options.o);
-
 async.waterfall([
   function loadFiles(done) {
     async.map(options._, function load(filename, next) {
       util.readJSON(filename, function(error, data) {
-	if (error) return done(error);
-	data.filename = filename;
-	return next(null, data);
+        if (error) return done(error);
+        data.filename = filename;
+        return next(null, data);
       });
     }, done);
   },
@@ -49,128 +47,129 @@ async.waterfall([
       if (errors) return console.error('error:', errors[0]);
 
       var proj = options.proj
-	? d3.geo[options.proj]()
-	: albersCustom();
+        ? d3.geo[options.proj]()
+        : albersCustom();
       if (options.center) {
-	var center = coord(options.center);
-	proj.center(center);
+        var center = coord(options.center);
+        proj.center(center);
       }
       if (options.scale) {
-	proj.scale(options.scale * proj.scale());
+        proj.scale(options.scale * proj.scale());
       }
       if (options.translate) {
-	var translate = coord(options.translate);
-	proj.translate(translate);
+        var translate = coord(options.translate);
+        proj.translate(translate);
       }
       var path = d3.geo.path()
-	.projection(proj);
+        .projection(proj);
 
       var size = proj.size
-	? proj.size()
-	: [options.width, options.height];
+        ? proj.size()
+        : [options.width, options.height];
 
       var svg = d3.select(window.document.querySelector('svg'))
-	.attr('xmlns', 'http://www.w3.org/2000/svg')
-	.attr('width', size[0])
-	.attr('height', size[1]);
+        .attr('xmlns', 'http://www.w3.org/2000/svg')
+        .attr('width', size[0])
+        .attr('height', size[1]);
 
       var style = svg.append('style')
-	.attr('type', 'text/css')
-	.text([
-	  '',
-	  'path { fill: none; stroke: #ccc; stroke-width: .5; vector-effect: non-scaling-stroke; }',
-	  '#states path { stroke: #999; stroke-width: 1; }',
-	  '#USA path { stroke: #eee; stroke-width: 3; }',
-	  '',
-	].join('\n'));
+        .attr('type', 'text/css')
+        .text([
+          '',
+          'path { fill: none; stroke: #ccc; stroke-width: .5; vector-effect: non-scaling-stroke; }',
+          '#states path { stroke: #999; stroke-width: 1; }',
+          '#USA path { stroke: #eee; stroke-width: 3; }',
+          '',
+        ].join('\n'));
 
       var bbox;
 
       var g = svg.selectAll('g.file')
-	.data(objects)
-	.enter()
-	.append('g')
-	  .attr('class', 'file')
-	  .attr('data-filename', function(d) {
-	    return d.filename;
-	  });
+        .data(objects)
+        .enter()
+        .append('g')
+          .attr('class', 'file')
+          .attr('data-filename', function(d) {
+            return d.filename;
+          });
 
       g.filter(function(d) {
-	  return d.type === 'Topology';
-	})
-	.selectAll('g.layer')
-	  .data(function(topology) {
-	    if (topology.bbox) bbox = topology.bbox;
-	    return d3.entries(topology.objects)
-	      .map(function(d) {
-		var mesh = topojson.mesh(topology, d.value);
-		mesh.id = d.key;
-		return mesh;
-	      });
-	  })
-	  .enter()
-	  .append('g')
-	    .attr('class', 'layer topojson')
-	    .attr('id', function(d) { return d.id; })
-	    .append('path')
-	      .attr('class', 'mesh')
-	      .attr('d', function(d) {
-		return path(d);
-	      });
+        return d.type === 'Topology';
+      })
+      .selectAll('g.layer')
+        .data(function(topology) {
+          if (topology.bbox) bbox = topology.bbox;
+          return d3.entries(topology.objects)
+            .map(function(d) {
+          var mesh = topojson.mesh(topology, d.value);
+          mesh.id = d.key;
+          return mesh;
+            });
+        })
+        .enter()
+        .append('g')
+          .attr('class', 'layer topojson')
+          .attr('id', function(d) { return d.id; })
+          .append('path')
+            .attr('class', 'mesh')
+            .attr('d', function(d) {
+          return path(d);
+            });
 
       g.filter(function(d) {
-	  return d.type === 'FeatureCollection';
-	})
-	.append('g')
-	  .attr('class', 'collection')
-	  .attr('id', function(d) {
-	    return d.filename;
-	  });
+          return d.type === 'FeatureCollection';
+        })
+        .append('g')
+          .attr('class', 'collection')
+          .attr('id', function(d) {
+            return d.filename;
+          });
 
       g.selectAll('g.collection')
-	.selectAll('path.feature')
-	.data(function(d) {
-	  return d.features;
-	})
-	.enter()
-	.append('path')
-	  .attr('class', 'feature')
-	  .attr('id', function(d) { return d.id; })
-	  .attr('d', path);
+        .selectAll('path.feature')
+        .data(function(d) {
+          return d.features;
+        })
+        .enter()
+        .append('path')
+          .attr('class', 'feature')
+          .attr('id', function(d) { return d.id; })
+          .attr('d', path);
 
       if (options.graticule) {
-	var graticule = d3.geo.graticule();
-	svg.append('path')
-	  .attr('class', 'graticule')
-	  .attr('d', path(graticule()))
-	  .attr('fill', 'none')
-	  .attr('stroke-dasharray', '2 2');
+        var graticule = d3.geo.graticule();
+        svg.append('path')
+          .attr('class', 'graticule')
+          .attr('d', path(graticule()))
+          .attr('fill', 'none')
+          .attr('stroke-dasharray', '2 2');
       }
 
       if (bbox) {
-	var bounds = path.bounds(bbox);
-	// console.warn('bbox:', bbox, '->', bounds);
-	var validBounds = bounds.every(function(c) {
-	  return c.every(isFinite);
-	});
-	if (validBounds) {
-	  svg.attr('viewBox', [
-	    bounds[0][0],
-	    bounds[0][1],
-	    bounds[1][0] - bounds[0][0],
-	    bounds[1][1] - bounds[0][1]
-	  ].join(' '));
-	}
+        var bounds = path.bounds(bbox);
+        // console.warn('bbox:', bbox, '->', bounds);
+        var validBounds = bounds.every(function(c) {
+          return c.every(isFinite);
+        });
+        if (validBounds) {
+          svg.attr('viewBox', [
+            bounds[0][0],
+            bounds[0][1],
+            bounds[1][0] - bounds[0][0],
+            bounds[1][1] - bounds[0][1]
+          ].join(' '));
+        }
       }
 
+      var out = rw.fileWriter(options.o);
       out.write('<?xml version="1.0" standalone="yes"?>\n');
       out.write(svg.property('outerHTML'));
+      out.end();
       done();
     });
   }
-], function(error, buffer) {
+], function(error) {
   if (error) return console.error('error:', error);
-  // out.close();
 });
 
 function coord(str) {

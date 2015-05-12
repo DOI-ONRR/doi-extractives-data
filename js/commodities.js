f5e71e329064c7b3f67ee13fcbbf50b42a50043c
Shawn Allen <shawn.allen@gsa.gov>
2015-05-11 17:32:18 -0700

    add commodity title

--- js/commodities.js
+++ js/commodities.js
@@ -3,6 +3,17 @@
   var data = exports.data = {
   };
 
+  var commodity = location.search
+    ? decodeURIComponent(location.search.substr(1))
+    : null;
+
+  var body = d3.select('body')
+    .classed('commodity-selected', !!commodity);
+
+  var title = d3.selectAll('.commodity-title')
+    .style('display', commodity ? null : 'none')
+    .text(commodity);
+
   var prog = progressive();
   d3.select('#progress')
     .call(progressive.bar(prog));
@@ -13,6 +24,7 @@
   queue()
     .defer(prog, d3.tsv, dataPath + 'national/revenues-yearly.tsv')
     .defer(prog, d3.tsv, dataPath + 'national/volumes-yearly.tsv')
+    .defer(prog, commodities.load, 'data/commodities.json')
     .await(function(error, natlRevenue, natlProduction) {
       if (error) return console.error(error.responseText);
 

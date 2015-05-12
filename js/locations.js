7ab080e036adf1d14405cbf11b803f8dafc215b9
Shawn Allen <shawn.allen@gsa.gov>
2015-05-11 17:32:02 -0700

    add region-map with links

--- js/locations.js
+++ js/locations.js
@@ -3,6 +3,19 @@
   var data = exports.data = {
   };
 
+  var place = location.search
+    ? decodeURIComponent(location.search.substr(1))
+    : null;
+
+  var body = d3.select('body')
+    .classed('place-selected', !!place);
+
+  var title = d3.selectAll('.place-title')
+    .style('display', place ? null : 'none')
+    .text(place);
+
+  var map = d3.select('#regions');
+
   var prog = progressive();
   d3.select('#progress')
     .call(progressive.bar(prog));
@@ -12,15 +25,31 @@
   var dataPath = 'output/';
   queue()
     .defer(prog, d3.csv, 'input/geo/states.csv')
-    .defer(prog, d3.json, dataPath + 'geo/us-topology.json')
-    .await(function(error, states, topology) {
+    .defer(mapLoaded)
+    .await(function(error) {
       if (error) return console.error(error.responseText);
-
       loaded(data);
     });
 
+  function mapLoaded(done) {
+    if (map.property('loaded')) {
+      return done();
+    }
+    map.on('load', done);
+  }
+
   function loaded(data) {
-    // TODO
+    var region = map.selectAll('g.region');
+    region.select('a')
+      .attr('xlink:href', function(d) {
+        if (d.properties.offshore) console.log(d.properties);
+        return [
+          '?',
+          d.properties.offshore ? 'offshore' : 'onshore',
+          '/',
+          encodeURIComponent(d.properties.name)
+        ].join('');
+      });
   }
 
 })(this);

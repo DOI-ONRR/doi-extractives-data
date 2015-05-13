a6d296d00d11a5c26e23f2596bbca8de91315c3f
Shawn Allen <shawn.allen@gsa.gov>
2015-05-13 00:29:26 -0700

    add eiti.url functions

--- js/eiti.js
+++ js/eiti.js
@@ -839,4 +839,117 @@
       .remove(_remove);
   }
 
+
+  /**
+   * URL mutation
+   */
+
+  eiti.url = {};
+
+  eiti.url.merge = function(href, data) {
+    return eiti.url.qs.merge(href, data);
+  };
+
+  eiti.url.qs = (function() {
+
+    var qs = function(data) {
+      return typeof data === 'string' ? qs.parse(data) : data || {};
+    };
+
+    // strings that are typically okay to include in the hash
+    qs.replacements = {
+      '%20': '+',
+      '%2C': ','
+    };
+
+    qs.encode = function(val) {
+      var replace = qs.replacements;
+      return encodeURIComponent(val)
+        .replace(/(\%[A-F0-9]{2})/g, function(_, hex) {
+          return hex in replace
+            ? replace[hex]
+            : hex;
+        });
+    };
+
+    qs.decode = function(str) {
+      return decodeURIComponent(str.replace(/\+/g, ' '));
+    };
+
+    // querystring.parse('?foo=a&baz=1') -> {foo: 'a', baz: 1}
+    qs.parse = function(str, separator) {
+      if (str.charAt(0) === '?') {
+        str = str.substr(1);
+      }
+
+      var query = {};
+      forEach(str.split(separator || '&'), function(bit) {
+        var parts = bit.split('=', 2),
+            key = qs.decode(parts[0]),
+            value = parts.length > 1
+              ? qs.decode(parts[1])
+              : true;
+        switch (value) {
+          case 'true':
+            value = true;
+            break;
+          case 'false':
+            value = false;
+            break;
+          case '':
+            break;
+          default:
+            var num = +value;
+            if (!isNaN(num)) value = num;
+        }
+        query[key] = value;
+      });
+
+      return query;
+    };
+
+    // querystring.format({foo: 'a', baz: 1}) -> '?foo=a&baz=1'
+    qs.format = function(obj, separator, sortKeys) {
+      var entries = [];
+      for (var key in obj) {
+        if (obj.hasOwnProperty(key) && typeof obj[key] !== 'function' && typeof obj[key] !== 'undefined') {
+          entries.push({key: key, value: String(obj[key])});
+        }
+      }
+      if (sortKeys) {
+        entries.sort(function(a, b) {
+          return a.key > b.key ? 1 : a.key < b.key ? -1 : 0;
+        });
+      }
+      return entries.length
+        ? entries.map(function(e) {
+            return [qs.encode(e.key), qs.encode(e.value)].join('=');
+          }).join(separator || '&')
+        : '';
+    };
+
+    qs.merge = function(url, data) {
+      var bits = url.split('?');
+      var query;
+      // if there's a query string...
+      if (bits.length > 1) {
+        query = qs.parse(bits[1]);
+        if (typeof data === 'string') {
+          data = qs.parse(data);
+        }
+        for (var key in data) {
+          query[key] = data[key];
+        }
+        query = qs.format(query);
+      } else {
+        query = (typeof data === 'string')
+          ? data
+          : qs.format(data);
+      }
+      return query ? [bits[0], query].join('?') : bits[0];
+    };
+
+    return qs;
+  })();
+
 })(this);

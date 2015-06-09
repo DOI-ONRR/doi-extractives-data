328b5f2f1e4f7320838b2d91dfc12091bbbfdc82
Shawn Allen <shawn.allen@gsa.gov>
2015-06-08 17:35:44 -0700

    more data, more page stubs

--- server.js
+++ server.js
@@ -1,28 +1,78 @@
 #!/usr/bin/env node
-var Festoon = require('festoon');
 var express = require('express');
 var cons = require('consolidate');
 var extend = require('extend');
-var app = express();
+var assert = require('assert');
+var Festoon = require('festoon');
 
-var basePath = __dirname;
+var helpers = require('./lib/server-helpers');
+var view = helpers.view;
+var redirect = helpers.redirect;
+var api = helpers.api;
+
+var app = express();
 
 // render html with nunjucks
 app.engine('html', cons.nunjucks);
 app.set('view engine', 'html');
-app.set('views', basePath + '/views');
+app.set('views', __dirname + '/views');
 
 var data = new Festoon({
-  path: basePath + '/output',
+  path: __dirname + '/output',
   sources: {
     // master resources (commodities) list, plus other metadata
     resources:  './data/commodities.json',
 
-    // national data sources
-    nationalRevenue:    'national/revenues-yearly.tsv',
-    nationalProduction: 'national/volumes-yearly.tsv',
+    locations: {
+      onshore: '#states',
+      offshore: '#offshoreAreas'
+    },
+
+    // revenues
+    nationalRevenue: {
+      onshore: '#stateRevenues',
+      offshore: '#offshoreRevenues'
+    },
+
+    // production volumes
+    nationalProduction: {
+      onshore: '#stateProduction',
+      offshore: '#offshoreProduction'
+    },
 
     // state data sources
+    states: './input/geo/states.csv',
+    state: {
+      // {{ state.meta.name }}
+      meta: Festoon.findByParam('states', 'state', 'abbr'),
+      // {{ state.revenues[] }}
+      revenues: Festoon.transform.filter('stateRevenues', function(d) {
+        return d.State === this.state;
+      }),
+      // {{ state.production[] }}
+      production: Festoon.transform.filter('stateProduction', function(d) {
+        return d.State === this.state;
+      })
+    },
+
+    // offshore planning areas
+    offshoreAreas: './input/geo/offshore/areas.tsv',
+    offshoreArea: {
+      // {{ area.meta.name }}
+      meta: Festoon.findByParam('offshoreAreas', 'area', 'id'),
+      // {{ area.revenues[] }}
+      // TODO: Area column should be a 3-letter ID, not name
+      revenues: Festoon.transform.filter('offshoreRevenues', function(d) {
+        return d.Area === this.area;
+      }),
+      // {{ area.production[] }}
+      // TODO: Area column should be a 3-letter ID, not name
+      production: Festoon.transform.filter('stateProduction', function(d) {
+        return d.Area === this.area;
+      })
+    },
+
+    // state (onshore) sources
     stateRevenues:      'state/revenues-yearly.tsv',
     stateProduction:    'state/volumes-yearly.tsv',
 
@@ -30,25 +80,47 @@ var data = new Festoon({
     countyRevenues:     'county/by-state/:state/revenues-yearly.tsv',
     allCountyRevenues:  'county/revenues-yearly.tsv',
 
-    // offshore (planning area) data sources
+    // offshore (planning area) sources
     offshoreRevenues:   'offshore/revenues-yearly.tsv',
-    offshoreProduction: 'offshore/volumes-yearly.tsv'
+    offshoreProduction: 'offshore/volumes-yearly.tsv',
   }
 });
 
-// common data
-app.use(data.decorate('resources'));
 
-var view = function(template, data) {
-  return function(req, res) {
-    return data
-      ? res.render(template, data)
-      : res.render(template);
-  };
-};
+// common data
+app.use(data.decorate(['resources', 'locations']));
+
+// static assets
+app.use('/static', express.static(__dirname + '/static'));
 
 // index
 app.get('/', view('index'));
+
+// locations page
+app.get('/locations',
+  view('locations'));
+
+app.get('/locations/onshore', redirect('/locations'));
+
+// state page
+app.get('/locations/onshore/:state.json',
+  data.decorate('state'),
+  api('state'));
+
+// state page
+app.get('/locations/onshore/:state/revenues.(csv|json)',
+  data.decorate('stateRevenues'),
+  api('stateRevenues', {filter: true}));
+
+// state page
+app.get('/locations/onshore/:state',
+  data.decorate('state'),
+  view('state'));
+
+// offshore area page
+app.get('/locations/offshore/:area',
+  data.decorate({area: 'offshoreArea'}),
+  view('offshore-area'));
 
 app.listen(process.env.PORT || 4000, function(error) {
   if (error) return console.error('error:', error);

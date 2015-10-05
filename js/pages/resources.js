ea85def2a9e52046393d4b73f44569ee13c0be7c
Shawn Allen <shawn.allen@gsa.gov>
2015-10-05 14:24:48 -0700

    add region and datatype navigation

--- js/pages/resources.js
+++ js/pages/resources.js
@@ -1,23 +1,83 @@
+
 var ResourceRouter = Backbone.Router.extend({
   routes: {
-    ':resource':                                        'resource',
-    ':resource/:datatype':                              'resource',
-    ':resource/:datatype/:region':                      'resource',
-    ':resource/:datatype/onshore/:region':              'resource',
-    ':resource/:datatype/onshore/:region/:subregion':   'resource',
-    ':resource/:datatype/offshore/:region':             'resource',
-    ':resource/:datatype/offshore/:region/:subregion':  'resource',
+    ':resource(/)':                                           'resource',
+    ':resource/:datatype(/)':                                 'resource',
+    ':resource/:datatype/:regiontype(/)':                     'resource',
+    ':resource/:datatype/:regiontype/:region(/)':             'resource',
+    ':resource/:datatype/:regiontype/:region/:subregion(/)':  'resource',
+    ':resource/:datatype/:regiontype/:region(/)':             'resource',
+    ':resource/:datatype/:regiontype/:region/:subregion(/)':  'resource',
   },
 
-  resource: function(resource, datatype, region, subregion) {
-    if (!resource) resource = 'all';
-    if (!datatype) datatype = 'revenue';
-    if (!region) region = 'US';
-    console.log('resource(', arguments, '):', {
-      resource: resource,
-      datatype: datatype,
+  initialize: function(options) {
+    this.regionSelector = document.querySelector('#region-selector');
+    this.regionSelector.addEventListener('change', this.onRegionChange.bind(this));
+    this.dataTypeSelector = document.querySelector('#datatype-selector');
+    this.dataTypeSelector.addEventListener('change', this.onDataTypeChange.bind(this));
+    this.subregionSelector = document.querySelector('#subregion-selector');
+  },
+
+  navigateToParameters: function(params) {
+    params = _.extend(this.params, params);
+    var path = [
+      params.resource || 'all',
+      params.datatype || 'revenue',
+      params.regiontype || 'US'
+    ];
+    if (params.region) {
+      path.push(params.region);
+      if (params.subregion) {
+        path.push(params.subregion);
+      }
+    }
+    var url = path.join('/');
+    this.navigate(url);
+  },
+
+  resource: function(resource, datatype, regiontype, region, subregion) {
+    var params = {
+      resource: resource || 'all',
+      datatype: datatype || 'revenue',
+      regiontype: regiontype || 'US',
       region: region,
       subregion: subregion
+    };
+    this.params = params;
+    console.log('resource():', params);
+    this.updateSelectors(params);
+  },
+
+  updateSelectors: function(params) {
+    this.updateRegionSelector(params);
+    this.updateDataTypeSelector(params);
+  },
+
+  updateRegionSelector: function(params) {
+    var region = params.region
+      ? [params.regiontype, params.region].join('/')
+      : params.regiontype;
+    console.log('selected region:', region);
+    this.regionSelector.value = region;
+  },
+
+  onRegionChange: function(e) {
+    var value = this.regionSelector.value;
+    var parts = value.split('/');
+    var params = {
+      regiontype: parts[0],
+      region: parts[1]
+    };
+    this.navigateToParameters(params);
+  },
+
+  updateDataTypeSelector: function(params) {
+    this.dataTypeSelector.value = params.datatype;
+  },
+
+  onDataTypeChange: function(e) {
+    this.navigateToParameters({
+      datatype: this.dataTypeSelector.value
     });
   }
 });

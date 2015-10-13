258656e0a32ff29899fa866d661440379bc59e3b
gemfarmer <bshslowfood@gmail.com>
2015-10-13 17:02:11 -0500

    change communties page for active nav

--- js/pages/communities.js
+++ js/pages/communities.js
@@ -1,13 +1,12 @@
-
-  (function(exports) {
-    var scrollLeft, scrollTop;
+(function(exports) {
+    var scrollLeft, 
+      scrollTop, 
+      sections;
     var findScrollPositions = function(){
       scrollLeft = (window.pageXOffset !== undefined) ? window.pageXOffset : (document.documentElement || document.body.parentNode || document.body).scrollLeft;
       scrollTop = (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
     };
     var communitiesContent = document.getElementsByClassName("communities-content")[0];
-    var csn = document.getElementsByClassName("communities-sticky-nav")
-    var child = csn
     
 
 
@@ -17,42 +16,54 @@
         communitiesNavItems[i].classList.remove('active');
       }
     }
-    var addActive = function(){
-      console.log('add active')
-      var communitiesNavItems = document.getElementsByClassName("communities-nav-item")
-      for (var i = 0; i < communitiesNavItems.length; i++) {
-        var item = communitiesNavItems[i];
-        item.addEventListener('click', function () {
-          console.log('click')
-          removeActive()
-           this.classList.add('active');
-        });
+    var addActive = function(name){
+      if (name){
+        removeActive();
+        var navItem = document.querySelector(('.'+name));
+        navItem.classList.add('active');
       }
     }
-    window.onload = function(){
-      addActive()
+
+    var communitiesNavItems = document.getElementsByClassName("communities-nav-item");
+    for (var i = 0; i < communitiesNavItems.length; i++) {
+      var item = communitiesNavItems[i];
+      item.addEventListener('click', function () {
+        removeActive();
+        this.classList.add('active');
+      });
     }
-    
-    
 
+    
+    var setPageSections = function(){
+      var communitiesSections = document.getElementsByClassName("communities-section"),
+        sections = [];
+      for (var i = 0; i < communitiesSections.length; i++) {
+        var section = communitiesSections[i];
+        sections.push({
+          name : section.getAttribute('name'),
+          top : section.offsetTop - section.offsetHeight + communitiesContent.offsetTop
+        });
+      }
+      return sections;
+    }
+    sections = setPageSections();
+    var chooseNavByScroll = function(){
 
+      for (var i = 0; i < sections.length; i++) {
+        var section = sections[i];
+        if(scrollTop >= section.top){
+          addActive(section.name);
+        }
+      }
+    }
 
     var setStickyPos = function () {
       var stickyNav = document.getElementsByClassName("communities-sticky-nav")[0],
           ccOffsetTop = communitiesContent.offsetTop;
-      // console.log(csn, communitiesNavItems, communitiesNavItems[0])
-      addActive()
 
-      // for (var i = 0; i < communitiesNavItems.length; i++) {
-      //   item = communitiesNavItems[i];
-      //   console.log(item)
-      //   var href = item.getAttribute('href')
-      //   console.log(href)
-      // };
-        
       // scrollDifference is the difference in height between the location of the top of the window and the top of the 'communities-content' div
       var scrollDifference = scrollTop - ccOffsetTop;
-      
+
       var combinedOffset = ccOffsetTop + communitiesContent.offsetHeight;
       var stickyNavHeight = stickyNav.clientHeight;
       var ccPadding = 50; // set value at all widths (I believe)
@@ -79,5 +90,10 @@
     window.onscroll = function() {
         findScrollPositions();
         setStickyPos();
+        chooseNavByScroll();
     };
+    window.onresize = function(){
+      sections = setPageSections();
+      chooseNavByScroll();
+    }
   })();

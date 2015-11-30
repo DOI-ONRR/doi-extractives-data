fe6c869d30aeea69e854e299a5ed1dc965767027
gemfarmer <bshslowfood@gmail.com>
2015-11-30 11:55:54 -0600

    adds stickynav.js

---
+++ js/components/sticky-nav.js
@@ -0,0 +1,72 @@
+(function(exports) {
+
+  var scrollLeft,
+    scrollTop,
+    sections;
+
+  var findScrollPositions = function(){
+    scrollLeft = (window.pageXOffset !== undefined)
+      ? window.pageXOffset
+      : (document.documentElement
+        || document.body.parentNode
+        || document.body).scrollLeft;
+    scrollTop = (window.pageYOffset !== undefined)
+      ? window.pageYOffset
+      : (document.documentElement
+        || document.body.parentNode
+        || document.body).scrollTop;
+  };
+
+  console.log('in', window)
+
+
+  StickyNav = function() {
+    this.elems = {
+      sticky : document.querySelector('.sticky_nav'),
+      main: document.querySelector('main')
+    }
+    this.height = this.elems.sticky.clientHeight;
+    this.offset = this.elems.sticky.offsetTop
+  };
+
+  StickyNav.prototype = {
+    setPositions : function () {
+      this.mainOffset = this.elems.main.offsetTop;
+      this.mainHeight = this.elems.main.clientHeight;
+      this.diffTop = scrollTop - this.mainOffset - this.offset;
+      this.diffBottom = scrollTop + this.height - this.mainHeight - this.mainOffset
+    },
+    update: function(){
+      if (this.diffTop >= 0){
+        this.elems.sticky.style.position = 'fixed';
+        this.elems.sticky.style.top = 0;
+        if (this.diffBottom >= 0){
+          this.elems.sticky.style.position = 'absolute';
+          this.elems.sticky.style.top = this.mainHeight - this.height - 50 + 'px';
+        } else {
+          this.elems.sticky.style.position = 'fixed';
+          this.elems.sticky.style.top = 0;
+        }
+      } else {
+        this.elems.sticky.style.position = 'static';
+      }
+    }
+  }
+
+  stickyNav = new StickyNav();
+
+  findScrollPositions();
+  stickyNav.setPositions();
+  stickyNav.update();
+
+  window.addEventListener('scroll', function() {
+    findScrollPositions();
+    stickyNav.setPositions();
+    stickyNav.update();
+  });
+
+
+  exports.stickyNav = stickyNav;
+
+
+})(this);

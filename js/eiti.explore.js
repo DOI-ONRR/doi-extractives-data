5fd712742debef0d02cabb028ecb7979cab72f3b
Shawn Allen <shawn.allen@gsa.gov>
2015-12-03 16:24:49 -0800

    first stab at company revenue display

---
+++ js/eiti.explore.js
@@ -0,0 +1,224 @@
+(function(eiti) {
+
+  eiti.explore = {};
+
+  /**
+   * @class stateManager
+   * The state manager helps you manage application state, using an
+   * Immutable.Map under the hood.
+   *
+   * @example
+   * var manager = eiti.explore.stateManager()
+   *   .on('change', function(state) {
+   *   })
+   *   .set('foo', 'bar');
+   */
+  eiti.explore.stateManager = function() {
+
+    var state = new Immutable.Map();
+    var updated = false;
+    var mutating = false;
+
+    var manager = {};
+    var dispatch = d3.dispatch('change');
+
+    /**
+     * Raw state mutation with a function, which should take an
+     * Immutable.Map and return either the same one or a mutated
+     * instance:
+     *
+     * @example
+     * manager.mutate(function(state) {
+     *   return state.set('x', 1);
+     * });
+     *
+     * @param Function mutator
+     */
+    manager.mutate = function(fn) {
+      mutateState(fn);
+      return manager;
+    };
+
+    /**
+     * set a single key
+     * @param String key
+     * @param {*} value
+     */
+    manager.set = function(key, value) {
+      mutateState(function(state) {
+        return state.set(key, value);
+      });
+      return manager;
+    };
+
+    /**
+     * merge the keys of the provided object
+     * @param Object keys
+     */
+    manager.merge = function(keys) {
+      mergeState(keys);
+      return manager;
+    };
+
+    /**
+     * Provide a validation function for your state before 'change'
+     * events are updated. This should return an Immutable.Map, e.g.
+     *
+     * @example
+     * manager.validate(function(state) {
+     *   if (!state.has('foo')) {
+     *     return state.set('foo', 'default foo');
+     *   }
+     *   return state;
+     * });
+     *
+     * @param Function validator
+     */
+    manager.validate = function(validator) {
+      validateState = validator;
+      return manager;
+    };
+
+    /**
+     * Initialize the manager state with an optional Object literal.
+     *
+     * @param {Object}? initial
+     */
+    manager.init = function(initial) {
+      var previous = state;
+      if (initial) {
+        mergeState(initial) || update(state, previous);
+      } else {
+        update(state, null);
+      }
+      return manager;
+    };
+
+    function update(state, previous) {
+      dispatch.change(state, previous, updated);
+      updated = true;
+    }
+
+    // mutate the state and update if the state has changed
+    function mutateState(fn) {
+      mutating = true;
+      var previous = state;
+      state = fn(state) || new Immutable.Map();
+      if (!Immutable.is(state, previous)) {
+        state = validateState(state, previous, updated);
+        update(state, previous);
+        mutating = false;
+        return true;
+      }
+      mutating = false;
+      return false;
+    }
+
+    function mergeState(keys) {
+      return mutateState(function(state) {
+        return state.merge(keys);
+      });
+    }
+
+    // the default state validator is a noop
+    function validateState(state, previous, updated) {
+      return state;
+    }
+
+    return d3.rebind(manager, dispatch, 'on');
+  };
+
+  eiti.explore.hash = function() {
+    var dispatch = d3.dispatch('change');
+
+    var hash = {};
+    var writing = false;
+
+    hash.read = function() {
+      if (!location.hash) return {};
+      var str = location.hash.substr(1);
+      return eiti.url.qs.parse(str);
+    };
+
+    hash.write = function(data) {
+      writing = true;
+      location.hash = data ? eiti.url.qs.format(data) : '';
+      writing = false;
+    };
+
+    function change(e) {
+      var y = window.scrollY;
+      if (writing) {
+        return;
+      }
+      dispatch.change(hash.read());
+      // prevent scrolling
+      e.preventDefault();
+      window.scrollY = y;
+    }
+
+    window.addEventListener('hashchange', change);
+
+    return d3.rebind(hash, dispatch, 'on');
+  };
+
+  eiti.explore.model = function(url) {
+    var model = {};
+    var dispatch = d3.dispatch('prefilter', 'postfilter');
+    var getDataURL = d3.functor(url);
+    var transform;
+    var req;
+
+    var filters = [];
+
+    model.load = function(state, done) {
+      if (req) {
+        req.abort();
+      }
+      var url = getDataURL(state);
+      // console.log('model.load():', url);
+      req = eiti.load(url, function(error, data) {
+        if (error) {
+          data = [];
+        }
+        if (transform) {
+          data.forEach(transform);
+        }
+        applyFilters(data, state, done);
+      });
+      return req;
+    };
+
+    model.filter = function(stateKey, filter) {
+      filters.push({
+        key: stateKey,
+        func: filter
+      });
+      return model;
+    };
+
+    model.transform = function(fn) {
+      if (arguments.length) {
+        transform = fn;
+        return model;
+      }
+      return transform;
+    };
+
+    function applyFilters(data, state, done) {
+      filters.forEach(function(filter) {
+        var value = state.get(filter.key);
+        dispatch.prefilter(filter.key, data);
+        if (value || value === 0) { // XXX
+          data = filter.func(data, value, filter.key);
+          dispatch.postfilter(filter.key, data);
+        }
+      });
+
+      done(null, data);
+    }
+
+    return d3.rebind(model, dispatch, 'on');
+  };
+
+})(eiti);
---
+++ js/pages/company-revenue.js
@@ -0,0 +1,324 @@
+(function() {
+  'use strict';
+
+  var root = d3.select('#companies');
+  var filterToggle = root.select('button.toggle-filters');
+  var revenueTypeList = root.select('#revenue-types');
+  var companyList = root.select('#companies');
+
+  var getter = eiti.data.getter;
+  var formatNumber = eiti.format.dollars;
+  var REVENUE_TYPE_PREFIX = /^[A-Z]+(\/[A-Z]+)?\s+-\s+/;
+
+  var groupByRevenueType = d3.nest()
+    .key(getter('revenueType'))
+    .rollup(function(d) {
+      return d3.sum(d, getter('Revenue'));
+    })
+    .sortValues(function(a, b) {
+      return d3.descending(+a.Revenue, +b.Revenue);
+    });
+
+  var state = eiti.explore.stateManager()
+    .on('change', update);
+
+  var hash = eiti.explore.hash()
+    .on('change', state.merge);
+
+  var model = eiti.explore.model(function(state) {
+      return eiti.data.path + 'company/revenue.tsv';
+    })
+    .transform(function(d) {
+      removeRevenueTypePrefix(d);
+    })
+    .filter('commodity', function(data, commodity) {
+      return data.filter(function(d) {
+        return d.Commodity === commodity;
+      });
+    })
+    .filter('type', function(data, type) {
+      return data.filter(function(d) {
+        return d.revenueType === type;
+      });
+    })
+    .on('prefilter', function(key, data) {
+      if (key === 'commodity') {
+        updateCommoditySelector(data);
+        updateRevenueTypeSelector(data);
+      } else if (key === 'type') {
+      }
+    });
+
+  var filters = root.selectAll('.filters [name]')
+    .on('change.state', function() {
+      state.set(this.name, this.value);
+    });
+
+  var initialState = hash.read();
+  if (Object.keys(initialState).length) {
+    filterToggle.attr('aria-expanded', true);
+  }
+
+  state.init(initialState);
+
+  function update(state) {
+    var query = state.toJS();
+    console.log('update:', query);
+    hash.write(query);
+
+    updateFilterDescription(state);
+
+    model.load(state, function(error, data) {
+      if (error) {
+        console.error('error:', error);
+        data = [];
+      }
+
+      filters.each(function() {
+        this.value = state.get(this.name) || '';
+      });
+
+      render(data, state);
+    });
+  }
+
+  function render(data, state) {
+    console.log('rendering %d rows', data.length, data[0]);
+    updateRevenueTypes(data);
+    updateCompanyList(data);
+  }
+
+  function updateCommoditySelector(data) {
+    var commodities = d3.nest()
+      .key(getter('Commodity'))
+      .entries(data)
+      .map(getter('key'))
+      .sort(d3.ascending);
+    var input = root.select('#commodity-selector');
+    var options = input.selectAll('option.value')
+      .data(commodities, identity);
+    options.enter().append('option')
+      .attr('class', 'value')
+      .attr('value', identity)
+      .text(identity);
+  }
+
+  function updateRevenueTypeSelector(data) {
+    var commodities = d3.nest()
+      .key(getter('revenueType'))
+      .entries(data)
+      .map(getter('key'))
+      .sort(d3.ascending);
+    var input = root.select('#type-selector');
+    var options = input.selectAll('option.value')
+      .data(commodities, identity);
+    options.enter().append('option')
+      .attr('class', 'value')
+      .attr('value', identity)
+      .text(identity);
+  }
+
+  function updateCommodities(data) {
+    var commodities = d3.nest()
+      .key(getter('Commodity'))
+      .entries(data)
+      .map(getter('key'))
+      .sort(d3.ascending);
+    var input = root.select('#commodity-selector');
+    var options = input.selectAll('option.value')
+      .data(commodities, identity);
+    options.enter().append('option')
+      .attr('class', 'value')
+      .attr('value', identity)
+      .text(identity);
+  }
+
+  function updateRevenueTypes(data) {
+    var types = groupByRevenueType.entries(data)
+      .map(function(d) {
+        return {
+          label: d.key,
+          value: d.values
+        };
+      });
+
+    var items = revenueTypeList.selectAll('.revenue-type')
+      .data(types, getter('label'));
+
+    items.exit().remove();
+    items.enter().append('tr')
+      .attr('class', 'revenue-type')
+      .call(setupRevenueItem);
+
+    var max = d3.max(types, getter('value'));
+    items
+      .call(updateRevenueItem, max)
+      .sort(function(a, b) {
+        return d3.descending(a.value, b.value);
+      });
+  }
+
+  function updateCompanyList(data) {
+
+    var companies = d3.nest()
+      .key(getter('Company'))
+      .entries(data)
+      .map(function(d) {
+        return {
+          name: d.key,
+          total: d3.sum(d.values, getter('Revenue')),
+          types: groupByRevenueType.entries(d.values)
+            .map(function(d) {
+              return {
+                label: d.key,
+                value: d.values
+              };
+            })
+        };
+      });
+
+    var rows = companyList.selectAll('tr.company')
+      .data(companies, getter('name'));
+
+    rows.exit().remove();
+    var enter = rows.enter().append('tr')
+      .attr('class', 'company')
+      .call(setupCompanyRow);
+
+    enter.select('.name').text(getter('name'));
+
+    var max = d3.max(companies, getter('total'));
+    console.log('company max $:', max);
+    rows
+      .call(updateCompanyRow, max)
+      .sort(function(a, b) {
+        return d3.descending(a.total, b.total);
+      });
+  }
+
+  function setupCompanyRow(selection) {
+    selection.append('td')
+      .attr('class', 'name');
+    var valueCell = selection.append('td')
+      .attr('class', 'revenue');
+
+    /*
+    var total = valueCell.append('div')
+      .attr('class', 'revenue-total')
+      .call(setupCompanyRevenueItem);
+    total.select('.label')
+      .text(' total');
+    */
+
+    valueCell.append('div')
+      .attr('class', 'revenue-types');
+  }
+
+  function updateCompanyRow(selection, max) {
+    /*
+    var total = selection.select('.revenue-total');
+    total.select('eiti-bar')
+      .attr('max', max)
+      .attr('value', getter('total'));
+    total.select('span.value')
+      .text(function(d) {
+        return formatNumber(d.total);
+      });
+    */
+
+    var types = selection.select('.revenue-types')
+      .selectAll('.revenue-type')
+      .data(getter('types'), getter('label'));
+
+    types.exit().remove();
+    types.enter().append('div')
+      .call(setupCompanyRevenueItem);
+
+    types
+      .call(updateCompanyRevenueItem, max)
+      .sort(function(a, b) {
+        return d3.descending(a.value, b.value);
+      });
+  }
+
+  function setupCompanyRevenueItem(selection) {
+    selection.append('eiti-bar');
+    selection.append('span')
+      .attr('class', 'value');
+    selection.append('span')
+      .attr('class', 'label');
+  }
+
+  function updateCompanyRevenueItem(selection) {
+    selection.select('eiti-bar')
+      .attr('max', max)
+      .attr('value', getter('value'));
+
+    selection.select('span.value')
+      .text(function(d) {
+        return formatNumber(d.value);
+      });
+
+    /*
+    selection.select('.label')
+      .text(getter('label'));
+    */
+  }
+
+  function setupRevenueItem(selection) {
+    selection.append('td')
+      .attr('class', 'label');
+    selection.append('td')
+      .attr('class', 'value');
+    selection.append('td')
+      .attr('class', 'bar')
+      .append('eiti-bar');
+  }
+
+  function updateRevenueItem(selection, max) {
+    selection.select('.label')
+      .text(getter('label'));
+
+    selection.select('.value')
+      .text(function(d) {
+        return formatNumber(d.value);
+      });
+
+    var bar = selection.select('eiti-bar')
+      .attr('value', getter('value'));
+    if (max) {
+      bar.attr('max', max);
+    }
+  }
+
+  function updateFilterDescription(state) {
+    var desc = root.select('#filter-description');
+
+    var data = {
+      type: state.get('type') || 'All revenue',
+      commodity: (state.get('commodity') || 'all resource').toLowerCase(),
+    };
+
+    /*
+    if (data.commodity === 'N/A') {
+      data.commodity = 'no applicable';
+    }
+    */
+
+    desc.selectAll('[data-key]')
+      .text(function() {
+        return data[this.getAttribute('data-key')];
+      });
+  }
+
+  function removeRevenueTypePrefix(row) {
+    if (!row.revenueType) {
+      row.revenueType = row['Revenue Type'].replace(REVENUE_TYPE_PREFIX, '');
+    }
+  }
+
+  function identity(d) {
+    return d;
+  }
+
+})(this);

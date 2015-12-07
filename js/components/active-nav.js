40b2dc99646f4f5a7777a3a93f6dbd5665fcb660
gemfarmer <bshslowfood@gmail.com>
2015-12-07 16:30:44 -0600

    works scrolling down

--- js/components/active-nav.js
+++ js/components/active-nav.js
@@ -1,8 +1,4 @@
 (function(exports) {
-    var scrollLeft,
-      scrollTop,
-      sections;
-
     var getScrollTop = function(){
       return scrollTop = (window.pageYOffset !== undefined)
         ? window.pageYOffset
@@ -12,19 +8,20 @@
     };
 
     function isElementInViewport(el) {
-        var rect = el.getBoundingClientRect();
+      console.log(el)
+      var rect = el.getBoundingClientRect();
 
-        return rect.bottom > 0 &&
-            rect.right > 0 &&
-            rect.left < (window.innerWidth || document. documentElement.clientWidth) /*or $(window).width() */ &&
-            rect.top < (window.innerHeight || document. documentElement.clientHeight) /*or $(window).height() */;
+      return rect.bottom > 0 &&
+          rect.right > 0 &&
+          rect.left < (window.innerWidth || document. documentElement.clientWidth) /*or $(window).width() */ &&
+          rect.top < (window.innerHeight || document. documentElement.clientHeight) /*or $(window).height() */;
     }
 
     var ActiveNav = function() {
-
-      // console.log(window.location.hash)
+      // init ActiveNav Properties
       this.active = window.location.hash || '#intro';
       this.navItems = document.querySelectorAll('[data-nav-item]');
+      this.navHeaders = document.querySelectorAll('[data-nav-header]');
       // this.scrollTop = {
       //   current: getScrollTop(),
       //   prev: getScrollTop()
@@ -53,18 +50,25 @@
       }
     };
 
-    ActiveNav.prototype.addActive = function(el){
-      this.active = el.getAttribute('data-na-item');
-      el.setAttribute('data-active', true);
+    ActiveNav.prototype.addActive = function(el, name){
+      if (!el){
+        el = document.querySelector('[data-nav-item="' + name + '"]');
+        this.active = name;
+        el.setAttribute('data-active', true);
+      } else {
+        this.active = el.getAttribute('data-nav-item');
+        el.setAttribute('data-active', true);
+      }
     };
 
-    ActiveNav.prototype.update = function(el){
+    ActiveNav.prototype.update = function(el, name){
       this.removeActive();
-      this.addActive(el);
+      this.addActive(el, name);
     }
 
     var activeNav = new ActiveNav();
 
+    // init click handlers
     for (var i = 0; i < activeNav.navItems.length; i++) {
       var item = activeNav.navItems[i];
       item.addEventListener('click', function () {
@@ -72,40 +76,28 @@
       });
     }
 
-    var chooseNavByScroll = function(){
-      //
+    ActiveNav.prototype.chooseNavByScroll = function(){
+      // console.log(typeof(this.navHeaders))
 
-      var navHeaders = document.querySelectorAll('[data-nav-header]');
-      // console.log('navHeaders', navHeaders, typeof(navHeaders))
+      Array.prototype.forEach.call(this.navHeaders, function(header){
 
-      Array.prototype.forEach.call(navHeaders, function(header){
-        // console.log('header', header)
-        var inViewPort = isElementInViewport(header)
-        // console.log(header.name, inViewPort)
+        var inViewPort = isElementInViewport(header);
         if (inViewPort) {
-          activeNav.active = header.name;
-          removeActive();
-          addActive(header.name);
+          activeNav.update(null, header.name);
         }
 
       });
-
-      // console.log(activeNav)
-
     };
-
-
-
 
     window.addEventListener('scroll', function() {
       // console.log('scroll')
-      // chooseNavByScroll();
+      activeNav.chooseNavByScroll();
     });
 
     window.addEventListener('resize', function(){
       // console.log('resize')
       // sections = setPageSections();
-      chooseNavByScroll();
+      activeNav.chooseNavByScroll();
     });
 
 

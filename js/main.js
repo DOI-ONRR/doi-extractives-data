95ee6d64e3a382fb82a1e72d17555854f109b56d
gemfarmer <bshslowfood@gmail.com>
2015-12-29 13:04:59 -0600

    added documentation

--- js/main.js
+++ js/main.js
@@ -1,36 +1,38 @@
-// // 3rd party dependencies
-// require("./vendor/d3.v3.min.js");
-// require("./vendor/colorbrewer.js");
-// require("./vendor/jquery.min.js");
+// CommonJS/RequireJS module requirements:
+// http://webpack.github.io/docs/commonjs.html
 
-// // custom map projection
-// require("./albers-custom.js");
+// 3rd party dependencies
+d3 = require("./vendor/d3.v3.min.js");
+queue = require("./vendor/queue.v1.min.js");
+topojson = require("./vendor/topojson.v1.min.js");
+datalib = require("./vendor/datalib.min.js");
+colorbrewer = require("./vendor/colorbrewer.js");
+$ = jQuery = require("./vendor/jquery.min.js");
 
-// // EITI libs
-require("./eiti.js");
+// custom map projection
+albersCustom = require("./albers-custom.js");
 
-// // custom elements
+// EITI
+eiti = require("./eiti.js");
+
+// custom elements
 require("./vendor/document-register-element.js");
 require("./components/eiti-map.js");
 require("./components/eiti-slider.js");
 require("./components/eiti-toggle.js");
 require("./components/search.js");
 
-console.log(eiti, '--', window, '--', exports)
-
 // Glossary
-// require("./vendor/lodash.min.js");
-// require("./vendor/list.min.js");
-// require("./components/glossary.js");
+_ = require("./vendor/lodash.min.js");
+List = require("./vendor/list.min.js");
+require("./components/glossary.js");
 
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-// <script src="{{ site.baseurl }}/js/eiti.js"></script>
-//     <script>
-//       eiti.data.path = '{{ site.baseurl }}/data/';
-//       eiti.commodities = ({{ site.data.commodities|jsonify }});
-//       eiti.resource = ({{ site.data.resource|jsonify }});
-//     </script>
-
-
-// currently eiti and List are experiencing issues with CommonJS/RequireJS module requirement:
-// http://webpack.github.io/docs/commonjs.html
+  ga('create', 'UA-48605964-8', 'auto');
+  ga('set', 'anonymizeIp', true);
+  ga('set', 'forceSSL', true);
+  ga('send', 'pageview');

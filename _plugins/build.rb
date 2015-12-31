9ce56276f7acd348a29440d2ce391fe0a035fc5c
gemfarmer <bshslowfood@gmail.com>
2015-12-31 14:27:09 -0600

    minify

--- _plugins/build.rb
+++ _plugins/build.rb
@@ -2,7 +2,6 @@
 
 production = ENV['BRANCH'] == 'master' or ENV['BRANCH'] == 'staging' or ENV['BRANCH'] == 'js-optimization'
 
-# ENV['NODE_ENV'] = if production then 'prod' else 'dev' end
-ENV['NODE_ENV'] = 'prod'
+ENV['NODE_ENV'] = if production then 'prod' else 'dev' end
 
 `npm webpack`

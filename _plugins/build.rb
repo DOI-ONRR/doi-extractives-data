3f461df7aab18a1914ab5bbc80a54444c1ad26c3
gemfarmer <bshslowfood@gmail.com>
2015-12-31 14:12:28 -0600

    test branch

--- _plugins/build.rb
+++ _plugins/build.rb
@@ -1,6 +1,6 @@
 `npm install`
 
-production = ENV['BRANCH'] == 'master' or ENV['BRANCH'] == 'staging'
+production = ENV['BRANCH'] == 'master' or ENV['BRANCH'] == 'staging' or ENV['BRANCH'] == 'js-optimization'
 
 ENV['NODE_ENV'] = if production then 'prod' else 'dev' end
 

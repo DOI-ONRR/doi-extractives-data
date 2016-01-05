c35956d1ac9aaf2ffe6657081125e6b18ec58a38
Shawn Allen <shawn.allen@gsa.gov>
2016-01-05 14:02:35 -0800

    reorganize

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,28 @@
-var webpack = require('webpack'),
-	minify = process.env.NODE_ENV !== 'dev';
+var webpack = require('webpack');
+var minify = process.env.NODE_ENV !== 'dev';
+
+var plugins = [];
+if (minify) {
+  plugins.push(new webpack.optimize.UglifyJsPlugin({minimize: true}));
+}
 
 module.exports = {
 
   entry: {
-  	'main.min': './js/src/main.js',
-  	'narrative.min': './js/src/narrative.js',
-  	'explore.min': './js/src/explore.js',
-  	'homepage.min': './js/src/homepage.js',
+    'main.min': './js/src/main.js',
+    'narrative.min': './js/src/narrative.js',
+    'explore.min': './js/src/explore.js',
+    'homepage.min': './js/src/homepage.js',
   },
+
   devtool: 'source-map',
+
   output: {
     path: './js/lib',
     filename: '[name].js',
     chunkFilename: '[id].js'
   },
-  plugins: minify ? [
-    new webpack.optimize.UglifyJsPlugin({minimize: true})
-  ] : []
+
+  plugins: plugins
 
 };

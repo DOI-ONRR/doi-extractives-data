9396390890b5f856c5a7abdb1a445bc332e5d82e
Shawn Allen <shawn.allen@gsa.gov>
2016-01-05 14:04:05 -0800

    reorganize main.js

--- js/src/explore.js
+++ js/src/explore.js
@@ -1,3 +1,10 @@
-require('./../components/sticky-nav.js');
-Immutable = require('./../vendor/immutable.min.js');
-EITIBar = require('./../components/eiti-bar.js');
+(function(exports) {
+  'use strict';
+
+  // FIXME: does this export anything?
+  require('./../components/sticky-nav.js');
+
+  exports.Immutable = require('immutable');
+  exports.EITIBar = require('./../components/eiti-bar.js');
+
+})(window);

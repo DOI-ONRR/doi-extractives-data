7ba782e6c79aba205b3f57cbfdcc36bffaffdba0
Shawn Allen <shawn.allen@gsa.gov>
2016-01-05 14:04:05 -0800

    reorganize main.js

--- js/src/main.js
+++ js/src/main.js
@@ -1,39 +1,52 @@
 // CommonJS/RequireJS module requirements:
 // http://webpack.github.io/docs/commonjs.html
+(function(exports) {
+  'use strict';
 
-// 3rd party dependencies
-d3 = require('./../vendor/d3.v3.min');
-queue = require('./../vendor/queue.v1.min');
-topojson = require('./../vendor/topojson.v1.min');
-datalib = require('./../vendor/datalib.min');
-colorbrewer = require('./../vendor/colorbrewer');
-$ = jQuery = require('./../vendor/jquery.min');
+  // 3rd party dependencies
+  var d3 = require('d3');
+  exports.d3 = d3;
 
-// Custom map projection
-albersCustom = require('./../albers-custom');
+  // Custom map projection
+  d3.geo.albersCustom = require('../albers-custom');
 
-// EITI
-eiti = require('./../eiti');
+  exports.queue = require('queue-async');
+  exports.topojson = require('topojson');
+  exports.colorbrewer = require('colorbrewer');
+  exports.$ = exports.jQuery = require('jquery');
 
-// custom elements
-registerElement = require('./../vendor/document-register-element');
-EITIMap = require('./../components/eiti-map');
-EITISlider = require('./../components/eiti-slider');
-EITIToggle = require('./../components/eiti-toggle');
-require('./../components/search');
+  // EITI
+  // TODO: require explicit dependencies (such as d3 and queue) in eiti.js
+  // rather than here
+  exports.eiti = require('../eiti');
 
-// Glossary
-_ = require('./../vendor/lodash.min');
-List = require('./../vendor/list.min');
-require('./../components/glossary');
+  // custom elements
+  require('document-register-element');
 
-// Google Analytics
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+  exports.EITIMap = require('../components/eiti-map');
+  exports.EITISlider = require('../components/eiti-slider');
+  exports.EITIToggle = require('../components/eiti-toggle');
+
+  // FIXME: does this export anything?
+  require('../components/search');
+
+  // Glossary
+  exports._ = require('lodash');
+  exports.List = require('../vendor/list');
+  // FIXME: does this export anything?
+  require('../components/glossary');
+
+  // Google Analytics
+  /* jshint ignore:start */
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
   ga('create', 'UA-48605964-8', 'auto');
   ga('set', 'anonymizeIp', true);
   ga('set', 'forceSSL', true);
   ga('send', 'pageview');
+  /* jshint ignore:end */
+
+})(window);

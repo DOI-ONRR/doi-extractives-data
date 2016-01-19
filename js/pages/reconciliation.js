f64e015e092803815b0f369ae2998da75adc3ff1
gemfarmer <bshslowfood@gmail.com>
2016-01-19 15:08:06 -0700

    glossary link is clickable

--- js/pages/reconciliation.js
+++ js/pages/reconciliation.js
@@ -220,26 +220,26 @@
 
       });
 
-    var hasChildren = companyList.select('.list-heading')[0][0];
+    // var hasChildren = companyList.select('.list-heading')[0][0];
 
-    if (!hasChildren) {
-      var heading = companyList
-        .append('thead')
-        .attr('class', 'list-heading')
-        .append('tr')
-      heading.append('th')
-        .attr('class', 'narrow')
-        .text('')
-      heading.append('th')
-        .html(function(d) {
-          return 'amount reported by company (<strong>co</strong>) and by government (<strong>gov</strong>)';
-        });
-      heading.append('th')
-        .attr('class', 'centered')
-        .html(function(d) {
-          return 'variance (<span class="red">red</span> indicates <span class="term term-p" data-term="material variance" title="Click to define" tabindex="0">material var.<i class="icon-book"></i></span>)';
-        });
-    }
+    // if (!hasChildren) {
+    //   var heading = companyList
+    //     .append('thead')
+    //     .attr('class', 'list-heading')
+    //     .append('tr')
+    //   heading.append('th')
+    //     .attr('class', 'narrow')
+    //     .text('')
+    //   heading.append('th')
+    //     .html(function(d) {
+    //       return 'amount reported by company (<strong>co</strong>) and by government (<strong>gov</strong>)';
+    //     });
+    //   heading.append('th')
+    //     .attr('class', 'centered')
+    //     .html(function(d) {
+    //       return 'variance (<span class="red">red</span> indicates <span class="term term-p" data-term="material variance" title="Click to define" tabindex="0">material var.<i class="icon-book"></i></span>)';
+    //     });
+    // }
 
     var items = companyList.selectAll('tbody.company')
       .data(companies, getter('name'));

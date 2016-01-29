9f28ca1754d8aabebfc8c084425815e5fa97f5da
Shawn Allen <shawn.allen@gsa.gov>
2016-01-29 11:44:45 -0800

    use double quotes to appease the @houndci gods

--- _plugins/build.rb
+++ _plugins/build.rb
@@ -1,9 +1,9 @@
-# TODO: remove 'federalist-js' before merge
-production_branches = ['master', 'staging', 'federalist-js']
-branch = ENV['BRANCH']
+# TODO: remove "federalist-js" before merge
+production_branches = ["master", "staging", "federalist-js"]
+branch = ENV["BRANCH"]
 
 if production_branches.include?(branch)
-  puts '[build.rb] webpacking for production'
-  ENV['NODE_ENV'] = 'production'
+  puts "[build.rb] webpacking for production"
+  ENV["NODE_ENV"] = "production"
   `(npm install; npm run webpack) &> build.log`
 end

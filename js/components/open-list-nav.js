325a53ba102b5d32bd193da161b685bbf517c50f
gemfarmer <bshslowfood@gmail.com>
2016-02-16 15:05:49 -0600

    export OpenListNav object

--- js/components/open-list-nav.js
+++ js/components/open-list-nav.js
@@ -16,7 +16,7 @@
           rect.top < (window.innerHeight || document. documentElement.clientHeight);
     }
 
-    var OpenListNav = function() {
+    exports.OpenListNav = function() {
       // init OpenListNav Properties
       this.active = window.location.hash || '#intro';
       this.navItems = document.querySelectorAll('[data-nav-item]');
@@ -28,97 +28,97 @@
         prev: getScrollTop(),
         direction: 'down'
       };
+
+      this.registerEventHandlers();
     };
 
-    OpenListNav.prototype.updateScrollTop = function() {
-      this.scrollTop.prev = this.scrollTop.current;
-      this.scrollTop.current = getScrollTop();
-      this.scrollTop.direction = (this.scrollTop.current >= this.scrollTop.prev)
-        ? 'down'
-        : 'up';
-    };
+    exports.OpenListNav.prototype = {
+      updateScrollTop: function() {
+        this.scrollTop.prev = this.scrollTop.current;
+        this.scrollTop.current = getScrollTop();
+        this.scrollTop.direction = (this.scrollTop.current >= this.scrollTop.prev)
+          ? 'down'
+          : 'up';
+      },
 
-    OpenListNav.prototype.removeActive = function(){
-      this.active = null;
-      for (var i = 0; i < this.navItems.length; i++) {
-        this.navItems[i].setAttribute('data-active', false);
-      }
-    };
-
-    OpenListNav.prototype.addActive = function(el, name){
-      if (!el){
-        el = document.querySelector('[data-nav-item="' + name + '"]');
-        this.active = name;
-        el.setAttribute('data-active', true);
-      } else {
-        this.active = el.getAttribute('data-nav-item');
-        el.setAttribute('data-active', true);
-      }
-    };
-
-    OpenListNav.prototype.update = function(el, name){
-      this.removeActive();
-      this.addActive(el, name);
-    };
-
-    OpenListNav.prototype.updateSelectField = function(newValue) {
-      if (newValue){
-        this.navSelect.val(newValue);
-      }
-    };
-
-    OpenListNav.prototype.registerEventHandlers = function(){
-      var self = this;
-      if (!this.navIsSelect) {
+      removeActive: function(){
+        this.active = null;
         for (var i = 0; i < this.navItems.length; i++) {
-          var item = this.navItems[i];
-          item.addEventListener('click', function () {
-            self.update(this);
-          });
+          this.navItems[i].setAttribute('data-active', false);
         }
+      },
+
+      addActive: function(el, name){
+        if (!el){
+          el = document.querySelector('[data-nav-item="' + name + '"]');
+          this.active = name;
+          el.setAttribute('data-active', true);
+        } else {
+          this.active = el.getAttribute('data-nav-item');
+          el.setAttribute('data-active', true);
+        }
+      },
+
+      update: function(el, name){
+        this.removeActive();
+        this.addActive(el, name);
+      },
+
+      updateSelectField: function(newValue) {
+        if (newValue){
+          this.navSelect.val(newValue);
+        }
+      },
+
+      registerEventHandlers: function(){
+        var self = this;
+        if (!this.navIsSelect) {
+          for (var i = 0; i < this.navItems.length; i++) {
+            var item = this.navItems[i];
+            item.addEventListener('click', function () {
+              self.update(this);
+            });
+          }
+        }
+
+        window.addEventListener('scroll', function() {
+          self.updateScrollTop();
+          // TODO: throttle
+          self.detectNavChange();
+        });
+
+        window.addEventListener('resize', function(){
+          // TODO: throttle
+          self.detectNavChange();
+        });
+
+      },
+
+      changeHandler: function(selector) {
+        window.location.hash = selector.value;
+      },
+
+
+      detectNavChange: function(){
+
+        var self = this;
+
+        // initialize nav status as not updated
+        var updated = false;
+
+         Array.prototype.forEach.call(this.navHeaders, function(header){
+           var inViewPort = isElementInViewport(header);
+           if (inViewPort && !self.navIsSelect && !updated) {
+             self.update(null, header.name);
+             updated = true;
+           } else if(inViewPort && self.navIsSelect && !updated) {
+             var newName = header.name || header.id;
+            self.updateSelectField(newName);
+             updated = true;
+           }
+        });
       }
-
-      window.addEventListener('scroll', function() {
-        self.updateScrollTop();
-        // TODO: throttle
-        self.detectNavChange();
-      });
-
-      window.addEventListener('resize', function(){
-        // TODO: throttle
-        self.detectNavChange();
-      });
-
     };
 
-    OpenListNav.prototype.changeHandler = function(selector) {
-      window.location.hash = selector.value;
-    };
-
-
-    OpenListNav.prototype.detectNavChange = function(){
-
-      var self = this;
-
-      // initialize nav status as not updated
-      var updated = false;
-
-       Array.prototype.forEach.call(this.navHeaders, function(header){
-         var inViewPort = isElementInViewport(header);
-         if (inViewPort && !self.navIsSelect && !updated) {
-           self.update(null, header.name);
-           updated = true;
-         } else if(inViewPort && self.navIsSelect && !updated) {
-           var newName = header.name || header.id;
-          self.updateSelectField(newName);
-           updated = true;
-         }
-      });
-    };
-
-    var openListNav = new OpenListNav();
-
-    openListNav.registerEventHandlers();
-
-    module.exports = openListNav;
+    module.exports = exports.OpenListNav;
   })(this);

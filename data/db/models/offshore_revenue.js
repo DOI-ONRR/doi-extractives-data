0653f8812c187bde1e7a6fd5b7aa562040490207
Shawn Allen <shawn.allen@gsa.gov>
2016-04-01 14:44:40 -0700

    normalize commodity, etc.

--- data/db/models/offshore_revenue.js
+++ data/db/models/offshore_revenue.js
@@ -5,39 +5,47 @@ var parse = require('../../../lib/parse');
 var util = require('../../../lib/util');
 var parserHelper = require('../parser-helper');
 
-const VOLUME_COLUMN = 'Sales Volumes';
+const REVENUE_COLUMN = 'Total';
+const REGION_COLUMN = 'Offshore Region';
 
 module.exports = {
   autoparse: false,
   parser: parserHelper(function(input) {
-    input.Volume = parse.number(input[VOLUME_COLUMN]);
+    input.Revenue = parse.dollars(input[REVENUE_COLUMN]);
+    input.Region = util.normalizeOffshoreRegion(input[REGION_COLUMN]);
     input.Commodity = util.normalizeCommodity(input.Commodity);
     return input;
   }),
 
   models: {
-    county_production: {
-      tableName: 'county_production',
+    offshore_revenue: {
+      tableName: 'offshore_revenue',
+
       fields: {
         year: {
           input: 'Calendar Year',
           name: 'year',
           type: new sequelize.INTEGER(4).UNSIGNED
         },
-        state: {
-          input: 'St',
-          name: 'state',
-          type: new sequelize.STRING(2)
-        },
-        county: {
-          input: 'County Name',
-          name: 'county',
+        region: {
+          input: REGION_COLUMN,
+          name: 'region',
           type: new sequelize.STRING(32)
         },
-        fips: {
-          input: 'FIPS',
-          name: 'fips',
-          type: new sequelize.INTEGER(5).UNSIGNED.ZEROFILL
+        planning_area: {
+          input: 'Planning Area',
+          name: 'planning_area',
+          type: new sequelize.STRING(32)
+        },
+        offshore_area: {
+          input: 'Offshore Area',
+          name: 'offshore_area',
+          type: new sequelize.STRING(32)
+        },
+        protraction: {
+          input: 'Protraction',
+          name: 'protraction',
+          type: new sequelize.STRING(8)
         },
         commodity: {
           input: 'Commodity',
@@ -49,14 +57,14 @@ module.exports = {
           name: 'product',
           type: new sequelize.STRING(64)
         },
-        volume_type: {
-          input: 'Volume Type',
-          name: 'volume_type',
+        revenue_type: {
+          input: 'Revenue Type',
+          name: 'revenue_type',
           type: new sequelize.STRING(32)
         },
-        volume: {
-          input: 'Volume',
-          name: 'volume',
+        revenue: {
+          input: 'Revenue',
+          name: 'revenue',
           type: new sequelize.DECIMAL(12, 2)
         },
       }

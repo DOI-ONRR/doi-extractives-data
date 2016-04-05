271545e0fbecd1691c6dd63268d2777bcb8fa032
Shawn Allen <shawn.allen@gsa.gov>
2016-04-04 17:04:33 -0700

    add all-land production data

--- data/createdb.sh
+++ data/createdb.sh
@@ -16,16 +16,43 @@ load_sql() {
 }
 
 # lookup tables
-$tables -d "sqlite://${db}" -i _input/geo/states.csv -n states
-$tables -d "sqlite://${db}" -i _input/geo/offshore/areas.tsv \
+$tables -d $db_url -i _input/geo/states.csv -n states
+$tables -d $db_url -i _input/geo/offshore/areas.tsv \
     -n offshore_planning_areas
 
-# Federal Revenue
-load _input/onrr/county-revenues.tsv county_revenue
-# Offshore Revenue
-load _input/onrr/offshore-revenues.tsv offshore_revenue
-# update revenue rollups
-load_sql db/rollup-revenue.sql
+# All Lands Production: Coal
+load _input/eia/commodity/coal.tsv all_production_coal
+# All Lands Production: Oil
+cat _input/eia/commodity/oil.tsv \
+    | ./bin/unroll-columns.js \
+        --columns 'US,FL,NY,PA,VA,WV,IL,IN,KS,KY,MI,MO,NE,ND,OH,OK,SD,TN,AL,AR,LA,MS,NM,TX,gulf,CO,MT,UT,WY,AK,,AZ,CA,NV,pacific alaska' \
+        --destkey region --valkey volume --skip ',undefined' --of csv \
+    | $tables -d $db_url -n all_production_oil
+# All Lands Production: Natural Gas (part 1)
+cat _input/eia/commodity/naturalgas.tsv \
+    | ./bin/unroll-columns.js \
+        --columns US,AK,AR,CA,pacific,CO,gulf,KS,LA,MT,NM,ND,OH,OK,PA,TX,UT,WV,WY \
+        --destkey region --valkey volume --skip ',undefined' --of csv \
+    | $tables -d $db_url -n all_production_naturalgas
+# All Lands Production: Natural Gas (part 2)
+cat _input/eia/commodity/naturalgas2.tsv \
+    | ./bin/unroll-columns.js \
+        --columns AL,AZ,FL,IL,IN,KY,MD,MI,MS,MO,NE,NV,NY,OR,SD,TN,VA \
+        --destkey region --valkey volume --skip ',undefined' --of csv \
+    | $tables -d $db_url -n all_production_naturalgas
+# All Lands Production: Renewables
+cat _input/eia/commodity/renewables.tsv \
+    | ./bin/unroll-columns.js \
+        --destkey year --valkey volume \
+        --skip ELEC.GEN.ALL-US-99.A,ELEC.GEN.ALL --of csv \
+    | $tables -d $db_url -n all_production_renewables
+
+load_sql db/rollup-all-production.sql
+
+# output some rows for debugging purposes
+./bin/query.js "
+    SELECT * FROM all_national_production
+    WHERE year = 2013"
 
 # Federal Production
 load _input/onrr/regional-production.tsv federal_county_production
@@ -33,6 +60,13 @@ load _input/onrr/regional-production.tsv federal_county_production
 load _input/onrr/offshore-production.tsv federal_offshore_production
 # update production rollups
 load_sql db/rollup-federal-production.sql
+
+# Federal Revenue
+load _input/onrr/county-revenues.tsv county_revenue
+# Offshore Revenue
+load _input/onrr/offshore-revenues.tsv offshore_revenue
+# update revenue rollups
+load_sql db/rollup-revenue.sql
 
 # output some rows for debugging purposes
 ./bin/query.js "

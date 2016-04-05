eae5b26c0a82bf44d518c91319c2a2504dc51645
Shawn Allen <shawn.allen@gsa.gov>
2016-04-05 11:25:43 -0700

    fix up bin/query.js

--- data/bin/query.js
+++ data/bin/query.js
@@ -1,17 +1,27 @@
 #!/usr/bin/env node
 var tito = require('tito');
+var streamify = require('stream-array');
 var Sequelize = require('sequelize');
 var models = require('../db/config').models;
-var streamify = require('stream-array');
-var db = new Sequelize('sqlite://data.db', models);
 
-var out = tito.formats.createWriteStream('tsv');
+var db = new Sequelize('sqlite://data.db', {
+  logging: null,
+  models: models
+});
+
+var format = tito.formats.createWriteStream('tsv');
+var out = process.stdout;
 var query = process.argv[2];
 
 // console.warn('executing:', query);
 
-db.query(query).spread(function(results, meta) {
+db.query(query).spread(function(results) {
   streamify(results)
-    .pipe(out)
-    .pipe(process.stdout);
+    // write an extra line break at the end
+    .on('end', function() {
+      out.write('\n');
+      process.exit(0);
+    })
+    .pipe(format)
+    .pipe(out);
 });

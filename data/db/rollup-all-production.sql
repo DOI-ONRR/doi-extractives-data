e4b241eae3a3dbf2853137161891c43529901fad
Shawn Allen <shawn.allen@gsa.gov>
2016-04-04 22:24:50 -0700

    create state ranking tables for production

--- data/db/rollup-all-production.sql
+++ data/db/rollup-all-production.sql
@@ -3,12 +3,15 @@
 DROP VIEW IF EXISTS all_regional_production;
 CREATE VIEW all_regional_production AS
     SELECT
-        year, state AS region,
+        year, states.abbr AS region,
         'Coal' AS commodity,
         'Coal (short tons)' AS product,
         SUM(volume) AS volume
     FROM all_production_coal
-    GROUP BY year, region, state
+    INNER JOIN states ON
+        states.name LIKE (region || '%')
+    GROUP BY
+        year, region, state
 UNION
     SELECT
         year, region,
@@ -18,11 +21,13 @@ UNION
     FROM all_production_naturalgas
 UNION
     SELECT
-        year, state AS region,
+        year, states.abbr AS region,
         'Renewables' AS commodity,
         source AS product,
         volume
     FROM all_production_renewables
+    INNER JOIN states ON
+        states.name = state
 UNION
     SELECT
         year, region,
@@ -37,4 +42,48 @@ CREATE VIEW all_national_production AS
         year, commodity, product,
         SUM(volume) AS volume
     FROM all_regional_production
-    GROUP BY year, commodity, product;
+    GROUP BY
+        year, commodity, product;
+
+-- create state rankings views
+DROP TABLE IF EXISTS all_production_state_rank;
+CREATE TABLE all_production_state_rank AS
+    SELECT
+        state.year AS year,
+        state.region AS state,
+        state.product AS product,
+        state.volume AS volume,
+        national.volume AS total,
+        -- these numbers are both integers, so we need to explicitly cast one
+        -- of them as a float in order to get a float back (because an integer
+        -- divided by an integer always returns an integer)
+        100 * (
+            CAST(state.volume AS FLOAT) /
+            national.volume
+        ) AS percent,
+        0 AS rank
+    FROM
+        all_regional_production AS state
+    INNER JOIN
+        all_national_production AS national
+    ON
+        national.year = state.year AND
+        national.product = state.product
+    WHERE
+        state.volume IS NOT NULL AND
+        LENGTH(state.region) = 2 AND
+        national.volume IS NOT NULL
+    ORDER BY
+        state.year,
+        state.product,
+        percent DESC;
+
+UPDATE all_production_state_rank
+SET rank = (
+    SELECT COUNT(distinct inner.percent) AS rank
+    FROM all_production_state_rank AS inner
+    WHERE
+        inner.year = all_production_state_rank.year AND
+        inner.product = all_production_state_rank.product AND
+        inner.percent > all_production_state_rank.percent
+) + 1;

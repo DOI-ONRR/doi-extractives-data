e1b50e88325b0b2949af1dbc5d2c31f22d2593bf
Shawn Allen <shawn.allen@gsa.gov>
2016-04-06 17:20:46 -0700

    add revenue rank per year

--- data/generate-state-federal-production.sh
+++ data/generate-state-federal-production.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 ./bin/query.js --format json "
     SELECT
-      state, product, year, ROUND(volume) AS volume
-    FROM federal_state_production
-    ORDER BY state, product, year" | \
+      state, product, year,
+      ROUND(volume) AS volume,
+      rank
+    FROM federal_production_state_rank
+    ORDER BY
+        state, product, year" | \
   ../node_modules/.bin/nestly --if json \
       -c ../_meta/state_federal_production.yml \
       -o ../_data/state_federal_production.yml

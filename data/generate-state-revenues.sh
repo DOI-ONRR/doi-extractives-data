87dda6be0f91c0a4fdf7c12bb779d3271f9127b9
Shawn Allen <shawn.allen@gsa.gov>
2016-04-06 17:21:18 -0700

    add revenue rank per year

--- data/generate-state-revenues.sh
+++ data/generate-state-revenues.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 ./bin/query.js --format json "
     SELECT
-      state, product, year, ROUND(revenue) AS revenue
-    FROM state_revenue
+      state, product, year,
+      ROUND(revenue) AS revenue,
+      rank
+    FROM state_revenue_rank
     WHERE revenue != 0
-    ORDER BY state, product, year" \
+    ORDER BY
+        state, product, year" \
     | ../node_modules/.bin/nestly --if json \
         -c ../_meta/state_revenues.yml \
         -o ../_data/state_revenues.yml

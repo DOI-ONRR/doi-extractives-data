c012ce4e44e4b11371d0773b5a8172237922b3df
Shawn Allen <shawn.allen@gsa.gov>
2016-04-07 16:03:05 -0700

    add "plural" and "percent" template filters

---
+++ _plugins/format.rb
@@ -0,0 +1,27 @@
+module EITI
+
+  module Format
+
+    def plural(num, plural="s")
+      (num.to_i == 1) ? "" : plural
+    end
+
+    def percent(num, precision=1, small="&lt;1")
+      # if it's less than 1, return the "small" representation
+      if num.to_f < 1.0
+        return small
+      end
+      # if it has decimal precision, format it
+      if num % 1 > 0
+        return num.round(precision)
+      end
+
+      # puts "percent(#{num}, #{precision}, '#{small}')"
+      num.to_i
+    end
+
+  end
+
+end
+
+Liquid::Template.register_filter(EITI::Format)

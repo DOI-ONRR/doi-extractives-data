6c5b208cbb2f7a62d7bdc502f8e7ea60558af441
Shawn Allen <shawn.allen@gsa.gov>
2016-04-12 15:57:36 -0700

    better % precision

--- data/generate-exports.sh
+++ data/generate-exports.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-./bin/query.js --format json "
+./bin/query.js --format ndjson "
     SELECT
       state, year,
-      value AS dollars,
-      round(share * 100, 1) AS percent,
+      ROUND(value, 2) AS dollars,
+      ROUND(share * 100, 2) AS percent,
       commodity
     FROM exports
     WHERE
       state != 'US'
   ORDER BY state, year" | \
-  ../node_modules/.bin/nestly --if json \
+  ../node_modules/.bin/nestly --if ndjson \
       -c ../_meta/state_exports.yml \
       -o ../_data/state_exports.yml

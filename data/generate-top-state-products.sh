4ceefbe05b5df3231ac9f5809e6e1aea705fb6b5
Shawn Allen <shawn.allen@gsa.gov>
2016-04-13 11:02:22 -0700

    include value in top products; tweak top N + %

--- data/generate-top-state-products.sh
+++ data/generate-top-state-products.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 # top N states for each product category in each year
-top=10
+top=3
 # minimum percent of nation-wide total
-percent=10
+percent=20
 ./bin/query.js --format json "
     SELECT
         state, product, percent, rank, year,
-        'revenue' AS category
+        revenue AS value, 'revenue' AS category
     FROM state_revenue_rank
-    WHERE rank <= ${top} AND percent > ${percent}
+    WHERE rank <= ${top} AND percent >= ${percent}
 UNION
     SELECT
         state, product, percent, rank, year,
-        'federal_production' AS category
+        volume AS value, 'federal_production' AS category
     FROM federal_production_state_rank
-    WHERE rank <= ${top} AND percent > ${percent}
+    WHERE rank <= ${top} AND percent >= ${percent}
         AND LENGTH(state) = 2
 UNION
     SELECT
         state, product, percent, rank, year,
-        'all_production' AS category
+        volume AS value, 'all_production' AS category
     FROM all_production_state_rank
-    WHERE rank <= ${top} AND percent > ${percent}
+    WHERE rank <= ${top} AND percent >= ${percent}
 ORDER BY state, year, rank, percent DESC
     " | ../node_modules/.bin/nestly --if json \
         -c ../_meta/top_state_products.yml \

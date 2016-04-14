3fa304e73a67ce392651fb9586c5c2345c860934
Shawn Allen <shawn.allen@gsa.gov>
2016-04-14 13:14:13 -0700

    switch output sort order

--- data/generate-state-disbursements.sh
+++ data/generate-state-disbursements.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 ./bin/query.js --format ndjson "
     SELECT
-        state, fund, source, year,
+        state, source, fund, year,
         ROUND(dollars, 2) AS dollars
     FROM state_disbursements
     WHERE dollars > 0
-    ORDER BY state, fund, source, year
+    ORDER BY state, source, fund, year
     " | \
   ../node_modules/.bin/nestly --if ndjson \
       -c ../_meta/state_disbursements.yml \

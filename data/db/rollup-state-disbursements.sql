ccb365b0efda8e3903c845f4777441dd74d0ef8f
Shawn Allen <shawn.allen@gsa.gov>
2016-04-14 16:25:15 -0700

    create all_disbursements table

--- data/db/rollup-state-disbursements.sql
+++ data/db/rollup-state-disbursements.sql
@@ -1,9 +1,5 @@
--- first, we delete the 'All' rows and the Historic Preservation rows, because
--- the onshore state totals include the HPF
-DELETE FROM state_disbursements
-WHERE
-    fund = 'All' OR
-    fund = 'Historic Preservation';
+-- first, we delete the 'All' rows
+DELETE FROM state_disbursements WHERE fund = 'All';
 
 -- then, we total up the onshore and offshore numbers to get the 'All' rows
 INSERT INTO state_disbursements
@@ -17,12 +13,14 @@ INSERT INTO state_disbursements
     GROUP BY year, state;
 
 -- then we incorporate HPF rows, which apparently come from offshore
-INSERT INTO state_disbursements
-    (year, state, fund, source, dollars)
+DROP TABLE IF EXISTS all_disbursements;
+CREATE TABLE all_disbursements AS
     SELECT
-        year, state,
-        'Historic Preservation' AS fund,
-        'Offshore' AS source,
-        dollars
+        year, state, fund, source, dollars
+    FROM state_disbursements
+UNION
+    SELECT
+        year, state, 'Historic Preservation' AS fund,
+        'Offshore' AS source, dollars
     FROM disbursements_historic_preservation
     WHERE source = 'HPF to States';

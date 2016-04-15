88074ee235246fe6b7ea7caf40e42ff42cf8e8a9
Shawn Allen <shawn.allen@gsa.gov>
2016-04-15 10:57:22 -0700

    update state revenue data

--- data/generate-state-revenues.sh
+++ data/generate-state-revenues.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-./bin/query.js --format json "
+./bin/query.js --format ndjson "
     SELECT
-      state, product, year, percent,
+      state, product, year,
+      ROUND(percent, 1) AS percent,
       ROUND(revenue) AS revenue,
       rank
     FROM state_revenue_rank
     WHERE revenue != 0
     ORDER BY
         state, product, year" \
-    | ../node_modules/.bin/nestly --if json \
+    | ../node_modules/.bin/nestly --if ndjson \
         -c ../_meta/state_revenues.yml \
         -o ../_data/state_revenues.yml

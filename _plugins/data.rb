0f7f0b7232ef4439605db2d310f6cb26364ee2dd
Shawn Allen <shawn.allen@gsa.gov>
2016-05-06 16:41:00 -0400

    coerce all get() keys to strings; comments

--- _plugins/data.rb
+++ _plugins/data.rb
@@ -2,8 +2,17 @@ module EITI
 
   module Data
 
+    # access a nested property of the (assumed) Hash data:
+    #
+    # get({'a' => 1000}, 'a') == 1000
+    # get({'2001' => 5}, 2001) === 5
     def get(data, *keys)
-      for key in keys.select{ |k| not k.empty? }
+      # coerce all of the keys to strings,
+      # and filter out any empty ones
+      keys = keys
+        .map{ |k| k.to_s }
+        .select{ |k| not k.empty? }
+      for key in keys
         for k in key.split('.')
           data = data[k]
           if not data
@@ -14,6 +23,8 @@ module EITI
       data
     end
 
+    # pad the provided string with the provided padding character (or a
+    # space, by default) if its length is less than a given length
     def pad_left(str, len, pad=' ')
       pad_by = len - str.size
       if pad_by > 0

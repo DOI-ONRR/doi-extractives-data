d5077ab32fb11648b9cc1d0c46176d772f07d3d6
Shawn Allen <shawn.allen@gsa.gov>
2016-05-09 15:31:45 -0700

    nix old models; update state page template

--- data/db/config.js
+++ data/db/config.js
@@ -5,11 +5,9 @@ var getModel = function(name) {
 };
 
 var models = {
-  county_revenue:               getModel('county_revenue'),
-  offshore_revenue:             getModel('offshore_revenue'),
-  federal_county_production:    getModel('federal_county_production'),
-  federal_offshore_production:  getModel('federal_offshore_production'),
-  bls_employment:               getModel('bls_employment'),
+  county_revenue:   getModel('county_revenue'),
+  offshore_revenue: getModel('offshore_revenue'),
+  bls_employment:   getModel('bls_employment'),
 };
 
 module.exports = {

1ea1449749981ef7ee75088688621cb9300024c1
Shawn Allen <shawn.allen@gsa.gov>
2016-05-26 14:37:01 -0700

    fix linting errors

--- test/data/revenue.js
+++ test/data/revenue.js
@@ -1,3 +1,6 @@
+/* jshint node: true, mocha: true */
+/* jshint -W089 */
+/* jshint -W110 */
 var tito = require('tito');
 var fs = require('fs');
 var path = require('path');
@@ -21,57 +24,101 @@ describe('state rollups', function() {
 
   describe('revenues by type', function() {
 
-    var dataSource = path.join(__dirname, '../../_data/state_revenues_by_type.yml');
-    var pivotSource = path.join(__dirname, '../../data/revenue/pivot.tsv');
-    var stateRevenueByType = yaml.safeLoad(fs.readFileSync(dataSource, 'utf8'));
+    var dataSource = path.join(
+      __dirname,
+      '../../_data/state_revenues_by_type.yml'
+    );
+
+    var pivotSource = path.join(
+      __dirname,
+      '../../data/revenue/pivot.tsv'
+    );
+
+    var stateRevenueByType = yaml.safeLoad(
+      fs.readFileSync(dataSource, 'utf8')
+    );
 
     it('match values in the pivot table', function(done) {
-      var line = 1;
+      var testRow = function(d, i) {
+        var expected = +d.Total;
+        var actual;
+        try {
+          actual = stateRevenueByType[
+            d.St
+          ][
+            d.Commodity.trim()
+          ][
+            d['Revenue Type']
+          ][
+            d.CY
+          ];
+        } catch (error) {
+          assert.ok(false, 'no data for: ' + JSON.stringify(d));
+        }
+        var difference = expected - actual;
+        assert.ok(
+          Math.abs(difference) <= 1,
+          actual,
+          (actual + ' != ' + expected + ' @ ' + (i + 1))
+        );
+      };
+
       load(pivotSource, 'tsv', function(error, rows) {
-        rows.forEach(function(d) {
-          var expected = +d.Total;
-          try {
-            var actual = stateRevenueByType[d.St][d.Commodity.trim()][d['Revenue Type']][d.CY];
-          } catch (error) {
-            assert.ok(false, 'no data for: ' + JSON.stringify(d));
-          }
-          var difference = expected - actual;
-          assert.ok(Math.abs(difference) <= 1, actual, (actual + ' != ' + expected + ' @ ' + line));
-          line++;
-        });
+        rows.forEach(testRow);
         done();
       });
     });
 
-    it('does not contain values that are not in the pivot table', function(done) {
+    it("doesn't contain values that aren't in the pivot table", function(done) {
       load(pivotSource, 'tsv', function(error, rows) {
-        for (var state in stateRevenueByType) {
-          for (var commodity in stateRevenueByType[state]) {
+        var state;
+        var commodity;
+        var type;
+        var year;
+        var actual;
+        var expected;
+        var difference;
+        var found;
+
+        var filter = function(d) {
+          return d.St === state &&
+                 d.Commodity.trim() === commodity &&
+                 d['Revenue Type'] === type &&
+                 d.CY === year;
+        };
+
+        for (state in stateRevenueByType) {
+          for (commodity in stateRevenueByType[state]) {
             if (commodity === 'All') {
               continue;
             }
-            for (var type in stateRevenueByType[state][commodity]) {
+            for (type in stateRevenueByType[state][commodity]) {
               if (type === 'All') {
                 continue;
               }
-              for (var year in stateRevenueByType[state][commodity][type]) {
-                var actual = stateRevenueByType[state][commodity][type][year];
-                var row = rows.filter(function(d) {
-                  return d.St === state &&
-                         d.Commodity.trim() === commodity &&
-                         d['Revenue Type'] === type &&
-                         d.CY === year;
-                });
+              for (year in stateRevenueByType[state][commodity][type]) {
+                actual = stateRevenueByType[state][commodity][type][year];
+                found = rows.filter(filter);
+
                 assert.equal(
-                  row.length, 1,
-                  'wrong row count: ' + row.length + ' for: ' + [state, commodity, type, year].join('/'));
-                var expected = row[0].Total;
-                var difference = expected - actual;
-                assert.ok(Math.abs(difference) <= 1, actual, (actual + ' != ' + expected));
+                  found.length, 1,
+                  'wrong row count: ' + found.length +
+                  ' for: ' + [state, commodity, type, year].join('/')
+                );
+
+                expected = found[0].Total;
+                difference = expected - actual;
+
+                assert.ok(
+                  Math.abs(difference) <= 1,
+                  actual,
+                  (actual + ' != ' + expected)
+                );
               }
             }
           }
         }
+
         done();
       });
     });
@@ -79,16 +126,27 @@ describe('state rollups', function() {
   });
 
   it('properly sums up "All" revenues (by commodity)', function() {
-    var dataSource = path.join(__dirname, '../../_data/state_revenues.yml');
-    var stateRevenuesByCommodity = yaml.safeLoad(fs.readFileSync(dataSource, 'utf8'));
+    var dataSource = path.join(
+      __dirname,
+      '../../_data/state_revenues.yml'
+    );
+
+    var stateRevenuesByCommodity = yaml.safeLoad(
+      fs.readFileSync(dataSource, 'utf8')
+    );
 
     for (var state in stateRevenuesByCommodity) {
       var commodities = stateRevenuesByCommodity[state].commodities;
       var allByYear = {};
       var totalsByYear = {};
       var count = 0;
-      for (var commodity in commodities) {
-        for (var year in commodities[commodity]) {
+
+      var commodity;
+      var year;
+      var difference;
+
+      for (commodity in commodities) {
+        for (year in commodities[commodity]) {
           var revenue = commodities[commodity][year].revenue;
           if (commodity === 'All') {
             allByYear[year] = revenue;
@@ -101,21 +159,23 @@ describe('state rollups', function() {
 
       // compare yearly totals, using the number of commodities as a standin
       // for the acceptable rounding error (+/- 1 for each)
-      for (var year in totalsByYear) {
-        var difference = Math.abs(allByYear[year] - totalsByYear[year]);
+      for (year in totalsByYear) {
+        difference = Math.abs(allByYear[year] - totalsByYear[year]);
         assert.ok(
           difference <= count,
-          'abs(' + allByYear[year] + ' - ' + totalsByYear[year] + ' = ' + difference + ')'
+          'abs(' + allByYear[year] + ' - ' +
+            totalsByYear[year] + ' = ' + difference + ')'
         );
       }
 
       // now check the keys for allByYear just to be sure that we don't have
       // extra years in there
       for (year in allByYear) {
-        var difference = Math.abs(allByYear[year] - totalsByYear[year]);
+        difference = Math.abs(allByYear[year] - totalsByYear[year]);
         assert.ok(
           difference <= count,
-          'abs(' + allByYear[year] + ' - ' + totalsByYear[year] + ' = ' + difference + ')'
+          'abs(' + allByYear[year] + ' - ' +
+            totalsByYear[year] + ' = ' + difference + ')'
         );
       }
     }

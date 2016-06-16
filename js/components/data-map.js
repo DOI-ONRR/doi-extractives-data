814e5a199d3208881b6f5616dc07c8c363da1723
gemfarmer <bshslowfood@gmail.com>
2016-06-16 16:36:43 -0500

    got rid of rounding issue

--- js/components/data-map.js
+++ js/components/data-map.js
@@ -32,6 +32,8 @@
           var steps = this.getAttribute('steps') || 5;
           var units = this.getAttribute('units') || '';
 
+          var stepDomain = [1, 9];
+
           var colors = colorbrewer[scheme][steps];
           if (!colors) {
             return console.error(
@@ -61,18 +63,90 @@
             .domain(domain)
             .range(colors);
 
+
           // console.log('scale  :',scale)
 
 
-          marks.attr('fill', scale);
 
+          var intervals = createIntervals(steps);
+          var _steps = createSteps(steps);
+          console.log(intervals)
 
-          d3.select(this).selectAll('[threshold-start]')
+          var uniqueSteps = getUnique(marks.data(), _steps, domain)
+          var domains = createDomains(intervals, domain);
 
+          var textScaleStart = d3.scale[type]()
+            .domain(stepDomain)
+            .range(domains[0]);
 
+          var textScaleEnd = d3.scale[type]()
+            .domain(stepDomain)
+            .range(domains[1]);
 
-          // svg:egend.select(".legendQuant")
-          //   .call(legend);
+          var swatches = d3.select(this)
+            .selectAll('dl [data-step]')
+            .datum(function() {
+              return +this.getAttribute('data-step') || 1;
+            });
+
+          swatches.style('background-color', scaleSteps);
+
+          format.start = function(value, units) {
+            if (units === '$' || units.includes('dollar')) {
+              return [format.dollars(value), ' –'].join(' ');
+            } else {
+              return [format.si(value), ' –'].join(' ');
+            }
+          }
+
+          format.end = function(value, units) {
+            if (units === '$' || units.includes('dollar')) {
+              return format.si(value);
+            } else if (units.includes('kilowatt')) {
+              return [format.si(value), 'kWh'].join(' ');
+            } else if(units.includes('thousands of pounds')) {
+              return [format.si(value), 'thousands of pounds'].join(' ');
+            } else if(units.includes('millions of btus')) {
+              return [format.si(value), 'millions of btus'].join(' ');
+            } else {
+              return [format.si(value), units].join(' ');
+            }
+          }
+
+          d3.select(this)
+              .selectAll('dl [legend-threshold]')
+              .datum(function() {
+                return +this.getAttribute('legend-threshold') || 1;
+              });
+
+          d3.select(this).selectAll('[legend-start]')
+            .text(function (d) {
+              return format.start(textScaleStart(d), units);
+            });
+          d3.select(this).selectAll('[legend-end]')
+            .text(function (d) {
+              return format.end(textScaleEnd(d), units);
+            });
+
+          // only show swatches that show up in map
+          filterSteps(uniqueSteps, this);
+
+          // trim height on map container
+          var svgContainer = d3.select(this)
+            .selectAll('.svg-container[data-dimensions]')
+            .datum(function() {
+              return (this.getBoundingClientRect().width
+                * +this.getAttribute('data-dimensions')
+                / 100)
+                + 50;
+            });
+
+          function pixelize(d) {
+            return d + 'px';
+          }
+
+          svgContainer.style('padding-bottom', pixelize);
+
         }}
       }
     )

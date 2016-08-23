4cc0ce1b57f9bc16841d51f91d4ff5aa8c4a6e48
Shawn Allen <shawn.allen@gsa.gov>
2016-08-23 16:16:35 -0700

    move product config to YAML; update offshore region names

--- data/all-production/update.js
+++ data/all-production/update.js
@@ -1,200 +1,25 @@
 #!/usr/bin/env node
 const async = require('async');
 const fs = require('fs');
-const qs = require('querystring');
 const path = require('path');
+const qs = require('querystring');
 const request = require('request');
 const streamify = require('stream-array');
 const thru = require('through2').obj;
 const tito = require('tito');
 const util = require('../../lib/util');
-
-const STATES_CSV_PATH = '../_input/geo/states.csv';
+const yaml = require('js-yaml');
 
 const API_BASE_URL = 'http://api.eia.gov/series/';
 const API_BASE_PARAMS = {
   api_key: process.env.EIA_API_KEY,
-  start: 2006,
-  end: 2015,
   out: 'json'
 };
 
 // where to write product-specific files
 const OUTPUT_FILENAME_TEMPLATE = 'product/{product}.tsv';
 
-const products = {
-
-  'coal': {
-    values: {
-      product: 'Coal'
-    },
-    params: {
-      series_id: 'COAL.PRODUCTION.TOT-{region}-TOT.A'
-    }
-  },
-
-  'oil': {
-    values: {
-      product: 'Crude Oil'
-    },
-    params: {
-      series_id: 'PET.MCRFP{region}1.A'
-    }
-  },
-
-  'offshore-oil': {
-    values: {
-      product: 'Crude Oil'
-    },
-    params: {
-      series_id: {
-        'PET.MCRFP3FM1.A': {
-          region: 'GOM'
-        },
-        'PET.MCRFP5F1.A': {
-          region: 'SOC'
-        }
-      }
-    }
-  },
-
-  'naturalgas': {
-    values: {
-      product: 'Natural Gas'
-    },
-    params: {
-      series_id: 'NG.N9010{region}2.A'
-    }
-  },
-
-  /*
-  // XXX this data is unusable because it ends in 2006.
-  'offshore-naturalgas': {
-    values: {
-      product: 'Natural Gas'
-    },
-    params: {
-      series_id: {
-        'NG.RNGR20R3FM_1.A': {
-          region: 'GOM'
-        }
-      }
-    }
-  },
-  */
-
-  /*
-  // XXX this is the total renewable energy production
-  'renewables': {
-    values: {
-      product: 'Renewable Energy'
-    },
-    params: {
-      series_id: 'SEDS.REPRB.{region}.A'
-    }
-  },
-  */
-
-  /*
-  // XXX this category is no longer listed on the EIA site
-  'biomass': {
-    values: {
-      product: 'Biomass (total)'
-    },
-    params: {
-      series_id: 'ELEC.GEN.BIO-{region}-99.A'
-    }
-  },
-  */
-
-  'geothermal': {
-    values: {
-      product: 'Geothermal',
-      units: 'MMWh'
-    },
-    params: {
-      series_id: 'ELEC.GEN.GEO-{region}-99.A'
-    }
-  },
-
-  'hydroelectric': {
-    values: {
-      product: 'Conventional Hydroelectric',
-      units: 'MMWh'
-    },
-    params: {
-      series_id: 'ELEC.GEN.HYC-{region}-99.A'
-    }
-  },
-
-  'solar': {
-    values: {
-      product: 'Solar',
-      units: 'MMWh'
-    },
-    params: {
-      series_id: 'ELEC.GEN.SUN-{region}-99.A'
-    }
-  },
-
-  'wind': {
-    values: {
-      product: 'Wind',
-      units: 'MMWh'
-    },
-    params: {
-      series_id: 'ELEC.GEN.WND-{region}-99.A'
-    }
-  },
-
-  'wood': {
-    values: {
-      product: 'Wood and wood-derived fuels'
-    },
-    params: {
-      series_id: 'ELEC.GEN.WWW-{region}-99.A'
-    }
-  },
-
-  'other-biomass': {
-    values: {
-      product: 'Other biomass'
-    },
-    params: {
-      series_id: 'ELEC.GEN.WAS-{region}-99.A'
-    }
-  },
-
-  /*
-  // XXX: we're removing this, per #1574
-  'other-renewables': {
-    values: {
-      product: 'All Other Renewables'
-    },
-    params: {
-      series_id: 'ELEC.GEN.AOR-{region}-99.A'
-    }
-  },
-  */
-
-};
-
-var requestedCommodityKeys = process.argv.slice(2);
-if (requestedCommodityKeys.length) {
-  for (var key in products) {
-    if (requestedCommodityKeys.indexOf(key) == -1) {
-      delete products[key];
-    }
-  }
-  if (Object.keys(products).length === 0) {
-    console.error('no products found matching:', requestedCommodityKeys.join(', '));
-    process.exit(1);
-  }
-  console.warn('only generating keys:', Object.keys(products).join(', '));
-  // process.exit(0);
-}
-
-const context = {};
+var config;
 
 const fetchSeries = function(params, values, done) {
   const url = [API_BASE_URL, qs.stringify(params)].join('?');
@@ -223,10 +48,15 @@ const fetchSeries = function(params, values, done) {
     });
 };
 
-const fetchCommodity = function(product, next) {
-  const source = products[product];
-  const params = Object.assign({}, API_BASE_PARAMS, source.params);
-  const series = params.series_id;
+const fetchProduct = function(product, next) {
+  const source = config.products[product];
+  const params = Object.assign(
+    {},
+    API_BASE_PARAMS,
+    config.params,
+    source.params
+  );
+  const series = source.series;
   console.warn('fetching product:', product, 'from', series);
 
   const file = OUTPUT_FILENAME_TEMPLATE.replace('{product}', product);
@@ -283,13 +113,13 @@ const fetchCommodity = function(product, next) {
      * concatenate the resulting series together.
      */
     async.mapSeries(
-      context.regions,
+      config.regions,
       function fetchRegion(region, done) {
         params.series_id = series.replace('{region}', region);
         const values = Object.assign(
           {
-            product:  product,
-            region:     region
+            product: product,
+            region: region
           },
           source.values
         );
@@ -321,22 +151,43 @@ const fetchCommodity = function(product, next) {
 };
 
 async.waterfall([
-  function loadStates(next) {
-    util.readData(
-      STATES_CSV_PATH,
-      tito.createReadStream('csv'),
-      next
+  function loadConfig(next) {
+    const configPath = path.join(__dirname, 'config.yml');
+    console.warn('loading config:', configPath);
+    fs.readFile(
+      configPath, 'utf8',
+      function(error, buffer) {
+        if (error) return next(error);
+        config = yaml.safeLoad(buffer);
+        console.warn('config loaded...', config.products);
+        next();
+      }
     );
   },
 
-  function updateContext(states, next) {
-    context.regions = states.map(function(d) { return d.abbr; });
-    context.regions.unshift('US');
+  function prepProducts(next) {
+    const products = config.products;
+
+    var productKeys = process.argv.slice(2);
+    if (productKeys.length) {
+      for (var key in products) {
+        if (productKeys.indexOf(key) == -1) {
+          delete products[key];
+        }
+      }
+      if (Object.keys(products).length === 0) {
+        console.error('no products found matching:', productKeys.join(', '));
+        process.exit(1);
+      }
+      console.warn('only generating keys:', Object.keys(products).join(', '));
+      // process.exit(0);
+    }
+
     next();
   },
 
   function fetchCommmodities(next) {
-    async.map(Object.keys(products), fetchCommodity, next);
+    async.map(Object.keys(config.products), fetchProduct, next);
   }
 ], function(error) {
   if (error) {

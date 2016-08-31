727e31026461631f27f7844cd709f3c53424da77
Shawn Allen <shawn.allen@gsa.gov>
2016-08-31 16:47:13 -0700

    kill the data/db directory!

--- data/db/parser-helper.js
+++
@@ -1,6 +0,0 @@
-module.exports = function(transform) {
-  return function(model, input) {
-    input = transform.call(this, input, model);
-    return this.autoParser(input, this.options.models);
-  };
-};

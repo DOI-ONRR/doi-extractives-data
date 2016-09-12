0c7b0e844a348a7a6583fb5f37d0998483c6c698
gemfarmer <bshslowfood@gmail.com>
2016-09-12 14:30:47 -0500

    add boiled down version of federal-production.js that checks sentinel values defined by Chris

--- test/data/federal-production.js
+++ test/data/federal-production.js
@@ -55,142 +55,50 @@ describe('federal production (ONRR)', function() {
     );
     var products = nationalValues.US.products;
 
+    var assertSentinelMatch = function (product, year, value) {
+      var expected = Math.round(value);
+      var actual = products[product].volume[year];
+      assert.equal(
+        expected, actual,
+        'expected ' + value + ' for: ' + [product, year].join(' | ')
+      );
+    }
+
     it('check sentinel values', function(done) {
 
-      // Oil | 2013: 623,103,687.09 barrels
-      // Salt | 2015: 764,801 tons
-      // Salt | 2006: 512,832 tons
-      // Gas | 2013: 4,941,408,110
-
-      var product;
-      var year;
-      var volume;
-      var units;
-      var matches;
-      var match;
-      var filter = function(d) {
-        return d.region === 'US'
-            && d.product === product
-            && d.year === year;
-      };
-
-      var salt2006 = products['Salt (tons)'].volume[2015]
-      var salt2015 = products['Salt (tons)'].volume[2015]
-      var oil2013 = products['Oil (bbl)'].volume[2013]
-
-
-      for (product in products) {
-        for (year in products[product].volume) {
-          volume = products[product].volume[year];
-          units = products[product].units;
-          matches = rows.filter(filter);
-          assert.equal(matches.length, 1);
-          match = matches[0];
-          assertVolumeMatch(
-            match.volume,
-            match.units,
-            volume,
-            units
-          );
+      var sentinels = [
+        {
+          product: 'Salt (tons)',
+          year: 2015,
+          value: 764801
+        },
+        {
+          product: 'Salt (tons)',
+          year: 2006,
+          value: 512832
+        },
+        {
+          product: 'Oil (bbl)',
+          year: 2013,
+          value: 623103681.09
+        },
+        {
+          product: 'Gas (mcf)',
+          year: 2013,
+          value: 4941408110
         }
-      }
+      ];
+
+      sentinels.forEach(function(sentinel) {
+        assertSentinelMatch(
+          sentinel.product,
+          sentinel.year,
+          sentinel.value
+        );
+      });
+
       done();
     });
-
-
-
-  });
-
-  describe('state values', function() {
-
-    var stateValues = yaml.safeLoad(
-      fs.readFileSync(
-        path.join(OUT_PATH, 'state_federal_production.yml'),
-        'utf8'
-      )
-    );
-
-    // it('all state input rows exist in YAML', function(done) {
-    //   loadAll(IN_PATH, function(error, rows) {
-    //     if (error) {
-    //       return done(error);
-    //     }
-
-    //     var volume;
-
-    //     rows
-    //       .filter(function(d) {
-    //         return d.region !== 'US'
-    //             && d.region.length === 2
-    //             && d.volume;
-    //       })
-    //       .forEach(function(d) {
-    //         var dataPath = [d.region, 'products', d.product, 'volume', d.year];
-    //         var product;
-    //         try {
-    //           product = stateValues[d.region].products[d.product];
-    //           volume = product.volume[d.year].volume;
-    //         } catch (error) {
-    //           throw new Error('no YAML data for row: ' + dataPath.join('.')
-    //                          + ' ' + JSON.stringify(d));
-    //         }
-
-    //         assertVolumeMatch(
-    //           d.volume,
-    //           d.units,
-    //           volume,
-    //           product.units
-    //         );
-    //       });
-
-    //     done();
-    //   });
-    // });
-
-    // it('all YAML values exist in the input data', function(done) {
-    //   loadAll(IN_PATH, function(error, rows) {
-    //     if (error) {
-    //       return done(error);
-    //     }
-
-    //     var state;
-    //     var product;
-    //     var year;
-    //     var volume;
-    //     var units;
-    //     var matches;
-    //     var match;
-    //     var filter = function(d) {
-    //       return d.region === state
-    //           && d.product === product
-    //           && d.year === year;
-    //     };
-
-    //     for (state in stateValues) {
-    //       for (product in stateValues[state].products) {
-    //         var values = stateValues[state].products[product];
-    //         units = values.units;
-    //         for (year in values.volume) {
-    //           volume = values.volume[year].volume;
-    //           matches = rows.filter(filter);
-    //           assert.equal(matches.length, 1);
-    //           match = matches[0];
-    //           assertVolumeMatch(
-    //             match.volume,
-    //             match.units,
-    //             volume,
-    //             units
-    //           );
-    //         }
-    //       }
-    //     }
-
-    //     done();
-    //   });
-    // });
-  });
-
-  describe('offshore values', function() {
   });
 
 });

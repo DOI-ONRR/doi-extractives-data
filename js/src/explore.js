b0dc70aec7190b5f1e13715f891f53aebc626eb9
Shawn Allen <shawn.allen@gsa.gov>
2016-09-12 16:56:21 -0700

    include aria-tabs

--- js/src/explore.js
+++ js/src/explore.js
@@ -3,6 +3,7 @@
 
   // FIXME: does this export anything?
   require('./../components/sticky.js');
+  require('./../components/aria-tabs.js');
 
   exports.Immutable = require('immutable');
   exports.EITIBar = require('./../components/eiti-bar.js');

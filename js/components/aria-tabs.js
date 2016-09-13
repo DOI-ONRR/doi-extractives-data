06ebe330da13aa6ca741b767cb70ec7d565a415e
Shawn Allen <shawn.allen@gsa.gov>
2016-09-13 15:20:52 -0700

    refactor aria-tabs

--- js/components/aria-tabs.js
+++ js/components/aria-tabs.js
@@ -1,129 +1,56 @@
-$( document ).ready(function() {
-  // ARIA Tab Interface
-  // Thanks to Practical ARIA Examples
-  // http://heydonworks.com/practical_aria_examples/#tab-interface
+$(function() {
+  'use strict';
 
-  // The class for the container div
+  var d3 = require('d3');
+  var receptor = require('receptor');
 
-  var $container = '.tab-interface';
+  var tablist = d3.selectAll('[role=tablist]');
 
-  // The setup
-
-  $($container +' [role=tablist] li')
-    .attr('role', 'presentation');
-
-  $($container + ' [role=tablist] a[href^="#"]')
-    .attr({
-      'role': 'tab',
-      'tabindex': '-1'
-    })
-    // Make each aria-controls correspond id of targeted section (re href)
-    .each(function() {
-      $(this).attr(
-        'aria-controls', $(this).attr('href').substring(1)
-      );
-    });
-
-  // Make the first tab selected by default and allow it focus
-
-  $($container + ' [role="tablist"] li:first-child a')
-    .attr({
-      'aria-selected': 'true',
-      'tabindex': '0'
-    });
-
-  // Make first child of each panel focusable programmatically
-  $($container +' [role=tabpanel] > *:first-child')
-    .attr({
-      'tabindex': '0'
-    });
-
-  // Make all but the first section hidden (ARIA state and display CSS)
-
-  $($container + ' [role=tabpanel]:not(:first-of-type)')
-    .attr({
-      'aria-hidden': 'true'
-    });
-
-  // Change focus between tabs with arrow keys
-
-  $('[role=tab]').on('keydown', function(e) {
-
-    // define current, previous and next (possible) tabs
-
-    var $original = $(this);
-    var $prev = $(this).parents('li').prev().children('[role=tab]');
-    var $next = $(this).parents('li').next().children('[role=tab]');
-    var $target;
-
-    // find the direction (prev or next)
-
-    switch (e.keyCode) {
-      case 37:
-        $target = $prev;
-        break;
-      case 39:
-        $target = $next;
-        break;
-      default:
-        $target = false
-        break;
-    }
-
-    if ($target.length) {
-      $original.attr({
-        'tabindex': '-1',
-        'aria-selected': null
-      });
-      $target.attr({
-        'tabindex': '0',
-        'aria-selected': true
-      }).focus();
-    }
-
-    // Hide panels
-
-    $($container +' [role=tabpanel]')
-      .attr('aria-hidden', 'true');
-
-    // Show panel which corresponds to target
-
-    $('#' + $(document.activeElement).attr('href').substring(1))
-      .attr('aria-hidden', null);
-
-  });
-
-  // Handle click on tab to show + focus tabpanel
-
-  $('[role=tab]').on('click', function(e) {
-
+  var clickHandler = receptor.delegate('[role=tab]', function(e) {
+    // prevent link follows
     e.preventDefault();
-
-    // remove focusability [sic] and aria-selected
-
-    $(this).find('[role=tab]')
-      .attr({
-        'tabindex': '-1',
-        'aria-selected': null
-      });
-
-    // replace above on clicked tab
-
-    $(this).attr({
-      'aria-selected': true,
-      'tabindex': '0'
-    });
-
-    // Hide panels
-
-    $($container +' [role=tabpanel]')
-      .attr('aria-hidden', 'true');
-
-    // show corresponding panel
-
-    $('#' + $(this).attr('href').substring(1))
-      .attr('aria-hidden', null);
-
+    toggle(this);
   });
+
+  var toggle = function(tab) {
+    var list = tab.closest('[role=tablist]');
+    d3.select(list)
+      .selectAll('li')
+        .attr('role', 'presentation')
+        .select('[role=tab]')
+          .attr('aria-expanded', function() {
+            return this === tab;
+          })
+          .each(function() {
+            var panel = getPanelFor(this);
+            var active = this === tab;
+            if (panel) {
+              d3.select(panel)
+                .attr('tabindex', active ? 0 : -1)
+                .attr('aria-hidden', !active);
+            } else {
+              console.warn('no panel for:', this);
+            }
+          });
+  };
+
+  var getPanelFor = function(tab) {
+    var id = tab.getAttribute('aria-controls');
+    if (!id && tab.hasAttribute('href')) {
+      id = tab.getAttribute('href').substr(1);
+      tab.setAttribute('aria-controls', id);
+    }
+    return document.getElementById(id);
+  };
+
+  tablist
+    .on('click', function() {
+      clickHandler.call(this, d3.event);
+    })
+    .each(function() {
+      var tabs = this.querySelectorAll('[role=tab]');
+      var expanded = this.querySelector('[role=tab][aria-expanded=true]');
+      toggle(expanded || tabs[0]);
+    });
 
 });

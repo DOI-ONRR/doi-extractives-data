abd9358a653821c52551d8e9aebb312ce8a63a5f
Shawn Allen <shawn.allen@gsa.gov>
2016-09-14 09:53:08 -0700

    fix rollups by commodity + rev type

--- data/company-revenue/rollup.sql
+++ data/company-revenue/rollup.sql
@@ -1,33 +1,8 @@
 DELETE FROM company_revenue
 WHERE
+    company = 'All' OR
     commodity = 'All' OR
     revenue_type = 'All';
-
-INSERT INTO company_revenue
-(
-    year, company, revenue_type, commodity, revenue
-)
-SELECT
-    year, 'Total' AS company,
-    'All' AS revenue_type,
-    'All' AS commodity,
-    SUM(revenue) AS revenue
-FROM company_revenue
-GROUP BY year;
-
-INSERT INTO company_revenue
-(
-    year, company, revenue_type, commodity, revenue
-)
-SELECT
-    year, company,
-    'All' AS revenue_type,
-    commodity,
-    SUM(revenue) AS revenue
-FROM company_revenue
-WHERE
-    company != 'Total'
-GROUP BY year, company, commodity;
 
 INSERT INTO company_revenue
 (
@@ -39,7 +14,32 @@ SELECT
     'All' AS commodity,
     SUM(revenue) AS revenue
 FROM company_revenue
-WHERE
-    company != 'Total' AND
-    revenue_type != 'All'
 GROUP BY year, company, revenue_type;
+
+INSERT INTO company_revenue
+(
+    year, company, revenue_type, commodity, revenue
+)
+SELECT
+    year, company,
+    'All' AS revenue_type,
+    commodity,
+    SUM(revenue) AS revenue
+FROM company_revenue
+GROUP BY year, company, commodity;
+
+INSERT INTO company_revenue
+(
+    year, company, revenue_type, commodity, revenue
+)
+SELECT
+    year,
+    'All' AS company,
+    'All' AS revenue_type,
+    'All' AS commodity,
+    SUM(revenue) AS revenue
+FROM company_revenue
+WHERE
+    revenue_type = 'All' AND
+    commodity = 'All'
+GROUP BY year;

0add13ff8441b636b761ac89fa61de91d68bbd7e
gemfarmer <bshslowfood@gmail.com>
2016-09-14 11:37:28 -0500

    eric feedback tweeks, bug picking

--- js/components/eiti-tooltip.js
+++ js/components/eiti-tooltip.js
@@ -20,6 +20,10 @@
     return value + 'px';
   }
 
+  var hideTooltip = function (tooltip) {
+    tooltip.attr('aria-hidden', true);
+  }
+
   var attached = function() {
     var root = d3.select(this);
     var self = d3.select(this);
@@ -27,64 +31,66 @@
     var svgParent = d3.select(findParentSVG(this).parentElement);
     var parent = d3.select(this.parentElement).select('use');
 
+    var tooltip,
+      tooltipText;
 
-    var hideTooltip = function (tooltip) {
-      tooltip.attr('aria-hidden', true);
-    }
-
-    var update = function() {
-      var delay = 50;
-
-      var tooltip = svgParent.select('.eiti-tooltip');
+    var init = function(initialize) {
+      tooltip = svgParent.select('.eiti-tooltip');
 
       if (tooltip.empty()) {
         svgParent.append('div')
-        .classed('eiti-tooltip', true)
+          .classed('eiti-tooltip', true);
       }
 
       tooltip.attr('aria-label', function(){
           return self.attr('alt');
         })
-        .attr('aria-hidden', false)
-        .text('')
-        .transition()
-        .delay(delay);
+        .attr('aria-hidden', false);
 
-      var tooltipText = tooltip.select('p');
+      tooltipText = tooltip.select('p');
 
       if (tooltipText.empty()) {
-        tooltipText = tooltip.append('p')
+        tooltipText = tooltip.append('p');
       }
+
+      // clear <title> text
+      // if no javascript runs, <title> will serve as the tooltip
+      self.text('');
+
+      if (initialize) {
+        hideTooltip(tooltip);
+      }
+    }
+
+    var update = function() {
+
+      init();
 
       tooltipText.text(function(){
         return self.attr('desc');
       });
 
-      tooltip.style('left', function() {
-        var tooltipWidth = depixelize(tooltip.style('width'));
-        var svgWidth = depixelize(svg.style('width'));
+      tooltip
+        .style('left', function() {
+          var tooltipWidth = depixelize(tooltip.style('width'));
+          var svgWidth = depixelize(svg.style('width'));
 
-        if (svgWidth <= tooltipWidth + event.layerX) {
-          return pixelize(event.layerX - tooltipWidth);
-        } else {
-          return pixelize(event.layerX);
-        }
-      })
-      .style('top', function() {
-        var tooltipHeight = depixelize(tooltip.style('height'));
-        var svgHeight = depixelize(svg.style('height'));
+          if (svgWidth <= tooltipWidth + event.layerX) {
+            return pixelize(event.layerX - tooltipWidth);
+          } else {
+            return pixelize(event.layerX);
+          }
+        })
+        .style('top', function() {
+          var tooltipHeight = depixelize(tooltip.style('height'));
+          var svgHeight = depixelize(svg.style('height'));
 
-        if (svgHeight <= tooltipHeight + event.layerY) {
-          return pixelize(event.layerY - tooltipHeight);
-        } else {
-          return pixelize(event.layerY);
-        }
-      });
-
-      // hide automatically after 5 seconds
-      setTimeout(function() {
-        hideTooltip(tooltip);
-      }, 5000);
+          if (svgHeight <= tooltipHeight + event.layerY) {
+            return pixelize(event.layerY - tooltipHeight);
+          } else {
+            return pixelize(event.layerY);
+          }
+        });
     };
 
     var hide = function () {
@@ -93,6 +99,8 @@
         hideTooltip(tooltip);
       }
     }
+
+    init(this);
 
     parent.on('mouseover', update);
     svg.on('mouseout', hide);

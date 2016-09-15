5e355c1908dbad7c17c0b6edacb736898db7441b
Shawn Allen <shawn.allen@gsa.gov>
2016-09-15 13:52:51 -0700

    simplify paths; fix typo

--- js/src/state-pages.js
+++ js/src/state-pages.js
@@ -1,23 +1,23 @@
 (function(exports) {
   'use strict';
 
-  require('./../components/aria-toggle.js');
-  require('./../components/eiti-data-map.js');
-  require('./../components/bar-chart-table.js');
-  require('./../components/eiti-bar-chart.js');
-  require('./../components/year-switcher-section.js');
-  require('./../components/eiti-data-map-table.js');
-  require('./../components/eiti-tooltip-wrapper.js');
+  require('../components/aria-toggle.js');
+  require('../components/eiti-data-map.js');
+  require('../components/bar-chart-table.js');
+  require('../components/eiti-bar-chart.js');
+  require('../components/year-switcher-section.js');
+  require('../components/eiti-data-map-table.js');
+  require('../components/eiti-tooltip-wrapper.js');
 
-  require('./../components/aria-tabs.js');
+  require('../components/aria-tabs.js');
 
-  require('./../components/sticky.js');
+  require('../components/sticky.js');
 
-  var OpenListNav = require('./../components/open-list-nav.js');
+  var OpenListNav = require('../components/open-list-nav.js');
 
   // exporting instance of OpenListNav because openListNav is
   // referenced in the markup:
-  // _includes/hash_selecor.html
+  // _includes/hash_selector.html
   exports.openListNav = new OpenListNav();
 
 })(window);

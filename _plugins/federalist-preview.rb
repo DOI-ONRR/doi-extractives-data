a717a9d0882b0460f37fe90c295623e71b926572
Shawn Allen <shawn.allen@gsa.gov>
2016-09-16 17:27:13 -0700

    single quotes

--- _plugins/federalist-preview.rb
+++ _plugins/federalist-preview.rb
@@ -3,8 +3,8 @@ module Jekyll
     def generate(site)
       # if $BRANCH is set (and it's not `master`), set `site.url` to the
       # Federalist preview URL for this branch.
-      branch = ENV["BRANCH"]
-      if branch && branch != "master"
+      branch = ENV['BRANCH']
+      if branch && branch != 'master'
         site.config['url'] = "https://federalist.18f.gov/preview/18F/doi-extractives-data/#{branch}"
         puts "*** Federalist URL: <#{site.config['url']}> ***"
       end

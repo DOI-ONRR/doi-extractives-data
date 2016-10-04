17353817bbbae258aca935d150acc015e24ee329
gemfarmer <bshslowfood@gmail.com>
2016-10-04 10:59:50 -0500

    added stuck functionality

--- js/components/sticky.js
+++ js/components/sticky.js
@@ -2,10 +2,12 @@
   'use strict';
 
   var sticky = require('stickyfill')();
+  var allStickies = [];
 
   [].forEach.call(
     document.querySelectorAll('.sticky'),
     function(el) {
+      allStickies.push(el);
       sticky.add(el);
       var isNav = el.classList.contains('sticky_nav');
       var preSticky = document.createElement('div');
@@ -17,6 +19,22 @@
       }
     }
   );
+
+  var watch = function() {
+    allStickies.forEach(function(sticky, i) {
+      var atTop = Number(sticky.getBoundingClientRect().top) === 0;
+      var isStuck = sticky.classList.contains('stuck');
+
+      if (atTop && !isStuck) {
+        sticky.classList.add('stuck');
+      } else if (!atTop && isStuck) {
+        sticky.classList.remove('stuck');
+      }
+    });
+  }
+
+  window.addEventListener('scroll', eiti.util.throttle(watch, 100));
+  window.addEventListener('resize', eiti.util.throttle(watch, 100));
 
   exports.stickyfill = sticky;
 

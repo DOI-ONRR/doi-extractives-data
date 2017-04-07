8105ca6564058131824ac28f7e68cdb516479316
gemfarmer <bshslowfood@gmail.com>
2017-04-07 10:38:11 -0500

    migrate to federalist.fr.cloud.gov

--- _plugins/federalist-preview.rb
+++ _plugins/federalist-preview.rb
@@ -5,7 +5,7 @@ module Jekyll
       # Federalist preview URL for this branch.
       branch = ENV['BRANCH']
       if branch && branch != 'master'
-        site.config['url'] = "https://federalist.18f.gov/preview/18F/doi-extractives-data/#{branch}"
+        site.config['url'] = "https://federalist.fr.cloud.gov//preview/18F/doi-extractives-data/#{branch}"
         puts "*** Federalist URL: <#{site.config['url']}> ***"
       end
     end

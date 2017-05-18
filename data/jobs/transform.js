1aa3450f608cc904ad71b806670928cd6d30c763
Shawn Allen <shawn.allen@gsa.gov>
2017-05-18 11:27:37 -0700

    better initialization of jobs column values

--- data/jobs/transform.js
+++ data/jobs/transform.js
@@ -1,11 +1,8 @@
-module.exports = {
-  year:       'year',
-  naics:      'naics',
-  commodity:  'commodity',
-  region_id:  () => null,
-  state:      'state',
-  county:     'county',
-  fips:       'fips',
-  jobs:       'jobs',
-  percent:    () => 0.01,
+module.exports = d => {
+  // pass through the other fields, and create columns for the following
+  // ones with types that match these values:
+  d.region_id = 'XX';
+  d.total = 1;
+  d.percent = 0.01;
+  return d;
 };

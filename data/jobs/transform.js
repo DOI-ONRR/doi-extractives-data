a3d80e4a98eff6767b120adadf5ea92e633ce08e
Shawn Allen <shawn.allen@gsa.gov>
2017-05-17 17:03:54 -0700

    reorder fields

--- data/jobs/transform.js
+++ data/jobs/transform.js
@@ -2,10 +2,10 @@ module.exports = {
   year:       'year',
   naics:      'naics',
   commodity:  'commodity',
+  region_id:  () => null,
   state:      'state',
   county:     'county',
   fips:       'fips',
   jobs:       'jobs',
   percent:    () => 0.01,
-  region_id:  () => null,
 };

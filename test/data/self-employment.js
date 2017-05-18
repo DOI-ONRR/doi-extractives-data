4f55f72dec004ef2a0bff3fb7a648dcda7a1fdd9
Shawn Allen <shawn.allen@gsa.gov>
2017-05-18 15:29:18 -0700

    nest tests under each year; clean up

--- test/data/self-employment.js
+++ test/data/self-employment.js
@@ -57,24 +57,25 @@ describe('self employment data', function() {
     '../../_data/county_jobs'
   );
 
-  var years = getDirectories(pivotSourceParent);
+  var years = getDirectories(pivotSourceParent)
+    .filter(function(dirname) {
+      return dirname.match(/^20\d\d$/);
+    });
+
   var countyJobs = fs.readdirSync(countyJobsPath)
     .filter(function(filename) {
       return filename.match(/^[A-Z]{2}\.yml$/);
     });
-  var pivotSource = {};
-  var i;
-  var year;
 
-  for (i = 0; i < years.length; i++) {
-    year = years[i];
-    pivotSource[year] = path.join(
+  var pivotSource = years.reduce(function(acc, year) {
+    acc[year] = path.join(
       __dirname,
       '../../data/jobs/bls/',
       year,
       'joined.tsv'
     );
-  }
+    return acc;
+  }, {});
 
   var selfEmployment = yaml.safeLoad(
     fs.readFileSync(dataSource, 'utf8')
@@ -82,197 +83,190 @@ describe('self employment data', function() {
 
   var countySelfEmployment = {};
 
-  _.each(countyJobs, function(region) {
+  countyJobs.forEach(function(region) {
     var regionEmployment = path.join(countyJobsPath, region);
     countySelfEmployment[region.split('.')[0]] = yaml.safeLoad(
       fs.readFileSync(regionEmployment, 'utf8')
     );
   });
 
-  for (i = 0; i < years.length; i++) {
-    year = years[i];
+  years.forEach(function(year) {
 
-    it(year + ' state rollups', function(done) {
-      load(pivotSource[year], 'tsv', function(error, rows) {
+    describe(year, function() {
 
-        var actual = {};
-        var expected = {};
+      it('state rollups', function(done) {
+        load(pivotSource[year], 'tsv', function(error, rows) {
 
-        rows
-          .filter(function(d) {
-            return !d.County;
-          })
-          .forEach(function(d) {
-            var state = states[d.State];
+          var actual = {};
+          var expected = {};
 
-            var expectedByState = selfEmployment[state];
+          rows
+            .filter(function(d) {
+              return !d.county;
+            })
+            .forEach(function(d) {
+              var state = states[d.state];
 
-            if (expectedByState) {
-              if (+d.Jobs) {
-                if (actual[state]) {
-                  actual[state] += +d.Jobs;
+              var expectedByState = selfEmployment[state];
+
+              if (expectedByState) {
+                if (+d.jobs) {
+                  if (actual[state]) {
+                    actual[state] += +d.jobs;
+                  } else {
+                    actual[state] = +d.jobs;
+                  }
+                }
+                if (expectedByState[d.year]) {
+                  expected[state] = expectedByState[d.year].count;
                 } else {
-                  actual[state] = +d.Jobs;
+                  console.warn('no data for:', d.state);
                 }
               }
-              if (expectedByState[d.Year]) {
-                expected[state] = expectedByState[d.Year].count;
-              } else {
-                console.warn('no data for:', d, expectedByState);
+            });
+
+          for (var state in actual) {
+            var difference = Math.abs(+actual[state] - +expected[state]);
+            assert.ok(
+              difference <= 0,
+              state + ': ' + actual[state] + ' != ' + expected[state]
+                + ' (' + JSON.stringify(expected) + ')'
+            );
+          }
+
+          done();
+        });
+      });
+
+      it('county data', function(done) {
+
+        load(pivotSource[year], 'tsv', function(error, rows) {
+
+          var actual = {};
+          actual.count = {};
+          actual.percent = {};
+
+          var expected = {};
+          expected.count = {};
+          expected.percent = {};
+
+          var round = 1;
+
+          rows.forEach(function(d) {
+            var state = states[d.state];
+
+            var expectedByState = countySelfEmployment[state];
+
+            if (expectedByState) {
+              try {
+                if (d.County) {
+                  var expectedByCounty = expectedByState[d.FIPS];
+                  if (+d.jobs) {
+                    if (actual.count[d.FIPS]) {
+                      actual.count[d.FIPS] += +d.jobs;
+                    } else {
+                      actual.count[d.FIPS] = +d.jobs;
+                    }
+                  }
+
+                  if (+d.Share) {
+                    if (actual.percent[d.FIPS]) {
+                      actual.percent[d.FIPS] += +d.Share * 100;
+                    } else {
+                      actual.percent[d.FIPS] = +d.Share * 100;
+                    }
+                  }
+                  // console.log(expectedByCounty)
+                  expected.count[d.FIPS] = expectedByCounty
+                    .employment[year].count;
+                  expected.percent[d.FIPS] = expectedByCounty
+                    .employment[year].percent;
+                }
+              } catch (error) {
+                assert.ok(false, 'no data for: ' + JSON.stringify(d));
               }
             }
           });
 
-        for (var state in actual) {
-          var difference = Math.abs(+actual[state] - +expected[state]);
-          assert.ok(
-            difference <= 0,
-            (actual[state] + ' != ' + expected[state])
-          );
-        }
+          for (var metric in actual) {
+            for (var fips in actual[metric]) {
 
-        done();
-      });
-    });
+              var difference = Math.abs(
+                +actual[metric][fips] - +expected[metric][fips]
+              );
 
-
-    it(year + ' county data', function(done) {
-
-      load(pivotSource[year], 'tsv', function(error, rows) {
-
-        var actual = {};
-        actual.count = {};
-        actual.percent = {};
-
-        var expected = {};
-        expected.count = {};
-        expected.percent = {};
-
-        var round = 1;
-
-        rows.forEach(function(d) {
-          var state = states[d.State];
-
-          var expectedByState = countySelfEmployment[state];
-
-          if (expectedByState) {
-            try {
-              if (d.County) {
-                var expectedByCounty = expectedByState[d.FIPS];
-                if (+d.Jobs) {
-                  if (actual.count[d.FIPS]) {
-                    actual.count[d.FIPS] += +d.Jobs;
-                  } else {
-                    actual.count[d.FIPS] = +d.Jobs;
-                  }
-                }
-
-                if (+d.Share) {
-                  if (actual.percent[d.FIPS]) {
-                    actual.percent[d.FIPS] += +d.Share * 100;
-                  } else {
-                    actual.percent[d.FIPS] = +d.Share * 100;
-                  }
-                }
-                // console.log(expectedByCounty)
-                expected.count[d.FIPS] = expectedByCounty
-                  .employment[year].count;
-                expected.percent[d.FIPS] = expectedByCounty
-                  .employment[year].percent;
+              if (metric === 'percent') {
+                round = 0.01;
+              } else {
+                round = 1;
               }
-            } catch (error) {
-              assert.ok(false, 'no data for: ' + JSON.stringify(d));
+              assert.ok(
+                difference <= round,
+                [
+                  metric, actual[metric][fips], '!=', expected[metric][fips],
+                  'for:', [fips, year].join('/')
+                ].join(' ')
+              );
             }
           }
+
+          done();
         });
-
-        for (var metric in actual) {
-          for (var fips in actual[metric]) {
-
-            var difference = Math.abs(
-              +actual[metric][fips] - +expected[metric][fips]
-            );
-
-            if (metric === 'percent') {
-              round = 0.01;
-            } else {
-              round = 1;
-            }
-            assert.ok(
-              difference <= round,
-              [
-                metric, actual[metric][fips], '!=', expected[metric][fips],
-                'for:', [fips, year].join('/')
-              ].join(' ')
-            );
-          }
-        }
-
-        done();
       });
+
+      var test = (
+        "data doesn't contain values that aren't in the pivot table"
+      );
+
+      it(test, function(done) {
+        var actualCount;
+        var actualPercent;
+        var found;
+        var state;
+        var fips;
+
+        var filter = function(d) {
+          return d.state === statesInverted[state] &&
+                d.year === year &&
+                d.fips === fips;
+        };
+
+        load(pivotSource[year], 'tsv', function(error, rows) {
+          for (state in countySelfEmployment) {
+            for (fips in countySelfEmployment[state]) {
+              if (countySelfEmployment[state][fips].employment[year]) {
+                actualCount = countySelfEmployment[state][fips]
+                  .employment[year]
+                  .count;
+                actualPercent = countySelfEmployment[state][fips]
+                  .employment[year]
+                  .percent;
+                found = rows.filter(filter);
+              }
+              assert.equal(
+                found.length, 1,
+                'wrong row count: ' + found.length +
+                ' for: ' + [state, fips, year].join('/')
+              );
+
+              var expectedCount = found[0].jobs;
+              var differenceCount = expectedCount - actualCount;
+              assert.ok(
+                Math.abs(differenceCount) <= 1,
+                [
+                  'wrong count:', actualCount, '!=', expectedCount,
+                  '\n' + [state, fips, year].join('/')
+                ].join(' ')
+              );
+            }
+          }
+
+          done();
+        });
+      });
+
     });
 
-    var test = year + " data doesn't contain values that aren't " +
-               'in the pivot table';
-    it(test, function(done) {
-      var actualCount;
-      var actualPercent;
-      var found;
-      var state;
-      var fips;
+  });
 
-      var filter = function(d) {
-        return d.State === statesInverted[state] &&
-               d.Year === year &&
-               d.FIPS === fips;
-      };
-
-
-      load(pivotSource[year], 'tsv', function(error, rows) {
-        for (state in countySelfEmployment) {
-          for (fips in countySelfEmployment[state]) {
-            if (countySelfEmployment[state][fips].employment[year]) {
-              actualCount = countySelfEmployment[state][fips]
-                .employment[year]
-                .count;
-              actualPercent = countySelfEmployment[state][fips]
-                .employment[year]
-                .percent;
-              found = rows.filter(filter);
-            }
-            assert.equal(
-              found.length, 1,
-              'wrong row count: ' + found.length +
-              ' for: ' + [state, fips, year].join('/')
-
-            );
-
-            var expectedCount = found[0].Jobs;
-            var expectedPercent = found[0].Share * 100;
-
-            var differenceCount = expectedCount - actualCount;
-            var differencePercent = expectedPercent - actualPercent;
-            assert.ok(
-              Math.abs(differenceCount) <= 1,
-              [
-                'wrong count:', actualCount, '!=', expectedCount,
-                '\n' + [state, fips, year].join('/')
-              ].join(' ')
-            );
-
-            assert.ok(
-              Math.abs(differencePercent) <= 1,
-              [
-                'wrong percent:', actualPercent, '!=', expectedPercent,
-                '\n' + [state, fips, year].join('/')
-              ].join(' ')
-            );
-          }
-        }
-
-        done();
-      });
-    });
-
-  }
 });

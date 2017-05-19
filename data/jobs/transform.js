8a60d1581984c4182b7681736356070214f0bee2
Shawn Allen <shawn.allen@gsa.gov>
2017-05-19 15:30:23 -0700

    hack to coerce FIPS codes to strings

--- data/jobs/transform.js
+++ data/jobs/transform.js
@@ -2,5 +2,7 @@ module.exports = d => {
   // pass through the other fields, and create columns for the following
   // ones with types that match these values:
   d.region_id = 'XX';
+  // this is a trick to coerce the FIPS codes to strings;
+  d.fips = 'FIPS' + d.fips;
   return d;
 };

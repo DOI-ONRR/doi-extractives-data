c28b3b626cd802aa05b2c6b63c65f786ce084b63
Shawn Allen <shawn.allen@gsa.gov>
2017-05-19 10:24:07 -0700

    nix total + percent coercions

--- data/jobs/transform.js
+++ data/jobs/transform.js
@@ -2,7 +2,5 @@ module.exports = d => {
   // pass through the other fields, and create columns for the following
   // ones with types that match these values:
   d.region_id = 'XX';
-  d.total = 1;
-  d.percent = 0.01;
   return d;
 };

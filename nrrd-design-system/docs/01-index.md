b5cea098c3798332aabeceb67f9574821ebe75f5
Marco <marco.segreto@gsa.gov>
2017-11-16 12:13:05 -0800

    Setup basic fractal styleguide
    The styleguide is in the `nrrd-design-system` folder. You can watch
    changes to the styleguide with `watch-styleguide` which gives you
    a local server to use. On federalist, the styleguide is built into
    static files and moved from `nrrd-design-system/public` to
    `./styleguide` where it's served by jekyll. So the fractal site will
    be available at `...gov/styleguide`.

---
+++ nrrd-design-system/docs/01-index.md
@@ -0,0 +1,5 @@
+---
+title: Nrrd Design System
+---
+
+This is your index page. You can edit its contents at `docs/01-index.hbs`
---
+++ nrrd-design-system/fractal.js
@@ -0,0 +1,32 @@
+'use strict';
+
+/*
+* Require the path module
+*/
+const path = require('path');
+
+/*
+ * Require the Fractal module
+ */
+const fractal = module.exports = require('@frctl/fractal').create();
+
+/*
+ * Give your project a title.
+ */
+fractal.set('project.title', 'NRRD Design System');
+
+/*
+ * Tell Fractal where to look for components.
+ */
+fractal.components.set('path', path.join(__dirname, 'components'));
+
+/*
+ * Tell Fractal where to look for documentation pages.
+ */
+fractal.docs.set('path', path.join(__dirname, 'docs'));
+
+/*
+ * Tell the Fractal web preview plugin where to look for static assets.
+ */
+fractal.web.set('static.path', path.join(__dirname, 'public'));
+fractal.web.set('builder.dest', __dirname + '/build');

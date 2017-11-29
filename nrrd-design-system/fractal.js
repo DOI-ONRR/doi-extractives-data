a7ed61d08bc3a03b6c5d591cbc06af1dc59a3be5
Eric Ronne <eric.ronne@gsa.gov>
2017-11-28 19:05:04 -0600

    Adds status labels woo!

--- nrrd-design-system/fractal.js
+++ nrrd-design-system/fractal.js
@@ -1,32 +1,53 @@
 'use strict';
 
-/*
-* Require the path module
-*/
 const path = require('path');
 
-/*
- * Require the Fractal module
- */
 const fractal = module.exports = require('@frctl/fractal').create();
 
-/*
- * Give your project a title.
- */
 fractal.set('project.title', 'NRRD Design System');
 
-/*
- * Tell Fractal where to look for components.
- */
 fractal.components.set('path', path.join(__dirname, 'components'));
+fractal.components.set('statuses', {
+  blocked: {
+    label: "Blocked",
+    description: "Blocked.",
+    color: "#be4900"
+  },
+  review: {
+    label: "Ready for review",
+    description: "Ready for another team member to review.",
+    color: "#f4b400"
+  },
+  draft: {
+    label: "In progress",
+    description: "Work in progress.",
+    color: "#ccc"
+  },
+  ready: {
+    label: "Ready",
+    description: "Accessibility review finished, documentation complete.",
+    color: "#0baf00"
+  },
+  deprecated: {
+    label: "Deprecated",
+    description: "Component exists only for backwards compatibility.",
+    color: "#8e8e8e"
+  }
+});
 
-/*
- * Tell Fractal where to look for documentation pages.
- */
 fractal.docs.set('path', path.join(__dirname, 'docs'));
+fractal.docs.set('statuses', {
+  draft: {
+    label: "Draft",
+    description: "Work in progress. There may be gaps in this documentation.",
+    color: "#f4b400"
+  },
+  ready: {
+    label: "Ready",
+    description: "Reviewed and considered reasonably complete.",
+    color: "#0baf00"
+  }
+});
 
-/*
- * Tell the Fractal web preview plugin where to look for static assets.
- */
 fractal.web.set('static.path', path.join(__dirname, 'public'));
 fractal.web.set('builder.dest', __dirname + '/build');

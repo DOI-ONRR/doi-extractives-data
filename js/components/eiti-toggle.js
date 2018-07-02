3e22c48e31842ffb8eb58ecb96177235d2306523
brentryanjohnson <brent.johnson@onrr.gov>
2018-07-02 13:56:37 -0700

    Tweaks image paths in js

--- js/components/eiti-toggle.js
+++ js/components/eiti-toggle.js
@@ -8,8 +8,8 @@
   };
 
   var innerMarkup = {
-    bars: '<img class="icon icon-bars" src="public/img/icons/icon-menu.svg" alt="Menu">',
-    x: '<img class="icon icon-close-x" src="public/img/icons/icon-close-x.svg" alt="Close menu">'
+    bars: '<img class="icon icon-bars" src="/public/img/icons/icon-menu.svg" alt="Menu">',
+    x: '<img class="icon icon-close-x" src="/public/img/icons/icon-close-x.svg" alt="Close menu">'
   };
 
   var EXPANDED = 'aria-expanded';

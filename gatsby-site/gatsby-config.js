7dda0581073b464f4f9ecd05fd2f568f9a4d44c9
Jeff Keene <jeff.keene@onrr.gov>
2018-07-02 15:21:32 -0400

    updated path prefix

--- gatsby-site/gatsby-config.js
+++ gatsby-site/gatsby-config.js
@@ -2,7 +2,7 @@ module.exports = {
   // Note: it must *not* have a trailing slash.
   // This is currently the realtive path in our Jekyl deployment. This path points to our Gatsby Pages. 
   // This prefix is appended to load all our related images and code.
-  pathPrefix: `/gatsby-deploy`,
+  pathPrefix: `/preview/onrr/doi-extractives-data/gatsby-dev/gatsby-deploy`,
   siteMetadata: {
     title: 'Natural Resources Revenue Data',
     description: 'This site provides open data about natural resource management on federal lands and waters in the United States, including oil, gas, coal, and other extractive industries.',

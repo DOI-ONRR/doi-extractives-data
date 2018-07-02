0958d2dfa9712939577adcec6d79a9f059d64cf4
Jeff Keene <jeff.keene@onrr.gov>
2018-07-02 17:00:59 -0400

    hard coded preview for prefix config, replaced layotu with none

--- gatsby-site/src/layouts/index.js
+++ gatsby-site/src/layouts/index.js
@@ -1,17 +1,26 @@
 import React from 'react';
-//import Helmet from 'react-helmet';
+import Helmet from 'react-helmet';
 //import { connect } from 'react-redux';
-//import Banner from '../components/Banner';
-//import Header from '../components/Header';
-//import Footer from '../components/Footer';
+import Banner from '../components/Banner';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 import "../styles/_main.scss";
 
 export default ({ data, children}) => {
   return (
     <div>
-    
+      <Helmet
+        title="Home | Natural Resources Revenue Data"
+        meta={[
+          { name: 'og:description', content: 'This site provides open data about natural resource management on federal lands and waters in the United States, including oil, gas, coal, and other extractive industries.' },
+          { name: 'twitter:description', content: 'This site provides open data about natural resource management on federal lands and waters in the United States, including oil, gas, coal, and other extractive industries.' },
+        ]}
+      />
+      <Banner />
+      <Header siteMetadata={data.site.siteMetadata} />
       {children()}
+      <Footer contactInfo={data.dataYaml} siteMetadata={data.site.siteMetadata} />
       
     </div>
   );
@@ -42,20 +51,3 @@ export const query = graphql`
   }
 `;
 
-
-/*
-
--- Removed common components until we no longer need jekyll
-
-<Helmet
-        title="Home | Natural Resources Revenue Data"
-        meta={[
-          { name: 'og:description', content: 'This site provides open data about natural resource management on federal lands and waters in the United States, including oil, gas, coal, and other extractive industries.' },
-          { name: 'twitter:description', content: 'This site provides open data about natural resource management on federal lands and waters in the United States, including oil, gas, coal, and other extractive industries.' },
-        ]}
-      />
-      <Banner />
-      <Header siteMetadata={data.site.siteMetadata} />
-      {children()}
-      <Footer contactInfo={data.dataYaml} siteMetadata={data.site.siteMetadata} />
-*/

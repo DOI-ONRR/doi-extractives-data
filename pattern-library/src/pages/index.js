cb85aa806b4e9cf9d12534bf4161ff058b3eccf5
Aaron D Borden <aaron.borden@gsa.gov>
2018-07-24 14:35:57 -0700

    Add a link to components
    react-router-dom doesn't know about the Gatsby path prefix, so when built with
    Jekyll, the Redirect doesn't work because /patterns/ doesn't match `/`.
    Federalist's preview route also causes some issues.

--- pattern-library/src/pages/index.js
+++ pattern-library/src/pages/index.js
@@ -1,10 +1,15 @@
 import React from "react"
 import { Route, Redirect } from "react-router-dom"
+import Link from 'gatsby-link';
 
+// TODO react-router-dom doesn't know about the Gatsby path prefix
 class Home extends React.Component {
   render() {
     return (
-      <Route exact path="/" render={() => <Redirect to="/components/" />} />
+      <div>
+        <Route exact path="/" render={() => <Redirect to="/components/" />} />
+        <Link to="/components/">Go to components</Link>
+      </div>
     )
   }
 }

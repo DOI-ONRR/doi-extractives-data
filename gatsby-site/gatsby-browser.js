5ad738f654996c0438c3f58d293036685a08a3de
Jeff Keene <jeff.keene@onrr.gov>
2018-07-26 23:18:14 -0400

    wip

--- gatsby-site/gatsby-browser.js
+++ gatsby-site/gatsby-browser.js
@@ -18,19 +18,6 @@ exports.replaceRouterComponent = ({ history }) => {
 };
 
 exports.onClientEntry = () => {
-  // Check for a custom pathname
-  const pathname = global.___pathname
-  if (!pathname) return
-
-  // Override the history location
-  const history = global.___history
-  history.location.pathname = pathname
-
   // Patch the resource loader
-  const loader = global.___loader
-  const { getResourcesForPathname } = loader
-
-  loader.getResourcesForPathname = (path, ...args) => {
-    return getResourcesForPathname(path === location.pathname ? pathname : path, ...args)
-  }
+  const loader = global.___loader.prefetcher.onPostLoadPageResources = () => {}
 }

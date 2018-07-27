fbd140b63653a527750741e4c92284ae9ab1d0d9
Jeff Keene <jeff.keene@onrr.gov>
2018-07-27 00:32:43 -0400

    wip

--- gatsby-site/gatsby-browser.js
+++ gatsby-site/gatsby-browser.js
@@ -16,7 +16,7 @@ exports.replaceRouterComponent = ({ history }) => {
 
   return ConnectedRouterWrapper;
 };
-let inInitialRender = true;
+
 exports.onClientEntry = () => {
   // Patch the resource loader
   const loader = global.___loader;
@@ -24,46 +24,6 @@ exports.onClientEntry = () => {
   const { getResourcesForPathname } = loader
 
   loader.getResourcesForPathname = (path, cb = () => {}) => {
-	    if (
-	      inInitialRender &&
-	      navigator &&
-	      navigator.serviceWorker &&
-	      navigator.serviceWorker.controller &&
-	      navigator.serviceWorker.controller.state === `activated`
-	    ) {
-	      // If we're loading from a service worker (it's already activated on
-	      // this initial render) and we can't find a page, there's a good chance
-	      // we're on a new page that this (now old) service worker doesn't know
-	      // about so we'll unregister it and reload.
-	      if (!findPage(path)) {
-	        navigator.serviceWorker
-	          .getRegistrations()
-	          .then(function(registrations) {
-	            // We would probably need this to
-	            // prevent unnecessary reloading of the page
-	            // while unregistering of ServiceWorker is not happening
-	            if (registrations.length) {
-	              for (let registration of registrations) {
-	                registration.unregister()
-	              }
-	              window.location.reload()
-	            }
-	          })
-	      }
-	    }
-	    inInitialRender = false
-	    // In development we know the code is loaded already
-	    // so we just return with it immediately.
-
-		const page = findPage(path)
-		if (!page) return cb()
-		const pageResources = {
-			component: syncRequires.components[page.componentChunkName],
-			json: syncRequires.json[page.jsonName],
-			layout: syncRequires.layouts[page.layout],
-			page,
-		}
-		cb(pageResources)
-		return pageResources;
+	    return cb()
 	}
 }

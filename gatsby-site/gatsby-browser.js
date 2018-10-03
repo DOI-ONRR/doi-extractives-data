68d5bd9fa7ec69e7d746e83b95da89368f809dc8
Jeff Keene <jeff.keene@onrr.gov>
2018-10-03 13:54:16 -0400

    added all states

--- gatsby-site/gatsby-browser.js
+++ gatsby-site/gatsby-browser.js
@@ -19,8 +19,57 @@ exports.replaceRouterComponent = ({ history }) => {
 
 
 const usStateIds =[
-  "AK"
+  "AK",
+  "AL",
+  "AR",
+  "AZ",
+  "CA",
+  "CO",
+  "CT",
+  "DC",
+  "DE",
+  "FL",
+  "GA",
+  "HI",
+  "IA",
+  "IL",
+  "IN",
+  "KS",
+  "KY",
+  "LA",
+  "MA",
+  "MD",
+  "ME",
+  "MI",
+  "MN",
+  "MO",
+  "MS",
+  "MT",
+  "NC",
+  "ND",
+  "NE",
+  "NH",
+  "NJ",
+  "NM",
+  "NV",
+  "NY",
+  "OH",
+  "OK",
+  "OR",
+  "PA".
+  "RI",
+  "SC",
+  "SD",
+  "TN",
+  "TX",
+  "UT",
+  "VA",
+  "VT",
+  "WA",
+  "WI",
+  "WV"
 ];
+
 exports.onClientEntry = () => {
   // Patch the resource loader
   const loader = global.___loader;

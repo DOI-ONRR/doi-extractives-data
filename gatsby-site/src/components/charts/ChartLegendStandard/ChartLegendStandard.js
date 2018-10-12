5aff3085b30a4cec29bdea1afb404ffe636bb33e
Jeff Keene <jeff.keene@onrr.gov>
2018-10-12 11:43:15 -0400

    added chart legend

---
+++ gatsby-site/src/components/charts/ChartLegendStandard/ChartLegendStandard.js
@@ -0,0 +1,109 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
+
+import utils from '../../../js/utils';
+
+import styles from "./ChartLegendStandard.module.css"
+
+
+/**
+ * Chart Legend Standard is used to display rows of data keys and values. Each data key can be 
+ * associated with a format function, a color(using css) and a display name.
+ */
+class ChartLegendStandard extends React.Component {
+
+	getKeyDisplayName(key){
+		if(this.props.displayNames && this.props.displayNames[key]) {
+			if(typeof this.props.displayNames[key].displayName === 'function') {
+				return this.props.displayNames[key].displayName();
+			}
+			else
+			{
+				return this.props.displayNames[key].displayName;
+			}
+		}
+		return key;
+	}
+
+	render() {
+		console.log(this.props.chartHeader)
+		let keys = (this.props.displayNames)? Object.keys(this.props.displayNames) : Object.keys(this.props.chartData[0]);
+		let total = 0;
+		return(
+			<table className={styles.chartLegendStandard}>
+				<thead>
+				{this.props.chartHeader &&
+            		<tr>
+	            		<th colSpan="2">{this.props.chartHeader[0]}</th>
+	            		<th>{this.props.chartHeader[1]}</th>
+	            	</tr>
+				}
+            	</thead>
+				<tbody>
+					{
+						keys.map((key, index) => {
+							if(this.props.chartData[0][key]){
+								total += this.props.chartData[0][key];
+								return(
+									<tr key={key}  >
+										<td>
+											<div className={"chart-legend-"+index} />
+										</td>
+										<td>
+											{this.getKeyDisplayName(key)}:
+										</td>
+										<td>
+											{ this.props.dataFormatFunc ?
+												this.props.dataFormatFunc(this.props.chartData[0][key])
+												:
+												this.props.chartData[0][key]
+											}
+										</td>
+									</tr>
+								);
+							}
+						})
+					}
+					<tr className={styles.chartLegendStandard_TotalRow}>
+						<td colSpan="2">
+							Total:
+						</td>
+						<td>
+							{ this.props.dataFormatFunc ?
+								this.props.dataFormatFunc(total)
+								:
+								total
+							}
+						</td>
+					</tr>
+					{this.props.additionalData && 
+						this.props.additionalData.map((data, index) => {
+							return (
+								<tr key={index} className="chart-legend-additional-data-row">
+									<td />
+									<td>{data.name}:</td>
+									<td>{data.value}</td>
+								</tr>)
+						})
+					}
+				</tbody>
+			</table>
+		);
+	}
+}
+
+ChartLegendStandard.propTypes = {
+	/** Array 1 to 2 values */
+	chartHeader: PropTypes.array,
+	/** Array of key value pairs */
+	chartData: PropTypes.array.isRequired,
+	/** Defines the display name/info of the data keys in the chart. Keys should match the data keys */
+	displayNames: PropTypes.object,
+	/** A function that will be applied on each data item of the chart data */
+	dataFormatFunc: PropTypes.func,
+	/** Specify a class name to be added to the outer element */
+	className: PropTypes.string
+}
+
+export default ChartLegendStandard;
---
+++ gatsby-site/src/components/charts/ChartLegendStandard/ChartLegendStandard_README.md
@@ -0,0 +1,28 @@
+---
+title: Chart Legend Standard
+componentName: ChartLegendStandard
+patternCategory: Charts
+---
+
+```
+<ChartLegendStandard chartHeader={["Source", "2017 (bbl)"]} chartData={[{'Onshore':1257456, 'Offshore': 3454054, 'GOMESA':3256897}]} ></ChartLegendStandard>
+```
+
+This is the standard chart legend used on the site. 
+
+## How to use
+
+The legend can be used with any chart to display the values of each fo the data points with a calculated sum for the total. This component uses css modules and has default settings. 
+
+### Chart Data:
+This is an array of objects that will be used to populate the legend
+
+### Display Names: 
+The developer can specify a name to display other than the default key for the data
+
+### Format Data Function:
+You can pass a format function that will be applied to each data value in the chart data.
+
+### CSS for colors:
+This can be overriden by passing in a set of colors to be used with each key in the legend. 
+

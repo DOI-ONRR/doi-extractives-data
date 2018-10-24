66660d3d2ee060f08382f6f0b42afb74a0f15d15
Jeff Keene <jeff.keene@onrr.gov>
2018-10-23 21:08:43 -0400

    added the inital tabordion component

---
+++ gatsby-site/src/components/layouts/Tabordion/Tabordion.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import styles from "./Tabordion.module.css";
+
+class Tabordion extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+    	tabId: this.props.children[0].props.id,
+      tabPanel: this.props.children[0].props.children,
+    };
+  }
+
+	tabClicked(id, content){
+		if(this.state.tabId !== id) {
+			this.setState({tabId: id, tabPanel: content});
+		}
+	}
+
+  render() {
+    return (
+      <div className={styles.root+" container-page-wrapper"}>
+        <div className={styles.tabContainer}>
+	        {this.props.children &&
+	        	React.Children.map(this.props.children, (child) => {
+	        		if(child.type.name === "Tab") {
+	        			return <Tab callBack={this.tabClicked.bind(this)} {...child.props} />;
+	        		}
+	        	})
+	        }
+        </div>
+      	<div className={styles.tabPanelContainer}>
+      		{this.state.tabPanel}
+      	</div>
+
+      </div>
+    );
+  }
+};
+
+export const Tab = (props) => {
+	return (
+	  <div id={props.id} className={styles.tab} onClick={() => props.callBack(props.id, props.children)}>
+	  	{(props.name || "Tabsdss")}
+	  </div>
+	);
+} 
+
+Tab.propTypes = {
+	 /** The Id for the element, used to ensure expandable containers have unique Ids. */
+  id: PropTypes.string.isRequired,
+}
+
+Tabordion.propTypes = {
+
+}
+
+export default Tabordion;
--- gatsby-site/src/pages/index.js
+++ gatsby-site/src/pages/index.js
@@ -4,6 +4,7 @@ import { connect } from 'react-redux';
 
 import {KeyStatsSection} from '../components/sections/KeyStatsSection';
 import {WhatsNew} from '../components/sections/WhatsNew';
+import {Tabordion, Tab} from '../components/layouts/Tabordion';
 
 class HomePage extends React.Component {
 
@@ -19,6 +20,21 @@ class HomePage extends React.Component {
             ]}
 
             />
+
+        <Tabordion>
+          <Tab id="tab-overview" name="Overview"> 
+            <p>When companies extract natural resources on federal lands and offshore areas, they pay bonuses, rent, and royalties to the federal government. The government distributes these funds for public use in a variety of ways.</p>
+          </Tab>
+          <Tab id="tab-production" name="Production"> 
+            <p>The United States is among the world's top producers of natural gas, oil, and coal. The U.S. is also a global leader in renewable energy production. We have data for energy and mineral production on federal lands and waters and Native American lands and energy production for all owners.</p>
+          </Tab>
+          <Tab id="tab-revenue" name="Revenue"> 
+            <p>Companies pay a wide range of fees, rates, and taxes to extract natural resources in the U.S. The amounts differ depending on the ownership of the resources. They are usually called "revenue" because they represent revenue to the American public.</p>
+          </Tab>
+          <Tab id="tab-disbursements" name="Disbursements"> 
+            <p>After collecting revenue from natural resource extraction, the Office of Natural Resources Revenue (ONRR) distributes that money to different agencies, funds, and local governments for public use. This process is called “disbursement.”</p>
+          </Tab>
+        </Tabordion>
 
         <KeyStatsSection></KeyStatsSection>
 

5b766eb66f6805759bb61e085ec48915a4f92e21
Jeff Keene <jeff.keene@onrr.gov>
2018-10-30 23:34:18 -0400

    added accordion functionality

--- gatsby-site/src/components/layouts/Tabordion/Tabordion.js
+++ gatsby-site/src/components/layouts/Tabordion/Tabordion.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
+import MediaQuery from 'react-responsive';
 
 import styles from "./Tabordion.module.css";
 
@@ -13,37 +15,80 @@ class Tabordion extends React.Component {
     };
   }
 
-	tabClicked(id, content){
+	tabClicked(id, content, element){
 		if(this.state.tabId !== id) {
 			this.setState({tabId: id, tabPanel: content});
+			//console.log(this.refs);
 		}
 	}
 
   render() {
     return (
-      <div className={styles.root+" container-page-wrapper"}>
-        <div className={styles.tabContainer}>
-	        {this.props.children &&
-	        	React.Children.map(this.props.children, (child,index) => {
-	        		return <Tab key={index} isSelected={(this.state.tabId === child.props.id)} callBack={this.tabClicked.bind(this)} {...child.props} />;
-	        	})
-	        }
-        </div>
-      	<div className={styles.tabPanelContainer}>
-      		{this.state.tabPanel}
-      	</div>
+    	<div>
+      	<MediaQuery minWidth={481}>
+	      	<div className={styles.root+" container-page-wrapper"}>
+		        <div className={styles.tabContainer}>
+			        {this.props.children &&
+			        	React.Children.map(this.props.children, (child,index) => {
+			        		return <Tab key={index} isSelected={(this.state.tabId === child.props.id)} callBack={this.tabClicked.bind(this)} {...child.props} />;
+			        	})
+			        }
+		        </div>
+		      	<div className={styles.tabPanelContainer}>
+		      		{this.state.tabPanel}
+		      	</div>
+		      </div>
+	      </MediaQuery>
+      	<MediaQuery maxWidth={480}>
+	      	<div className={styles.root+" container-page-wrapper"}>
+		        <div className={styles.tabContainer}>
+			        {this.props.children &&
+			        	React.Children.map(this.props.children, (child,index) => {
+			        		let isSelected = (this.state.tabId === child.props.id);
 
-      </div>
+			        		if(isSelected) {
+			        			return (
+			        				<div>
+												<Tab key={index} isAccordion={true} isSelected={(this.state.tabId === child.props.id)} callBack={this.tabClicked.bind(this)} {...child.props} />
+								      	<div className={styles.tabPanelContainer+" "+styles.accordionPanelContainer}>
+								      		{this.state.tabPanel}
+								      	</div>
+								      </div>
+			        			);
+			        		}
+			        		else {
+			        			return <Tab key={index} isAccordion={true} isSelected={(this.state.tabId === child.props.id)} callBack={this.tabClicked.bind(this)} {...child.props} />;
+			        		}
+
+			        	})
+			        }
+
+		        </div>
+		      </div>
+	      </MediaQuery>
+	    </div>
     );
   }
 };
 
-export const Tab = (props) => {
-	return (
-	  <div id={props.id} className={props.isSelected ? styles.tabSelected : styles.tab} onClick={() => props.callBack(props.id, props.children)}>
-	  	{(props.name || "Tabsdss")}
-	  </div>
-	);
+export class Tab extends React.Component {
+  componentDidMount() {
+  	if(this.props.isSelected && this.props.isAccordion){
+  		window.scrollTo(0, this.node.offsetTop-5, {behavior:"smooth"})
+  	}
+  }
+
+	render() {
+		return (
+		  <div id={this.props.id} 
+		  			ref={node => this.node = node} 
+		  			className={this.props.isSelected ? styles.tabSelected+" "+styles.tab : styles.tab} 
+		  			onClick={() => this.props.callBack(this.props.id, this.props.children)}>
+		  	{(this.props.name || "Tabsdss")}
+		  </div>
+		);
+	}
+
 } 
 
 Tab.propTypes = {

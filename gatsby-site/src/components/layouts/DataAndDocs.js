6a46d1aaa9a28aed5be00eeed008bbbaad4165e9
brentryanjohnson <brent.johnson@onrr.gov>
2018-11-02 16:08:05 -0700

    Adds hover state to icon for testing

--- gatsby-site/src/components/layouts/DataAndDocs.js
+++ gatsby-site/src/components/layouts/DataAndDocs.js
@@ -2,7 +2,7 @@ import React from 'react';
 import DataAndDocIcon from '-!svg-react-loader!../../img/svg/icon-data-and-docs-circle.svg';
 
 const DataAndDocs = (props) => ( 
-    <span>
+    <span className="icon-link">
         <DataAndDocIcon />
         {props.children === undefined ?
             'Downloads and documentation' :

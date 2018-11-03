82dcc4ca07963f55469f8987490869a7281bc427
brentryanjohnson <brent.johnson@onrr.gov>
2018-11-02 17:55:03 -0700

    Changes 'tribal' to 'native american' in map legend in accordance with our style guide

--- gatsby-site/src/components/maps/FederalLandOwnershipLegend.js
+++ gatsby-site/src/components/maps/FederalLandOwnershipLegend.js
@@ -4,7 +4,7 @@ const FederalLandOwnershipLegend = (props) => (
     <div className="legend-ownership-svg">
     <ul className="cells">
         <li className="cell"><span className="swatch"></span>Federal{props.land && " land"}</li>
-        <li className="cell"><span className="swatch"></span>Tribal{props.land && " land"}</li>
+        <li className="cell"><span className="swatch"></span>Native American{props.land && " land"}</li>
         <li className="cell"><span className="swatch"></span>Other (state, local, or private{props.land && " land"})</li>
     </ul>
     </div>

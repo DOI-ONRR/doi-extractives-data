75fdc74e6c0605c1ebba6f327d46d190d64b90e6
brentryanjohnson <brent.johnson@onrr.gov>
2018-11-05 08:35:39 -0800

    Consistency of plural usage for 'lands'

--- gatsby-site/src/components/maps/FederalLandOwnershipLegend.js
+++ gatsby-site/src/components/maps/FederalLandOwnershipLegend.js
@@ -4,8 +4,8 @@ const FederalLandOwnershipLegend = (props) => (
     <div className="legend-ownership-svg">
     <ul className="cells">
         <li className="cell"><span className="swatch"></span>Federal{props.land && " lands and waters"}</li>
-        <li className="cell"><span className="swatch"></span>Native American{props.land && " land"}</li>
-        <li className="cell"><span className="swatch"></span>Other{props.land && " land"} (state, local, or private)</li>
+        <li className="cell"><span className="swatch"></span>Native American{props.land && " lands"}</li>
+        <li className="cell"><span className="swatch"></span>Other{props.land && " lands"} (state, local, or private)</li>
     </ul>
     </div>
 );

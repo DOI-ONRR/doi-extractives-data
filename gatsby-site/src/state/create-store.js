b8e69a6bacaf73f287ea7b5354dce02047d922dd
Jeff Keene <jeff.keene@onrr.gov>
2018-11-06 14:13:44 -0500

    rebuild branch remove console logs

--- gatsby-site/src/state/create-store.js
+++ gatsby-site/src/state/create-store.js
@@ -7,18 +7,11 @@ import rootReducer from './reducers';
 
 
 export default () => {  
-  console.log("******* Starting Create Store *********");
-
 	const devtools = 
 		(process.env.NODE_ENV === 'development' && window.devToolsExtension) ? 
 			window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 		: 
 			f => f;
 
-	const store = createStore(rootReducer, devtools);
-
-	console.log("Store State: ", store.getState());
-
-	console.log("******* Finished Create Store *********");
-	return store;
+	return createStore(rootReducer, devtools);
 };

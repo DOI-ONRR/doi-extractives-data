10c2fc9ceb4160efb52e207e2ab1789e8189e9d9
Jeff Keene <jeff.keene@onrr.gov>
2018-11-20 16:01:57 -0500

    fix offshore link on map

--- gatsby-site/src/components/maps/OffshoreRegionsMap.js
+++ gatsby-site/src/components/maps/OffshoreRegionsMap.js
@@ -8,7 +8,7 @@ const OffshoreRegionsMap = (props) => {
         <g id="OffshoreRegionsMap">
             {props.offshore_regions.map((item, index) => (
                 <g key={index}>
-                    <a xlinkHref={ withPrefix(item.offshore_region.frontmatter.permalink) } href={ item.offshore_region.frontmatter.permalink }>
+                    <a xlinkHref={ withPrefix(item.offshore_region.frontmatter.permalink) } href={ withPrefix(item.offshore_region.frontmatter.permalink) }>
                         <g className="state offshore-area feature" fill="true">
                             <title>{item.offshore_region.frontmatter.title }</title>
                             <use xlinkHref={withPrefixSVG("/maps/offshore/all.svg#")+item.offshore_region.frontmatter.unique_id }  aria-label={ item.offshore_region.frontmatter.title }></use>

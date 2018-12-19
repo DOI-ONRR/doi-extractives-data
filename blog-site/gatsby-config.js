fb344ac8880ad6c2568103dd4985b1ca2f9775d4
brentryanjohnson <brent.johnson@onrr.gov>
2018-12-18 20:23:41 -0800

    Updates blog packages, adds autolink feature for headings

--- blog-site/gatsby-config.js
+++ blog-site/gatsby-config.js
@@ -39,6 +39,7 @@ module.exports = {
       resolve: `gatsby-transformer-remark`,
       options: {
         plugins: [
+          `gatsby-remark-autolink-headers`,
           {
             resolve: `gatsby-remark-images`,
             options: {

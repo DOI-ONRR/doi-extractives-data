d7ebf4d4cbf2325d982a8c42dcfd3c5462fd1961
brentryanjohnson <brent.johnson@onrr.gov>
2019-02-12 11:28:32 -0800

    Initial work on shutdown alert component in gatsby

--- gatsby-site/src/layouts/index.js
+++ gatsby-site/src/layouts/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Helmet from 'react-helmet';
 
 import {Banner} from '../components/layouts/Banner';
+import {AlertBanner} from '../components/layouts/AlertBanner';
 import {Header} from '../components/layouts/Header';
 import {Footer} from '../components/layouts/Footer';
 import Glossary from '../components/utils/Glossary';
@@ -62,6 +63,7 @@ export default ({ data, children}) => {
           </noscript>
         </Helmet>
         <Banner />
+        <AlertBanner />
         <Header siteMetadata={data.site.siteMetadata} />
 
         <main>

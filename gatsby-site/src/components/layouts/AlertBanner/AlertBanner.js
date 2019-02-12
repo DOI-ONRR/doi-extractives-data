69fb10cf02572d131c2c432dc4e3e7dbc2522b90
brentryanjohnson <brent.johnson@onrr.gov>
2019-02-12 15:02:12 -0800

    Adds component for shutdown notice, but icon path is broken

---
+++ gatsby-site/src/components/layouts/AlertBanner/AlertBanner.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import styles from "./AlertBanner.module.css";
+/*import alert from "../../../img/icons/icon-alert.svg";*/
+import AlertIcon from '-!svg-react-loader!../../../img/svg/icon-alert.svg';
+
+const AlertBanner = () => (
+  <section className={styles.root}>
+    <span><img className={styles.alertImage} src={AlertIcon} /> Due to the government shutdown, we're unable to update this site. We will resume work on the site when the shutdown ends.</span>
+  </section>
+);
+
+export default AlertBanner;

e3b06606fdbc799c41d4f027e011ac22c07283ac
brentryanjohnson <brent.johnson@onrr.gov>
2019-04-02 14:20:59 -0700

    Adds link to downloads

--- gatsby-site/src/components/sections/WhatsNew/WhatsNew.js
+++ gatsby-site/src/components/sections/WhatsNew/WhatsNew.js
@@ -9,7 +9,7 @@ const WhatsNew = (props) => (
 			<h2>What's new</h2>
 			<p>In our latest release on April 2, 2019, we made the following changes:</p>
         <ul className="list-bullet ribbon-card-top-list">
-          <li>Updated fiscal year production data file for FY2018.</li>
+          <li>Updated <Link to="/downloads/federal-production/">fiscal year production data file for 2018</Link>.</li>
         </ul>
       <p>Review our <a href="https://github.com/ONRR/doi-extractives-data/releases">full release details</a>.</p>
 		</div>

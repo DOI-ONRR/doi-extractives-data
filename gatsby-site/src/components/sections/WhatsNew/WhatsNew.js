63562bf6919bde51f81fdf6306c38dace1de02b7
brentryanjohnson <brent.johnson@onrr.gov>
2019-04-02 14:40:19 -0700

    Updates release date

--- gatsby-site/src/components/sections/WhatsNew/WhatsNew.js
+++ gatsby-site/src/components/sections/WhatsNew/WhatsNew.js
@@ -7,7 +7,7 @@ const WhatsNew = (props) => (
   <section className={styles.root+" slab-delta"}>
   	<div className="container-page-wrapper">
 			<h2>What's new</h2>
-			<p>In our latest release on April 2, 2019, we made the following changes:</p>
+			<p>In our latest release on April 3, 2019, we made the following changes:</p>
         <ul className="list-bullet ribbon-card-top-list">
           <li>Updated <Link to="/downloads/federal-production/">fiscal year production data file for 2018</Link>.</li>
         </ul>

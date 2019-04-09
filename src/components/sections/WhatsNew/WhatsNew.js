134f037a135c819b439fb3adf3cf64baadd6e37d
brentryanjohnson <brent.johnson@onrr.gov>
2019-04-08 17:53:17 -0700

    Updates whats new

--- src/components/sections/WhatsNew/WhatsNew.js
+++ src/components/sections/WhatsNew/WhatsNew.js
@@ -7,12 +7,11 @@ const WhatsNew = props => (
   <section className={styles.root + ' slab-delta'}>
   	<div className="container-page-wrapper">
       <h2>What's new</h2>
-      <p>In our latest release on March 13, 2019, we made the following changes:</p>
+      <p>In our latest release on April 9, 2019, we made the following changes:</p>
       <ul className="list-bullet ribbon-card-top-list">
-        <li>Updated monthly production and revenue data</li>
-        <li>Published a <Link to="/blog/becoming-a-product-manager/">new blog post about a unique path to digital product management</Link></li>
-        <li>Updated the status of the <Link to="/how-it-works/land-and-water-conservation-fund/">Land and Water Conservation Fund</Link> after new legislation was signed into law</li>
-        <li>Updated <Link to="/how-it-works/coal-excise-tax/">Coal Excise Tax rates</Link> and replaced a chart image with tabular data for accessibility</li>
+        <li>Updated site codebase</li>
+        <li>Updated production data download file to FY2018</li>
+        <li>Added download file for monthly disbursements</li>
       </ul>
       <p>Review our <a href="https://github.com/ONRR/doi-extractives-data/releases">full release details</a>.</p>
     </div>

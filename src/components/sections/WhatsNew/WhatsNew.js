80e096f2501a9bebe370c14a7ff994bb5955c617
Jennifer Malcolm <30806839+jennmalcolm@users.noreply.github.com>
2019-04-15 09:25:03 -0400

    Update what's new

--- src/components/sections/WhatsNew/WhatsNew.js
+++ src/components/sections/WhatsNew/WhatsNew.js
@@ -7,11 +7,10 @@ const WhatsNew = props => (
   <section className={styles.root + ' slab-delta'}>
   	<div className="container-page-wrapper">
       <h2>What's new</h2>
-      <p>In our latest release on April 9, 2019, we made the following changes:</p>
+      <p>In our latest release on April 15, 2019, we made the following changes:</p>
       <ul className="list-bullet ribbon-card-top-list">
-        <li>Updated site codebase</li>
-        <li>Updated production data download file to FY2018</li>
-        <li>Added download file for monthly disbursements</li>
+        <li>Added <Link to="explore/revenue/">new revenue data table</Link></li>
+        <li>Updated monthly graphs and download files</li>
       </ul>
       <p>Review our <a href="https://github.com/ONRR/doi-extractives-data/releases">full release details</a>.</p>
     </div>

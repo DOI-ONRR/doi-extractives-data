ca024d37765ec922dd716cc802d31f55a249116f
brentryanjohnson <brent.johnson@onrr.gov>
2019-04-30 16:40:00 -0700

    Transform content for oh for

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import Layout from '../components/layouts/DefaultLayout'
+import Rig404 from '-!svg-react-loader!../img/svg/rig-404.svg'
 
 const NotFoundPage = () => (
   <Layout>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <div class="case_studies-map-container">
+      <Rig404 />
+      <h1>There's nothing here.</h1>
+      <p>If we're missing something, please let us know at <a href="mailto:nrrd@onrr.gov">nrrd@onrr.gov</a>, or find other contact options below 👇.</p>
+    </div>  
   </Layout>
 )
 

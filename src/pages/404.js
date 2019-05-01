4d15b15fc5c8499e9683fa82c6f0deb57f07b63c
brentryanjohnson <brent.johnson@onrr.gov>
2019-04-30 19:13:48 -0700

    Styling

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,10 +4,12 @@ import Rig404 from '-!svg-react-loader!../img/svg/rig-404.svg'
 
 const NotFoundPage = () => (
   <Layout>
-    <div class="case_studies-map-container">
+    <div class="ribbon-card">
       <Rig404 />
+      <div>
       <h1>There's nothing here.</h1>
       <p>If we're missing something, please let us know at <a href="mailto:nrrd@onrr.gov">nrrd@onrr.gov</a>, or find other contact options below 👇.</p>
+      </div>
     </div>  
   </Layout>
 )

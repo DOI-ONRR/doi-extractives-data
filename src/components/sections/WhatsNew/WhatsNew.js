a5bde2898f26caf62bca00c18f7ba411ddfaf089
brentryanjohnson <brent.johnson@onrr.gov>
2019-05-15 12:19:34 -0700

    Adds to release notes

--- src/components/sections/WhatsNew/WhatsNew.js
+++ src/components/sections/WhatsNew/WhatsNew.js
@@ -12,7 +12,10 @@ const WhatsNew = props => (
         <li>Updated <Link to="/how-it-works/federal-revenue-by-company/2018/">company revenue data to 2018</Link></li>
         <li>Updated <Link to="/explore/revenue/">revenue detail table</Link> for improved functionality</li>
         <li>Updated <Link to="/how-it-works/renewables/">renewables content</Link></li>
+        <li>Added revenue trends section</li>
         <li>Added Native American production data to homepage production charts</li>
+        <li>Updated monthly production data (now through January 2019)</li>
+        <li>Updated monthly revenue data (now through April 2019)</li>
       </ul>
       <p>Review our <a href="https://github.com/ONRR/doi-extractives-data/releases">full release details</a>.</p>
     </div>

da874e2a5e4617c200cbf38245e66ff9b46d872c
echang97 <edward.chang@onrr.gov>
2019-06-11 12:43:27 -0400

    Testing Ascending Sort

--- src/pages/explore/index.js
+++ src/pages/explore/index.js
@@ -205,7 +205,7 @@ export const query = graphql`
     }
     states_data:allMarkdownRemark (filter:{fileAbsolutePath: {regex: "/states/"}}) {
       sort: {
-        fields: [frontmatter__title]
+        fields: [frontmatter___title]
         order: ASC
       }
       states:edges {

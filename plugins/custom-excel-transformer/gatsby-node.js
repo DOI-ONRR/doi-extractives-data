c68e233c215cda5ec9baefe1f336a89fea3dcf33
Jeffery Keene <Jeff.Keene@master12studio.com>
2019-06-12 09:31:03 -0400

    wip

--- plugins/custom-excel-transformer/gatsby-node.js
+++ plugins/custom-excel-transformer/gatsby-node.js
@@ -1,6 +1,6 @@
 /**
  *
- * This plugin is called from the gatsby api after the initial node creation and  
+ * This plugin is called from the gatsby api after the initial node creation and
  * before the queries are run on the pages. This is where we add application ready nodes
  * created from the raw graphql nodes that get created from our source data files.
  * This is run during the build process and provides the application graphql nodes that
@@ -16,63 +16,65 @@
  * Constants that are only used by this plugin
  **/
 const DATA_TRANSFORMER_CONSTANTS = {
-  PRODUCTION_VOLUMES_EXCEL :'ProductionVolumesXlsx__Sheet1',
-  PRODUCTION_VOLUMES_FY:'FederalProductionFy2009201820190326Xlsx__FyFederalProductionVolumes',
-  REVENUES_MONTHLY_EXCEL :'MonthlyRevenueXlsx__MontlyRev',
-  FEDERAL_DISBURSEMENTS_EXCEL:'DisbursementsXlsx__Data',
-  FEDERAL_REVENUE_FY:'FederalRevenueAcctYearFy061820181213Xlsx__FederalRevenuesFy06Fy18',
-  NATIVE_AMERICAN_REVNUE_FY: 'NativeAmericanRevenuesXlsx__NativeAmericanRevenues'
+  PRODUCTION_VOLUMES_EXCEL: 'productionvolumesxlsx__sheet1',
+  PRODUCTION_VOLUMES_FY: 'federalproductionfy2009201820190326xlsx__fyfederalproductionvolumes',
+  REVENUES_MONTHLY_EXCEL: 'monthlyrevenuexlsx__data',
+  FEDERAL_DISBURSEMENTS_EXCEL: 'disbursementsxlsx__data',
+  FEDERAL_REVENUE_FY: 'federalrevenueacctyearfy061820181213xlsx__federalrevenuesfy06fy18',
+  NATIVE_AMERICAN_REVNUE_FY: 'nativeamericanrevenuesxlsx__nativeamericanrevenues'
 }
 
 const TRANSFORMER_NODE_TYPES = [
-	DATA_TRANSFORMER_CONSTANTS.PRODUCTION_VOLUMES_EXCEL,
-	DATA_TRANSFORMER_CONSTANTS.PRODUCTION_VOLUMES_FY,
-	DATA_TRANSFORMER_CONSTANTS.FEDERAL_DISBURSEMENTS_EXCEL,
-	DATA_TRANSFORMER_CONSTANTS.REVENUES_MONTHLY_EXCEL,
-	DATA_TRANSFORMER_CONSTANTS.FEDERAL_REVENUE_FY,
-	DATA_TRANSFORMER_CONSTANTS.NATIVE_AMERICAN_REVNUE_FY
-];
+  DATA_TRANSFORMER_CONSTANTS.PRODUCTION_VOLUMES_EXCEL,
+  DATA_TRANSFORMER_CONSTANTS.PRODUCTION_VOLUMES_FY,
+  DATA_TRANSFORMER_CONSTANTS.FEDERAL_DISBURSEMENTS_EXCEL,
+  DATA_TRANSFORMER_CONSTANTS.REVENUES_MONTHLY_EXCEL,
+  DATA_TRANSFORMER_CONSTANTS.FEDERAL_REVENUE_FY,
+  DATA_TRANSFORMER_CONSTANTS.NATIVE_AMERICAN_REVNUE_FY
+]
 
-const productionVolumesTransformer = require('./data-transformers/production-volumes-transformer');
-const revenuesTransformer = require('./data-transformers/revenues-transformer');
-const federalDisbursementsTransformer = require('./data-transformers/federal-disbursements-transformer');
+const productionVolumesTransformer = require('./data-transformers/production-volumes-transformer')
+const revenuesTransformer = require('./data-transformers/revenues-transformer')
+const federalDisbursementsTransformer = require('./data-transformers/federal-disbursements-transformer')
 
-async function onCreateNode(
-	{ node, actions, createNodeId, createContentDigest},
+async function onCreateNode (
+  { node, actions, createNodeId, createContentDigest },
   options = {}
 ) {
-	const { createNode } = actions;
+  const { createNode } = actions
 
-	if(TRANSFORMER_NODE_TYPES.includes(node.internal.type)) {
-		let newNode;
-		switch(node.internal.type) {
-			case DATA_TRANSFORMER_CONSTANTS.PRODUCTION_VOLUMES_EXCEL:
-				newNode = productionVolumesTransformer(node, 'ProductVolumes');
-				break;
-			case DATA_TRANSFORMER_CONSTANTS.PRODUCTION_VOLUMES_FY:
-				newNode = productionVolumesTransformer(node, 'ProductVolumesFiscalYear');
-				break;
-			case DATA_TRANSFORMER_CONSTANTS.FEDERAL_DISBURSEMENTS_EXCEL:
-				newNode = federalDisbursementsTransformer(node);
-				break;
-			case DATA_TRANSFORMER_CONSTANTS.REVENUES_MONTHLY_EXCEL:
-				newNode = revenuesTransformer(node, 'ResourceRevenuesMonthly');
-				break;
-			case DATA_TRANSFORMER_CONSTANTS.FEDERAL_REVENUE_FY:
-				newNode = revenuesTransformer(node, 'ResourceRevenuesFiscalYear');
-				break;
-			case DATA_TRANSFORMER_CONSTANTS.NATIVE_AMERICAN_REVNUE_FY:
-				newNode = revenuesTransformer(node, 'ResourceRevenuesFiscalYear');
-				break;
-		}
+  if (TRANSFORMER_NODE_TYPES.includes(node.internal.type.toLowerCase())) {
+    let newNode
 
-		newNode.id = createNodeId(node.id);
-		newNode.parent = node.id;
-		newNode.children = [];
-	  newNode.internal.contentDigest = createContentDigest(newNode);
+    switch (node.internal.type.toLowerCase()) {
+    case DATA_TRANSFORMER_CONSTANTS.PRODUCTION_VOLUMES_EXCEL:
+      newNode = productionVolumesTransformer(node, 'ProductVolumes')
+      break
+    case DATA_TRANSFORMER_CONSTANTS.PRODUCTION_VOLUMES_FY:
+      newNode = productionVolumesTransformer(node, 'ProductVolumesFiscalYear')
+      break
+    case DATA_TRANSFORMER_CONSTANTS.FEDERAL_DISBURSEMENTS_EXCEL:
+      newNode = federalDisbursementsTransformer(node)
+      break
+    case DATA_TRANSFORMER_CONSTANTS.REVENUES_MONTHLY_EXCEL:
+      console.log("Monthly revenue")
+      newNode = revenuesTransformer(node, 'ResourceRevenuesMonthly')
+      break
+    case DATA_TRANSFORMER_CONSTANTS.FEDERAL_REVENUE_FY:
+      newNode = revenuesTransformer(node, 'ResourceRevenuesFiscalYear')
+      break
+    case DATA_TRANSFORMER_CONSTANTS.NATIVE_AMERICAN_REVNUE_FY:
+      newNode = revenuesTransformer(node, 'ResourceRevenuesFiscalYear')
+      break
+    }
 
-		createNode(newNode);
-	}
+    newNode.id = createNodeId(node.id)
+    newNode.parent = node.id
+    newNode.children = []
+	  newNode.internal.contentDigest = createContentDigest(newNode)
+
+    createNode(newNode)
+  }
 }
 
-exports.onCreateNode = onCreateNode 
+exports.onCreateNode = onCreateNode

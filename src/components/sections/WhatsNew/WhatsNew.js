dbd86dbc736709d210baa9eb236fa6ae2b5f5e7a
Jennifer Malcolm <30806839+jennmalcolm@users.noreply.github.com>
2019-06-12 13:44:35 -0400

    what's new

--- src/components/sections/WhatsNew/WhatsNew.js
+++ src/components/sections/WhatsNew/WhatsNew.js
@@ -7,10 +7,12 @@ const WhatsNew = props => (
   <section className={styles.root + ' slab-delta'}>
   	<div className="container-page-wrapper">
       <h2>What's new</h2>
-      <p>In our latest release on May 28, 2019, we made the following changes:</p>
+      <p>In our latest release on June 13, 2019, we made the following changes:</p>
       <ul className="list-bullet ribbon-card-top-list">
-        <li>Updated <Link to="downloads/federal-revenue-by-location/">revenue data downloads to include 2003-2018 data</Link></li>
-        <li>Updated revenue trends section styling and changed to a 10-year rolling trend line </li>
+        <li>Added csv option for all <Link to="downloads/">downloads</Link></li>
+        <li>Updated <Link to="downloads/federal-production/">calendar year production file to include 2003-2018 data</Link></li>
+        <li>Updated homepage graphs with monthly revenue through May 2019 and production through February 2019 </li>
+        <li>Updated <Link to="downloads/federal-disbursements-by-month/"> monthly disbursements file through April 2019 </li>
       </ul>
 
       <p>Review our <a href="https://github.com/ONRR/doi-extractives-data/releases">full release details</a>.</p>

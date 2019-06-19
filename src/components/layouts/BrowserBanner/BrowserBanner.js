e6fed4fb8a27895c4072317506e341a6377eea12
brentryanjohnson <brent.johnson@onrr.gov>
2019-06-19 10:17:00 -0700

    Corrects whitespace

--- src/components/layouts/BrowserBanner/BrowserBanner.js
+++ src/components/layouts/BrowserBanner/BrowserBanner.js
@@ -7,7 +7,7 @@ const BrowserBanner = () => (
     <span>
       <p>>We try to make this website work for everyone, but some older web browsers don’t display every feature on this site.</p> 
 
-       <p>If it looks like something isn't working as it should, try using a different browser, such as Chrome, Edge, or Firefox.</p>
+      <p>If it looks like something isn't working as it should, try using a different browser, such as Chrome, Edge, or Firefox.</p>
     </span>
   </section>
 )

fd36eb1a8918f2b3a6a69d645e186421bca56300
brentryanjohnson <brent.johnson@onrr.gov>
2019-06-19 17:48:55 -0700

    Removes redundant title tag from CountyMap component

--- src/components/maps/CountyMap.js
+++ src/components/maps/CountyMap.js
@@ -135,7 +135,6 @@ const CountyMap = props => {
 			                            data-value={countyData[1].products[props.productKey].volume[props.year]}
 			                            data-year-values={JSON.stringify(countyData[1].products[props.productKey].volume)}
 			                            >
-			                            <title>{ countyData[1].name }</title>
 			                            <use xlinkHref={usStateSVG + '#county-' + countyData[0]}></use>
 			                        </g>
 			                    )

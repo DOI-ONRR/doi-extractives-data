6114dec7565f47d0e388259d7cd8f01a755b8b0a
Jennifer Malcolm <30806839+jennmalcolm@users.noreply.github.com>
2019-07-24 17:43:18 -0400

    Updating dates
    changing blog and release dates to tomorrow

--- src/components/sections/WhatsNew/WhatsNew.js
+++ src/components/sections/WhatsNew/WhatsNew.js
@@ -7,7 +7,7 @@ const WhatsNew = props => (
   <section className={styles.root + ' slab-delta'}>
   	<div className="container-page-wrapper">
       <h2>What's new</h2>
-      <p>In our latest release on July 24, 2019, we made the following changes:</p>
+      <p>In our latest release on July 25, 2019, we made the following changes:</p>
       <ul className="list-bullet ribbon-card-top-list">
         <li>Published <Link to="blog/intern-blog-post/">blog post on our summer intern's experiences and contributions</Link></li>
         <li>Added <Link to="archive-page/archive/">an archive page</Link> for content we no longer maintain</li>

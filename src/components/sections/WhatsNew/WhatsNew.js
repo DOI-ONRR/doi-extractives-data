782254f9888afca80011fdcbbda97d946a58b3bd
brentryanjohnson <brent.johnson@onrr.gov>
2019-07-25 10:18:16 -0700

    Fixes link to archive

--- src/components/sections/WhatsNew/WhatsNew.js
+++ src/components/sections/WhatsNew/WhatsNew.js
@@ -10,7 +10,7 @@ const WhatsNew = props => (
       <p>In our latest release on July 25, 2019, we made the following changes:</p>
       <ul className="list-bullet ribbon-card-top-list">
         <li>Published <Link to="blog/intern-blog-post/">blog post on our summer intern's experiences and contributions</Link></li>
-        <li>Added <Link to="archive-page/archive/">an archive page</Link> for content we no longer maintain</li>
+        <li>Added <Link to="archive">an archive page</Link> for content we no longer maintain</li>
       </ul>
 
       <p>Review our <a href="https://github.com/ONRR/doi-extractives-data/releases">full release details</a>.</p>

01bb8b64e0645916feaddd9d15f41f5e17d557bf
Jeff Keene <Jeff.Keene@onrr.gov>
2019-08-20 13:29:35 -0400

    ally fix for glossary

--- src/components/utils/Glossary.js
+++ src/components/utils/Glossary.js
@@ -11,7 +11,7 @@ import GLOSSARY_TERMS from '../../data/terms.yml'
 class GlossaryItem extends React.Component {
   state = {
     toggle: this.props.toggle || false,
-    show: this.props.show || true,
+    show: this.props.term.show || true,
   };
 
   onClickHandler (e) {
@@ -25,9 +25,17 @@ class GlossaryItem extends React.Component {
 
   render () {
     return (
-      <li className="glossary-click glossary-item" aria-hidden={!this.state.show} data-accordion-item aria-expanded={this.state.toggle} onClick={this.onClickHandler.bind(this)}>
+      <li
+        className="glossary-click glossary-item"
+        aria-hidden={!this.state.show}
+        data-accordion-item aria-expanded={this.state.toggle}
+        onClick={this.onClickHandler.bind(this)}>
         <h2 className="glossary-click glossary-term">{this.props.term.name}</h2>
-        <button className="glossary-click" data-accordion-button role="button" aria-controls="glossary-accordion--content--2" ><span className="glossary-click sr-only">Toggle for {this.props.term.name}</span></button>
+        <button
+          className="glossary-click"
+          data-accordion-button role="button"
+          aria-controls="glossary-accordion--content--2"
+          tabIndex={this.state.show && -1}><span className="glossary-click sr-only">Toggle for {this.props.term.name}</span></button>
         <p data-accordion-content className="glossary-click glossary-definition accordion-content" aria-hidden={!this.state.toggle}>{this.props.term.definition}</p>
       </li>
     )
@@ -82,15 +90,26 @@ class Glossary extends React.Component {
     return (
       <div id="glossary" className="drawer glossary-click" aria-describedby="glossary-result" aria-hidden={this.state.toggleHidden}>
         <div className="glossary-click container">
-          <button id="glossary-toggle" className=" glossary-click button button--close toggle" onClick={this.onCloseHandler.bind(this)}>
+          <button
+            id="glossary-toggle"
+            className=" glossary-click button button--close toggle"
+            onClick={this.onCloseHandler.bind(this)}
+            tabIndex={this.state.toggleHidden && -1}>
             <label htmlFor="glossary-toggle" className="glossary-click sr-only">Close glossary</label><div className="icon-close-x"></div>
           </button>
         </div>
 
         <h1 className="h2 drawer-header glossary-click"><GlossaryIcon/> Glossary </h1>
         <label htmlFor="drawer-search-bar" className="glossary-click label">Filter glossary terms</label>
-        <input id="drawer-search-bar" className="glossary-click js-glossary-search drawer-search" type="search" placeholder="e.g. Fossil fuel"
-          value={this.state.glossaryTerm} onChange={this.handleChange.bind(this)} />
+        <input
+          id="drawer-search-bar"
+          title="Glossary Term Search"
+          className="glossary-click js-glossary-search drawer-search"
+          type="search"
+          placeholder="e.g. Fossil fuel"
+          value={this.state.glossaryTerm}
+          onChange={this.handleChange.bind(this)}
+          tabIndex={this.state.toggleHidden && -1} />
         <div id="glossary-click glossary-result">
           <ul className="glossary-click js-glossary-list list-unstyled" data-accordion="glossary-accordion">
             {(filteredTerms.terms).map((term, index) => (

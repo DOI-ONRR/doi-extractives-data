87653a28e24b658d3ec1298e86a8c5a39d9da8ea
Mojo Nichols <mojo.nichols@onrr.gov>
2019-09-06 11:02:58 -0400

    Added hasura container and postgres db

---
+++ database/scripts/create.db.sh
@@ -0,0 +1,9 @@
+
+
+#psql -Upostgres <<EOF
+
+#create DATABASE onrr_db;
+#create user onrr_user  with SUPERUSER with PASSWORD 'Oui4aP';
+#grant all privileges on DATABASE "onrr_db" to onrr_user;
+
+#EOF
---
+++ database/scripts/helloworld.sh
@@ -0,0 +1,1 @@
+echo helloworld 

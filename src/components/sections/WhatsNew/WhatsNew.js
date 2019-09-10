aa811ad5f404ad95254017283b8fccb87a6bab6a
Jennifer Malcolm <30806839+jennmalcolm@users.noreply.github.com>
2019-09-10 15:59:04 -0400

    added missing s on changes

--- src/components/sections/WhatsNew/WhatsNew.js
+++ src/components/sections/WhatsNew/WhatsNew.js
@@ -7,7 +7,7 @@ const WhatsNew = props => (
   <section className={styles.root + ' slab-delta'}>
   	<div className="container-page-wrapper">
       <h2>What's new</h2>
-      <p>In our latest release on September 11, 2019, we made the following change:</p>
+      <p>In our latest release on September 11, 2019, we made the following changes:</p>
       <ul className="list-bullet ribbon-card-top-list">
         <li>Updated monthly revenue and production data</li>
         <li>Updated outdated links in how it works</li>

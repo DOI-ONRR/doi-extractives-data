3be6b7e8b41306ae03c6df47b073f6144c7edd43
Mojo Nichols <mojo.nichols@onrr.gov>
2019-09-11 21:01:46 -0400

    Modified sqlite scripts to load postgres

---
+++ database/src/create.views.db.js
@@ -0,0 +1,98 @@
+
+const { Pool, Client } = require('pg')
+const db = new Pool({user: 'postgres',
+			 host: 'localhost',
+			 database: 'onrr_db',
+			 password: 'secret',
+			 port: 5433,
+			});
+
+
+
+const main = () => {
+
+   // yearly_calendar_revenue();
+   // yearly_fiscal_revenue();
+    
+    	revenue_trends();
+	
+}
+
+
+const yearly_calendar_revenue = () => {
+    let view=`
+-- drop view yearly_calendar_revenue;
+CREATE view yearly_calendar_revenue as
+select calendar_year as year, 
+       sum(case when revenue_category='Federal offshore' then revenue else 0 end) as 'Federal offshore',
+       sum(case when revenue_category='Federal onshore' then revenue else 0 end) as 'Federal onshore',
+       sum(case when revenue_category='Native American' then revenue else 0 end) as 'Native American',
+       sum(case when revenue_category='Not tied to a lease' then revenue else 0 end) as 'Not tied to a lease'
+
+from revenue 
+  natural join period 
+  natural join commodity 
+where commodity is not null
+group by calendar_year
+`
+    const create=db.query(view);
+
+}
+
+const yearly_fiscal_revenue = () => {
+    let view=`
+-- drop view yearly_fiscal_revenue;
+CREATE view yearly_fiscal_revenue as
+select fiscal_year as year, 
+       sum(case when revenue_category='Federal offshore' then revenue else 0 end) as 'Federal offshore',
+       sum(case when revenue_category='Federal onshore' then revenue else 0 end) as 'Federal onshore',
+       sum(case when revenue_category='Native American' then revenue else 0 end) as 'Native American',
+       sum(case when revenue_category='Not tied to a lease' then revenue else 0 end) as 'Not tied to a lease'
+
+from revenue 
+  natural join period 
+  natural join commodity 
+where commodity is not null
+group by fiscal_year
+
+
+`
+    const create=db.query(view);
+
+}
+
+
+const revenue_trends = () => {
+    let view=`
+drop view revenue_trends;
+CREATE view revenue_trends as
+select fiscal_year, 
+       case when (revenue_type='Other Revenues' or revenue_type='Civil Penalties' or revenue_type='Inspection Fees') then 'Other Revenues' else  revenue_type end as trend_type,  
+       (select month_long from period where period_date=(select max(period_date) from period where period_date <= '2019-07-01')) as current_month,
+       sum(case when fiscal_month <= (select fiscal_month from period where period_date=(select max(period_date) from period where period_date <= '2019-07-01' )) then revenue else 0 end) as total_ytd, 
+       sum(revenue) as total 
+from revenue 
+  natural join period 
+  natural join commodity 
+where commodity not null
+and period_date <= '2019-07-01'
+group by fiscal_year, trend_type
+
+union 
+select fiscal_year,  
+       'All Revenue' as trend_type,
+       (select month_long from period where period_date=(select max(period_date) from period where period_date <= '2019-07-01' )) as current_month,
+       sum(case when fiscal_month <= (select fiscal_month from period where period_date=(select max(period_date) from period where period_date <= '2019-07-01')) then revenue else 0 end) as total_ytd, 
+       sum(revenue) as total 
+from revenue natural join period natural join commodity 
+where commodity is not null and  period_date <= '2019-07-01'
+group by fiscal_year;
+
+
+`
+    const create=db.query(view);
+
+}
+
+
+main();
--- database/src/load.db.js
+++ database/src/load.db.js
@@ -14,23 +14,27 @@ const CONSTANTS = require('../../src/js/constants')
 
 
 const main =async () => {
-    //let period_lookup= await initPeriod({});
-    
-    let commodity_lookup=initCommodity({});
-    //let location_lookup=initLocation({});
+    let period_lookup=await  initPeriod({});
+    console.debug("PL", period_lookup);
+    let commodity_lookup=await initCommodity({});
+    let location_lookup=await initLocation({});
     etl.file(CSV)
 	.pipe(etl.csv()).pipe(etl.collect(BATCH)).promise().then(async  (data) => {
 	    data.map(async  (rows,index) => {
 		rows.map( async (row, ii) => {
-		    //let period_id=await addPeriod(row,period_lookup)[0];
-		    
+		   let location= await addLocation(row,location_lookup);
+		    let location_id=location[0]
+		    let commodity=await addCommodity(row,commodity_lookup);
+		    let commodity_id=commodity[0];; 
+		    let period= await addPeriod(row,period_lookup);
+		    let period_id=period[0];
+		    let raw_revenue=getRevenue(row);
+		    let revenue=cleanRevenue(raw_revenue);
+		    try {
+			
+			const insert = await db.query('insert into revenue( location_id, period_id, commodity_id, revenue , raw_revenue) values ($1 , $2 , $3 , $4 , $5 )',[location_id, period_id, commodity_id, revenue , raw_revenue]);
+		    } catch(err)  { console.debug("Fact", err)};
 
-		    let commodity_id=addCommodity(row,commodity_lookup)[0];
-		    //let location_id=addLocation(row,location_lookup)[0];
-		    //let raw_revenue=getRevenue(row);
-		    //let revenue=cleanRevenue(raw_revenue);
-		    //const insert = db.prepare('insert into revenue( location_id, period_id, commodity_id, revenue , raw_revenue) values (? , ? , ? , ? , ? )');
-		    //insert.run([location_id,period_id,commodity_id, revenue, raw_revenue])
 		});
 	    });
 	});
@@ -39,200 +43,23 @@ const main =async () => {
     
 }
 
-const initCommodity = (lookup) => {
-    let r=db.query('select * from commodity').then(
-	(res) => {
-	    res.rows.map( (row, i) => {
-		let key=row.product+'-'+row.commodity+'-'+row.revenue_type+'-'+row.mineral_lease_type;
-		lookup[key]=[row.commodity_id, row.product, row.commodity, row.revenue_type, row.mineral_lease_type]
-	    })
-	    return lookup
-	}).catch( err => console.debug(err));
-    return r;
-    
-    
-}
-
-const addCommodity= async (row, lookup) => {
-    let product='';
-    let commodity='Not tied to a commodity';
-    let revenue_type='';
-    let revenue_category='';
-    let mineral_lease_type='';
-    for( let field in row) {
-	switch (field) {
-	case 'Product':
-	    product=row[field];
-	    break;
-	case 'Commodity':
-	    commodity=row[field];
-	    break;
-	case 'Revenue Type':
-	    revenue_type=row[field];
-	    break;
-	case 'Mineral Lease Type':
-	    mineral_lease_type=row[field];
-	    break;
-	    
-	}
-    }
-    // add a revenue category based on land class and category otherwise  default
-    revenue_category=LAND_CLASS_CATEGORY_TO_REVENUE_CATEGORY[row["Land Class"]]
-	&& LAND_CLASS_CATEGORY_TO_REVENUE_CATEGORY[row["Land Class"]][row["Land Category"]];
-    
-    if (revenue_category === undefined) {
-	if (row["Land Class"] === CONSTANTS.NATIVE_AMERICAN) {
-	    revenue_category = CONSTANTS.NATIVE_AMERICAN
-	} else {
-	    revenue_category = 'Not tied to a lease'
-	}
-    }
-
-    let key=product+'-'+commodity+'-'+revenue_type+'-'+revenue_category+'-'+mineral_lease_type;
-    
-    if(lookup[key]) {
-	return lookup[key];
-    } else {
-
-	try {
-	    const insert = await db.query('insert into commodity(  product,commodity, revenue_type, revenue_category,  mineral_lease_type) values ($1 , $2 , $3 , $4 , $5   ) ON CONFLICT DO NOTHING returning commodity_id', [ product, commodity, revenue_type, revenue_category, mineral_lease_type]);
-	    if(insert.rows.length > 0 ) {
-		console.debug("INSERT ----------------------", insert);
-		lookup[key]=[insert.rows[0].commodity_id, product, commodity, revenue_type, revenue_category, mineral_lease_type];
-	    } else {
-		console.debug("IGNORE ----------------------")
-	    }
-	} catch  (err) {
-	    console.log("ERROR: ", err.stack);
-	}
-    }
-    return lookup[key]
-}
-
-
-
-
-
-const initPeriod = (lookup) => {
-    
-    
-    let r=  db.query('select * from period',).then(
-	(res) => {
-	    res.rows.map( (row, i) => {
-		let key=row.period+'-'+row.calendar_year+'-'+row.fiscal_year+'-'+row.month+'-'+row.month_long+'-'+row.fiscal_month+'-'+row.period_date;
-		lookup[key]=[row.period_id, row.calendar_year, row.fiscal_year, row.month, row.month_long, row.fiscal_month, row.period_date];
-	    });
-	    
-	    return lookup
-	}).catch( err => console.debug(err));
-    return r;
-    
-
-}
-
-const addPeriod=async (row, lookup) => {
-    let period='';
-    let calendar_year='';
-    let fiscal_year='';
-    let month='';
-    let month_long='';
-    let fiscal_month='';
-    let period_date='';
-
-    for( let field in row) {
-	switch (field) {
-	case 'Fiscal Year':
-	    period='Fiscal Year';	    fiscal_year=row[field];
-	    break;
-	case 'Month':
-	    period='Monthly';
-	    month_long=row[field];
-	    month=monthToNumber(row[field]);
-	    fiscal_month=monthToFiscalMonth(row[field]);
-	    break;
-	case 'Calendar Year':
-	    calendar_year=row[field];
-	    
-	    
-	}
-    }
-    if(month && calendar_year) {
-	period_date=calendar_year+'-'+month+'-01 00:00:00';
-	
-	fiscal_year=getFiscalYear(period_date)
-	
-
-    } else if (calendar_year) {
-	period_date=calendar_year+'-01-01 00:00:00';
-    } 
-    let key=period+'-'+calendar_year+'-'+fiscal_year+'-'+month+'-'+month_long+'-'+fiscal_month+'-'+period_date;
-    if(lookup[key]) {
-	return lookup[key];
-    } else {
-
-	try {
-	    const insert =await db.query('insert into period( period, calendar_year,fiscal_year, month, month_long,fiscal_month, period_date) values ($1 , $2 , $3 , $4 , $5 , $6 , $7 ) ON CONFLICT DO NOTHING returning period_id', [ period, calendar_year,fiscal_year, month, month_long, fiscal_month, period_date] )
-	    if(insert.rows.length > 0 ) {
-		console.debug("INSERT ----------------------", insert);
-		lookup[key]=[insert.rows[0].period_id, period, calendar_year, fiscal_year, month, month_long, fiscal_month,  period_date];
-		db.end()
-	    }else {
-		console.debug("IGNORE ----------------------")
-	    }
-	   
-	} catch (err) {
-	    console.log("ERROR: ", err.stack);
-	}
-	//	rowid=insert.run([null, period, calendar_year,fiscal_year, month, month_long, fiscal_month, period_date]);
-	//lookup[key]=[rowid.lastInsertRowid, period, calendar_year, fiscal_year, month, month_long, fiscal_month,  period_date];
-	return lookup[key]; 
-
-    }
-//    console.debug("lookup -=-------", lookup);
-}
-
-
-
-
-const getRevenue = (row) => {
-    let revenue=null;
-    for ( let field in row) {
-	switch (field) {
-	case ' Revenue ':
-	    revenue=row[field];
-	    break;
-	}
-    }
-    return revenue;
-
-}
-
-const cleanRevenue = (revenue) => {
-    let trim=/[\s,\$\)]/g;
-    let neg=/\(/g;
-    revenue=revenue.replace(trim, '');
-    revenue=revenue.replace(neg, '-');
-    revenue=Number(revenue);
-    if(isNaN(revenue)) {
-	return null;
-    } else {
-	return revenue;
-    }
-}
-
 const initLocation = (lookup) => {
-    let sql=db.prepare('select * from location');
-    sql.all().map( (row, i) => {
-	let key=row.fips_code+'-'+row.state+'-'+row.county+'-'+row.land_class+'-'+row.land_category+'-'+row.offshore_region+'-'+row.offshore_planning_area+'-'+row.offshore_planning_area_code+'-'+row.offshore_block+'-'+row.offshore_protraction;
-	lookup[key]=[row.location_id,row.fips_code,row.state,row.county,row.land_class,row.land_category,row.offshore_region,row.offshore_planning_area,row.offshore_planning_area_code,row.offshore_block,row.offshore_protraction];
-    });
-    return lookup
+    let r=db.query('select * from location').then(
+	(res) => {
+	    res.rows.map( (row, i) => {
+		let key=row.fips_code+'-'+row.state+'-'+row.county+'-'+row.land_class+'-'+row.land_category+'-'+row.offshore_region+'-'+row.offshore_planning_area+'-'+row.offshore_planning_area_code+'-'+row.offshore_block+'-'+row.offshore_protraction;
+		lookup[key]=[row.location_id,row.fips_code,row.state,row.county,row.land_class,row.land_category,row.offshore_region,row.offshore_planning_area,row.offshore_planning_area_code,row.offshore_block,row.offshore_protraction];
+	
+	    });
+	    return lookup
+	}).catch( err => console.debug(err));
+	return r
     
 
 }
 
-const addLocation=(row, lookup) => {
-
+const addLocation= async (row, lookup) => {
+   
     let fips_code='' , state='' , county='' , land_class='' , land_category='' , offshore_region='' , offshore_planning_area='' , offshore_planning_area_code='' , offshore_block='' , offshore_protraction='';
     for( let field in row) {
 	switch (field) {
@@ -283,29 +110,244 @@ const addLocation=(row, lookup) => {
     if(lookup[key]) {
 	return lookup[key];
     } else {
-	 const insert = db.prepare('insert into location( location_id,	fips_code, state,county,land_class,land_category, offshore_region, offshore_planning_area, offshore_planning_area_code, offshore_block,offshore_protraction) values ( ?, ?, ?, ?, ?, ?, ?, ?, ?, ?,?)'); 
-
-	rowid=insert.run([null, fips_code,state,county,land_class,land_category,offshore_region,offshore_planning_area,offshore_planning_area_code,offshore_block,offshore_protraction]);
-	lookup[key]=[rowid.lastInsertRowid, fips_code,state,county,land_class,land_category,offshore_region,offshore_planning_area,offshore_planning_area_code,offshore_block,offshore_protraction];
+	try {
+	    const insert = await  db.query('insert into location(fips_code, state,county,land_class,land_category, offshore_region, offshore_planning_area, offshore_planning_area_code, offshore_block,offshore_protraction) values ( $1, $2, $3, $4, $5, $6, $7, $8, $9, $10) ON CONFLICT DO NOTHING  returning location_id', [fips_code,state,county,land_class,land_category,offshore_region,offshore_planning_area,offshore_planning_area_code,offshore_block,offshore_protraction]); 
+	    
+	    if(insert.rows.length > 0 ) {
+		console.debug("INSERT ----------------------", insert);
+		lookup[key]=[insert.rows[0].location_id, fips_code,state,county,land_class,land_category, offshore_region,offshore_planning_area,offshore_planning_area_code,offshore_block,offshore_protraction];
+		
+	    } else {
+	    	//console.debug("IGNORE ----------------------");
+	    }
+	} catch  (err) {
+	    console.log("ERROR: ", err.stack);
+	}
+	console.debug("addLocation", lookup[key])
 	return lookup[key];
     }
 }
 
 
+
+
+const initCommodity = (lookup) => {
+    let r=db.query('select * from commodity').then(
+	(res) => {
+	    res.rows.map( (row, i) => {
+		let key=row.product+'-'+row.commodity+'-'+row.revenue_type+'-'+row.revenue_category+'-'+row.mineral_lease_type;
+		lookup[key]=[row.commodity_id, row.product, row.commodity, row.revenue_type,row.revenue_category, row.mineral_lease_type]
+	    })
+	    return lookup
+	}).catch( err => console.debug(err));
+    return r;
+    
+    
+}
+
+const addCommodity= async (row, lookup) => {
+    let product='';
+    let commodity='Not tied to a commodity';
+    let revenue_type='';
+    let revenue_category='';
+    let mineral_lease_type='';
+    for( let field in row) {
+	switch (field) {
+	case 'Product':
+	    product=row[field];
+	    break;
+	case 'Commodity':
+	    commodity=row[field];
+	    break;
+	case 'Revenue Type':
+	    revenue_type=row[field];
+	    break;
+	case 'Mineral Lease Type':
+	    mineral_lease_type=row[field];
+	    break;
+	    
+	}
+    }
+    // add a revenue category based on land class and category otherwise  default
+    revenue_category=LAND_CLASS_CATEGORY_TO_REVENUE_CATEGORY[row["Land Class"]]
+	&& LAND_CLASS_CATEGORY_TO_REVENUE_CATEGORY[row["Land Class"]][row["Land Category"]];
+    
+    if (revenue_category === undefined) {
+	if (row["Land Class"] === CONSTANTS.NATIVE_AMERICAN) {
+	    revenue_category = CONSTANTS.NATIVE_AMERICAN
+	} else {
+	    revenue_category = 'Not tied to a lease'
+	}
+    }
+
+    let key=product+'-'+commodity+'-'+revenue_type+'-'+revenue_category+'-'+mineral_lease_type;
+    if(lookup[key]) {
+
+	return lookup[key];
+    } else {
+
+	try {
+	    const insert = await db.query('insert into commodity(  product,commodity, revenue_type, revenue_category,  mineral_lease_type) values ($1 , $2 , $3 , $4 , $5   ) ON CONFLICT DO NOTHING returning commodity_id', [ product, commodity, revenue_type, revenue_category, mineral_lease_type]);
+	    if(insert.rows.length > 0 ) {
+		console.debug("INSERT ----------------------", insert);
+		lookup[key]=[insert.rows[0].commodity_id, product, commodity, revenue_type, revenue_category, mineral_lease_type];
+	    } else {
+		//console.debug("IGNORE ----------------------")
+	    }
+	} catch  (err) {
+	    console.log("ERROR: ", err.stack);
+	}
+    }
+    console.debug("addCommodity", key, lookup);
+    
+    return lookup[key]
+}
+
+
+
+
+
+const initPeriod = (lookup) => {
+    
+    
+    let r=  db.query('select * from period',).then(
+	(res) => {
+	    res.rows.map( (row, i) => {
+		let date=row.period_date.toISOString().replace('T', ' ');
+		date=date.replace('Z','');
+
+		let key=row.period+'-'+row.calendar_year+'-'+row.fiscal_year+'-'+row.month+'-'+row.month_long+'-'+row.fiscal_month
+		lookup[key]=[row.period_id, row.calendar_year, row.fiscal_year, row.month, row.month_long, row.fiscal_month, date];
+	    });
+	    
+	    return lookup
+	}).catch( err => console.debug(err));
+    return r;
+    
+
+}
+
+const addPeriod=async (row, lookup) => {
+    let period='';
+    let calendar_year='';
+    let fiscal_year='';
+    let month='';
+    let month_long='';
+    let fiscal_month='';
+    let period_date='';
+
+    for( let field in row) {
+	switch (field) {
+	case 'Fiscal Year':
+	    period='Fiscal Year';	    fiscal_year=row[field];
+	    break;
+	case 'Month':
+	    period='Monthly';
+	    month_long=row[field];
+	    month=monthToNumber(row[field]);
+	    fiscal_month=monthToFiscalMonth(row[field]);
+	    break;
+	case 'Calendar Year':
+	    calendar_year=row[field];
+	    
+	    
+	}
+    }
+    if(month && calendar_year) {
+	period_date=calendar_year+'-'+month+'-01 00:00:00';
+	
+	fiscal_year=getFiscalYear(period_date)
+	
+
+    } else if (calendar_year) {
+	period_date=calendar_year+'-01-01 00:00:00';
+    }
+
+    let key=period+'-'+calendar_year+'-'+fiscal_year+'-'+month+'-'+month_long+'-'+fiscal_month
+    console.debug("key", key, lookup)
+    if(lookup[key]) {
+	return lookup[key];
+    } else {
+
+	try {
+	    const insert =await db.query('insert into period( period, calendar_year,fiscal_year, month, month_long,fiscal_month, period_date) values ($1 , $2 , $3 , $4 , $5 , $6 , $7 ) ON CONFLICT DO NOTHING returning period_id', [ period, calendar_year,fiscal_year, month, month_long, fiscal_month, period_date] )
+	    if(insert.rows.length > 0 ) {
+		console.debug("INSERT ----------------------", insert);
+		lookup[key]=[insert.rows[0].period_id, period, calendar_year, fiscal_year, month, month_long, fiscal_month,  period_date];
+	    }else {
+
+
+
+		console.debug("key", key    );
+		console.debug("lookup", lookup    );
+		console.debug("row", row    );
+
+	//	console.debug("IGNORE----------------------");
+	//	process.exit();
+	    }
+	   
+	} catch (err) {
+	    console.log("ERROR: ", err.stack);
+	}
+	//	rowid=insert.run([null, period, calendar_year,fiscal_year, month, month_long, fiscal_month, period_date]);
+	//lookup[key]=[rowid.lastInsertRowid, period, calendar_year, fiscal_year, month, month_long, fiscal_month,  period_date];
+
+	return lookup[key]; 
+
+    }
+//    console.debug("lookup -=-------", lookup);
+}
+
+
+
+
+const getRevenue = (row) => {
+    let revenue=null;
+    for ( let field in row) {
+	switch (field) {
+	case ' Revenue ':
+	    revenue=row[field];
+	    break;
+	}
+    }
+    return revenue;
+
+}
+
+const cleanRevenue = (revenue) => {
+    let trim=/[\s,\$\)]/g;
+    let neg=/\(/g;
+    revenue=revenue.replace(trim, '');
+    revenue=revenue.replace(neg, '-');
+    revenue=Number(revenue);
+    if(isNaN(revenue)) {
+	return null;
+    } else {
+	return revenue;
+    }
+}
+
+
+
 const monthToNumber = (month_long) => {
     const m={
-	'january':'01',
-	'february':'02',
-	'march':'03',
-	'april':'04',
-	'may':'05',
-	'june':'06',
-	'july':'07',
-	'august':'08',
-	'september':'09',
-	'october':'10',
-	'november':'11',
-	'december':'12'
+	'january':1,
+	'february':2,
+	'march':3,
+	'april':4,
+	'may':5,
+	'june':6,
+	'july':7,
+	'august':8,
+	'september':9,
+	'october':10,
+	'november':11,
+	'december':12
+
+
+
+
+	
     }
     return m[month_long.toLowerCase()];
     

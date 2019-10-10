42731c1da7d61f759813f2b9a1d34eb3723063a2
Jerome <jerome.louis@onrr.gov>
2019-10-10 16:12:09 -0600

    Install gatsby-plugin-sitemap, init config

--- gatsby-config.js
+++ gatsby-config.js
@@ -121,6 +121,39 @@ let config = {
           node.frontmatter.tags !== 'exempt',
       },
     },
+    {
+      resolve: `gatsby-plugin-sitemap`,
+      options: {
+        output: `/nrrd-sitemap.xml`,
+        // Exclude specific pages or groups of pages using glob parameters
+        // See: https://github.com/isaacs/minimatch
+        exclude: [],
+        query: `
+          {
+            site {
+              siteMetadata {
+                siteUrl
+              }
+            }
+
+            allSitePage {
+              edges {
+                node {
+                  path
+                }
+              }
+            }
+        }`,
+        serialize: ({ site, allSitePage }) =>
+          allSitePage.edges.map(edge => {
+            return {
+              url: site.siteMetadata.siteUrl + edge.node.path,
+              changefreq: `daily`,
+              priority: 0.7,
+            }
+          })
+      }
+    },
     'custom-excel-transformer',
     `gatsby-plugin-meta-redirect`, // make sure to put last in the array
   ],

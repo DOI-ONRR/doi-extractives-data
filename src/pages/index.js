803a5800310ce6cd86a66ab64da877db6fe3cfea
Jerome <jerome.louis@onrr.gov>
2019-10-10 15:40:36 -0600

    Remove non breaking space

--- src/pages/index.js
+++ src/pages/index.js
@@ -122,7 +122,7 @@ class Beta extends React.Component {
 		  ]} >
 		</Helmet>
 	  <section className="container-page-wrapper">
-      <span className="h3-bar">&nbsp;</span>
+      <span className="h3-bar"></span>
 		  <p> When companies extract energy and mineral resources on property leased from the federal government and Native Americans, they pay <GlossaryTerm termKey="Bonus">bonuses</GlossaryTerm>, <GlossaryTerm>rent</GlossaryTerm>, and <GlossaryTerm termKey="Royalty">royalties</GlossaryTerm>. The Office of Natural Resources Revenue (ONRR) collects and <GlossaryTerm termKey="disbursement">disburses</GlossaryTerm> revenue from federal lands and waters to different agencies, funds, and local governments for public use. All revenue collected from extraction on Native American lands is disbursed to Native American tribes, nations, or individuals.</p>
 		</section>
 		<Tabordion>

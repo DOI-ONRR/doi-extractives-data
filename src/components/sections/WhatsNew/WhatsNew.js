97b6f11587dd9c2b15cfc85e7a4b414434a3521a
Jennifer Malcolm <30806839+jennmalcolm@users.noreply.github.com>
2019-10-24 08:04:11 -0400

    updated what's new

--- src/components/sections/WhatsNew/WhatsNew.js
+++ src/components/sections/WhatsNew/WhatsNew.js
@@ -7,9 +7,8 @@ const WhatsNew = props => (
   <section className={styles.root + ' slab-delta'}>
   	<div className="container-page-wrapper">
       <h2>What's new</h2>
-      <p>In our latest release on October 23, 2019, we made the following changes:</p>
+      <p>In our latest release on October 24, 2019, we made the following changes:</p>
       <ul className="list-bullet ribbon-card-top-list">
-        <li>Updated revenue and disbursements data through Fiscal Year 2019 </li>
           <li>Updated monthly production data through June 2019 </li>
           <li>Added <Link to="/blog">blog post about designing for accessibility and inclusion</Link></li>
       </ul>

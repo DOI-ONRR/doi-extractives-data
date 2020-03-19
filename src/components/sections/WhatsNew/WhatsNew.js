b4aa166de8af684d859e51282320a5aabb3a1ad9
Jennifer Malcolm <30806839+jennmalcolm@users.noreply.github.com>
2020-03-19 16:00:57 -0400

    Updated what's new

--- src/components/sections/WhatsNew/WhatsNew.js
+++ src/components/sections/WhatsNew/WhatsNew.js
@@ -7,12 +7,11 @@ const WhatsNew = props => (
   <section className={styles.root + ' slab-delta'}>
   	<div className="container-page-wrapper">
       <h2>What's new</h2>
-      <p>In our latest release on March 6, 2020, we made the following changes:</p>
+      <p>In our latest release on March 20, 2020, we made the following changes:</p>
       <ul className="list-bullet ribbon-card-top-list">
-        <li>Updated <Link to="/downloads/federal-disbursements-by-month/">monthly disbursements data through January 2020</Link></li>
-        <li>Updated <Link to="/downloads/federal-production-by-month/">monthly production data through October 2019</Link></li>
-        <li>Updated <Link to="/downloads/federal-revenue-by-month/">monthly revenue data through January 2020</Link></li>
-        <li>Added <Link to="/downloads/#data-collection-validation">content for data collection and validation</Link></li>
+        <li>Updated <Link to="/downloads/federal-production-by-month/">monthly production data through November 2019</Link></li>
+        <li>Updated <Link to="/downloads/federal-revenue-by-month/">monthly revenue data through February 2020</Link></li>
+        <li>Added <Link to="/blog/remote-design-studio">blog post on running a remote design studio</Link></li>
       </ul>
 
       <p>Review our <a href="https://github.com/ONRR/doi-extractives-data/releases">full release details</a>.</p>

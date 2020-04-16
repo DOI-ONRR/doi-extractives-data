07c20bc1b6e073236c2398633519e7098c32539f
Jennifer Malcolm <30806839+jennmalcolm@users.noreply.github.com>
2020-04-16 12:25:21 -0400

    update what's new

--- src/components/sections/WhatsNew/WhatsNew.js
+++ src/components/sections/WhatsNew/WhatsNew.js
@@ -7,10 +7,11 @@ const WhatsNew = props => (
   <section className={styles.root + ' slab-delta'}>
   	<div className="container-page-wrapper">
       <h2>What's new</h2>
-      <p>In our latest release on March 30, 2020, we made the following changes:</p>
+      <p>In our latest release on April 16, 2020, we made the following changes:</p>
       <ul className="list-bullet ribbon-card-top-list">
-        <li>Updated <Link to="/downloads/federal-production">fiscal year production data through 2019</Link></li>
-        <li>Added <Link to="/how-it-works/gomesa/#revenue-sharing">2020 GOMESA disbursements to states and local governments</Link></li>
+        <li>Updated <Link to="/downloads/production-by-month">monthly production data through December 2019</Link></li>
+        <li>Updated <Link to="/downloads/revenue-by-month">monthly revenue data through March 2020</Link></li>
+        <li>Added <Link to="/blog/training-update">blog post on our progress since peer training on user research</Link></li>
       </ul>
 
       <p>Review our <a href="https://github.com/ONRR/doi-extractives-data/releases">full release details</a>.</p>

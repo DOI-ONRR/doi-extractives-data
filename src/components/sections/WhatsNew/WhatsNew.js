679e892b7e03102dd47adee3c3f138e42f33ff41
Jennifer Malcolm <30806839+jennmalcolm@users.noreply.github.com>
2020-07-16 17:15:11 -0400

    fix one link and one typo

--- src/components/sections/WhatsNew/WhatsNew.js
+++ src/components/sections/WhatsNew/WhatsNew.js
@@ -11,8 +11,8 @@ const WhatsNew = props => (
       <ul className="list-bullet ribbon-card-top-list">
         <li>Updated <Link to="/downloads/revenue-by-month">monthly revenue data</Link> through June 2020</li>
         <li>Updated <Link to="/downloads/production-by-month">monthly production data</Link> through March 2020</li>
-        <li>Added <Link to="/downloads/revenue-by-company"> calendar year 2019 revenue by comppany download file</Link></li>
-        <li>Added <Link to="/blog/Annie">blog post from our intern discussing her journey working with our team</Link></li>
+        <li>Added <Link to="/downloads/federal-revenue-by-company"> calendar year 2019 revenue by company file</Link></li>
+        <li>Added <Link to="/blog/intern-onrr-dot-gov/">blog post from our intern discussing her journey working with our team</Link></li>
       </ul>
 
       <p>Review our <a href="https://github.com/ONRR/doi-extractives-data/releases">full release details</a>.</p>

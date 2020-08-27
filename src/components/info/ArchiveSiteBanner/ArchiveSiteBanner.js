ee5e0b03d427aea03129d29dbd4ab5c530dc9b81
Jeffery Keene <Jeff.Keene@onrr.gov>
2020-08-27 14:12:39 -0400

    added archive site banner

---
+++ src/components/info/ArchiveSiteBanner/ArchiveSiteBanner.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Link from '../../utils/temp-link'
+
+import styles from './ArchiveSiteBanner.module.scss'
+import DataArchiveIcon from '-!svg-react-loader!../../../img/svg/icon-archive.svg'
+
+const ArchiveSiteBanner = () => (
+  <div className={styles.root}>
+    <p className={styles.content}>
+      <DataArchiveIcon/> This is an archived version of this website that is no longer being updated. For current data, visit the <Link to="/archive">latest version of the site.</Link>
+    </p>
+  </div>
+)
+
+export default ArchiveSiteBanner
